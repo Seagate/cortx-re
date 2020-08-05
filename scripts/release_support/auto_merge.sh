73e4adf25c142921b006333b33ea57e1f50e8364
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-08-05 06:19:35 -0600

    Updated auto_merge.sh script

--- scripts/release_support/auto_merge.sh
+++ scripts/release_support/auto_merge.sh
@@ -2,8 +2,8 @@
 
 function usage() {
 echo "No inputs provided exiting..."
-echo "Please provide GitHub Token, source branch and destination branch.Script should be executed as.."
-echo "$0 <GITHUB TOKEN> dev relase"
+echo "Please provide GitHub Token, repo url,source branch and destination branch.Script should be executed as.."
+echo "$0 <GITHUB TOKEN> https://github.com/Seagate/cortx-re dev relase"
 exit 1
 }
 
@@ -11,42 +11,23 @@ if [ $# -eq 0 ]; then
 usage
 fi
 
-GIT_CRED=$1
-SOURCE_BRANCH=$2
-DEST_BRANCH=$3
+TOKEN=$1
+REPO_URL=$2
+SOURCE_BRANCH=$3
+DEST_BRANCH=$4
 
 git config --global user.email "cortx-application@seagate.com"
 git config --global user.name "cortx-admin"
 
-
-#declare -A COMPONENT_LIST=(
-#[cortx-s3server]="https://$GIT_CRED@github.com/Seagate/cortx-s3server.git"
-#[cortx-motr]="https://$GIT_CRED@github.com/Seagate/cortx-motr.git"
-#[cortx-hare]="http://gitlab.mero.colo.seagate.com/mero/hare.git"
-#[cortx-ha]="https://$GIT_CRED@github.com/Seagate/cortx-ha.git"
-#[cortx-prvsnr]="https://$GIT_CRED@github.com/Seagate/cortx-prvsnr.git"
-#[cortx-sspl]="https://$GIT_CRED@github.com/Seagate/cortx-sspl.git"
-#[cortx-csm_agent]="https://$GIT_CRED@github.com/Seagate/cortx-csm.git"
-#[cortx-fs]="https://$GIT_CRED@github.com/Seagate/cortx-posix.git"
-#)
-
-declare -A COMPONENT_LIST=(
-[cortx-re]="https://$GIT_CRED@github.com/shailesh-vaidya/cortx-re.git"
-)
+REPO_CLONE_URL=$(echo $REPO_URL | sed 's/github/'$TOKEN'@github/g')
 
 clone_dir="/root/git_auto_merge"
-time_zone="Asia/Calcutta"
-
 test -d $clone_dir/clone && $(rm -rf $clone_dir/clone;mkdir -p $clone_dir/clone) || mkdir -p $clone_dir/clone
 
 pushd $clone_dir/clone
 
-for component in "${!COMPONENT_LIST[@]}"
-do
-        echo "Component:$component"
-        echo "Repo:${COMPONENT_LIST[$component]}"
-         dir=$(echo ${COMPONENT_LIST[$component]} |  awk -F'/' '{print $NF}')
-         git clone --branch dev ${COMPONENT_LIST[$component]} $dir
+         dir=$(echo $REPO_CLONE_URL |  awk -F'/' '{print $NF}')
+         git clone --branch dev $REPO_CLONE_URL  $dir
          rc=$?
           if [ $rc -ne 0 ]; then
           echo "ERROR:git clone failed for $component"
@@ -57,5 +38,4 @@ do
          git checkout $DEST_BRANCH && git merge $SOURCE_BRANCH -m "Automated merge from dev to release at $(date +"%d-%b-%Y %H:%M")" && git push origin release
          popd
 
-done
 popd
