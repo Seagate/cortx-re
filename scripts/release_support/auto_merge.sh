66b1a8d924e239d7cd756a41e6331541bf39db59
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-08-05 07:41:56 -0600

    Fixed logical error's in auto_merge.sh script

--- scripts/release_support/auto_merge.sh
+++ scripts/release_support/auto_merge.sh
@@ -19,23 +19,22 @@ DEST_BRANCH=$4
 git config --global user.email "cortx-application@seagate.com"
 git config --global user.name "cortx-admin"
 
-REPO_CLONE_URL=$(echo $REPO_URL | sed 's/github/'$TOKEN'@github/g')
-
+REPO_CLONE_URL="${REPO_URL//github/$TOKEN@github}"
 clone_dir="/root/git_auto_merge"
 test -d $clone_dir/clone && $(rm -rf $clone_dir/clone;mkdir -p $clone_dir/clone) || mkdir -p $clone_dir/clone
 
-pushd $clone_dir/clone
+pushd $clone_dir/clone || exit
 
-         dir=$(echo $REPO_CLONE_URL |  awk -F'/' '{print $NF}')
-         git clone --branch dev $REPO_CLONE_URL  $dir
+         dir=$(echo "$REPO_CLONE_URL" |  awk -F'/' '{print $NF}')
+         git clone --branch dev "$REPO_CLONE_URL"  "$dir"
          rc=$?
           if [ $rc -ne 0 ]; then
-          echo "ERROR:git clone failed for $component"
+          echo "ERROR:git clone failed for $dir"
           exit 1
           fi
-         pushd $dir
+         pushd "$dir" || exit
          echo -e "\t--[ Automated merge for $dir ]--"
-         git checkout $DEST_BRANCH && git merge $SOURCE_BRANCH -m "Automated merge from dev to release at $(date +"%d-%b-%Y %H:%M")" && git push origin release
-         popd
+         git checkout "$DEST_BRANCH" && git merge "$SOURCE_BRANCH" -m "Automated merge from dev to release at $(date +"%d-%b-%Y %H:%M")" && git push origin release
+         popd || exit
 
-popd
+popd || exit
