51d0ea3e1856fce92fe172f637e1a08bef7e2be7
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-08-25 09:47:07 -0600

    Updated license-term-validation.sh

--- scripts/reports/license-term-validation.sh
+++ scripts/reports/license-term-validation.sh
@@ -13,9 +13,9 @@ if [ -z "$GITHUB_TOKEN" ]
     exit 1
 fi
 
-SEARCH_KEYWORDS=("xyratex", "Copyright")
+SEARCH_KEYWORDS=("xyratex")
 
-NEGATION_SEARCH_KEYWORDS=("Copyright")
+EXCLUDE_SEARCH_KEYWORDS=("Copyright")
 
 GITUB_REPO_QUERY_API="https://api.github.com/orgs/seagate/repos?access_token=${GITHUB_TOKEN}&per_page=100&page=1"
 
@@ -25,7 +25,7 @@ GITUB_REPO_QUERY_API="https://api.github.com/orgs/seagate/repos?access_token=${G
 #                "HA:cortx-ha"
 #                "CSM:cortx-csm,statsd-utils,cortx-py-utils"
 #                "NFS:cortx-posix,cortx-fs,cortx-utils,cortx-nsal,cortx-dsal,cortx-fs-ganesha"
-#                "Provisioner:cortx-prvsnr"  
+#                "Provisioner:cortx-prvsnr"
 #                "SSPL:cortx-sspl"
 #                "RE:cortx-re"
 #                "Motr-Test:castor-integration-tests,engservice-tools,xperior,xperior-perl-libs")
@@ -35,7 +35,7 @@ GITHUB_REPOS=(  "Provisioner:cortx-prvsnr"
                 "RE:cortx-re"
                 )
 
-                
+
 TABLE_HTML="<!DOCTYPE html><html><head><style>#search{font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif;
 border-collapse: collapse; width: 100%;}#search td, #search th{border: 1px solid #ddd; padding: 8px;}#search tr:nth-child(even)
 {background-color: #f2f2f2;}#search tr:hover{background-color: #ddd;}#search th{padding-top: 12px; padding-bottom: 12px;
@@ -55,8 +55,8 @@ summary_total_file_count=0
 
 _clone_repo(){
     for repo in "${component_repos[@]}"
-    do  
-		git clone https://$GITHUB_TOKEN@github.com/Seagate/$repo.git
+    do
+                git clone https://$GITHUB_TOKEN@github.com/Seagate/$repo.git
     done
 }
 
@@ -71,33 +71,25 @@ _prepare_summary_report_header(){
     summary_header=""
     for keywords in "${SEARCH_KEYWORDS[@]}"
     do
-        summary_header+="<th>$keywords</th>" 
+        summary_header+="<th>$keywords</th>"
     done
     summary_header+="<th>Total No of Occurrences</th><th>Total Files</th>"
     SUMMARY_HTML=${SUMMARY_HTML/HEADER_SECTION/$summary_header}
 }
 
 _generate_search_report(){
-    
+
     total_occurrences_count=0
     total_occurrences_file_count=0
 
     # Generate Repository Report
     for repo_name in ${component_repos[@]}
-    do           
+    do
         result_html=""
         default_git_branch=$(cd $repo_name && git symbolic-ref --short HEAD)
         git_repo=$(cd $repo_name && git config --get remote.origin.url)
         for keywords in "${SEARCH_KEYWORDS[@]}"
         do
-
-            GREP_EXTRA_ARG="v"
-            for fullsearch_word in "${NEGATION_SEARCH_KEYWORDS[@]}"
-            do
-                if [ "$fullsearch_word" == "$keywords" ] ; then
-                    GREP_EXTRA_ARG=""
-                fi
-            done
 
             echo "Searching for Keyword [ $keywords ] in branch [ $default_git_branch ] with GREP_EXTRA_ARG=$GREP_EXTRA_ARG"
             echo "------------------------------------"
@@ -106,43 +98,60 @@ _generate_search_report(){
             search_result=$(grep -ril$GREP_EXTRA_ARG "$keywords" "$repo_name" --exclude-dir=".git" | sed "s|.*|<a href=\"$git_repo\/&\" target=\"_blank\">&<\/a>|")
             search_result=$(echo -e ${search_result//$'\n'/<br />})
             search_result=$(echo -e ${search_result} | sed -e "s=/$repo_name.git/$repo_name=/$repo_name/blob/$default_git_branch=g")
-            
+
             occurrences_text="-"
             if [[ $search_occurrences_count -ne 0 ]];
-            then 
+            then
                 occurrences_text="Found $search_occurrences_count occurrences in $search_occurrences_file_count Files"
             fi
 
             result_html+="<tr class='repo'><td>${keywords}</td><td>$search_result</td><td>$occurrences_text</td></tr>"
-            
+
             echo -e "\t Found $search_occurrences_count in $search_occurrences_file_count file for $keywords in $repo_name"
             echo "------------------------------------"
 
         done
+		
+		for keywords in "${EXCLUDE_SEARCH_KEYWORDS[@]}"
         
+		do
+
+            echo "Searching for files without Keyword [ $keywords ] in branch [ $default_git_branch ] with GREP_EXTRA_ARG=$GREP_EXTRA_ARG"
+            echo "------------------------------------"
+            search_occurrences_file_count=$(grep -riL$GREP_EXTRA_ARG "$keywords" "$repo_name" --exclude-dir=".git" | wc -l)
+            search_result=$(grep -riL$GREP_EXTRA_ARG "$keywords" "$repo_name" --exclude-dir=".git" | sed "s|.*|<a href=\"$git_repo\/&\" target=\"_blank\">&<\/a>|")
+            search_result=$(echo -e ${search_result//$'\n'/<br />})
+            search_result=$(echo -e ${search_result} | sed -e "s=/$repo_name.git/$repo_name=/$repo_name/blob/$default_git_branch=g")
+
+            occurrences_text="-"
+            if [[ $search_occurrences_file_count -ne 0 ]];
+            then
+                occurrences_text="Found $search_occurrences_file_count Files"
+            fi
+
+            result_html+="<tr class='repo'><td>${keywords}</td><td>$search_result</td><td>$occurrences_text</td></tr>"
+
+            echo -e "\t Found $search_occurrences_file_count files without $keywords in $repo_name"
+            echo "------------------------------------"
+
+        done
+		
+
         local_html=${TABLE_HTML/$REPORT_HEADER/$REPORT_HEADER in $repo_name}
         echo ${local_html/TABLE_DATA_SECTION/$result_html} > $repo_name.html
-        
+
         component_cell=""
     done
 }
 
 _calculate_summary(){
-    
+
     # Generate Summary Report
     total_search_occurrences_count=0
     total_search_occurrences_file_count=0
     summary_data+="<tr><td>$component_name</td><td>$(printf '%s\n' "${component_repos[@]}" | sed 's/.*/<a href=&.html target="_blank">&<\/a><br \/>/')</td>"
     for keywords in "${SEARCH_KEYWORDS[@]}"
     do
-
-        GREP_EXTRA_ARG="v"
-        for fullsearch_word in "${NEGATION_SEARCH_KEYWORDS[@]}"
-        do
-            if [ "$fullsearch_word" == "$keywords" ] ; then
-                GREP_EXTRA_ARG=""
-            fi
-        done
 
         search_occurrences_count=0
         search_occurrences_file_count=0
@@ -156,35 +165,27 @@ _calculate_summary(){
             search_occurrences_count=$((search_occurrences_count + local_search_occurrences_count))
             search_occurrences_file_count=$((search_occurrences_file_count + local_search_occurrences_file_count))
         done
-        
+
         summary_data+="<td>$search_occurrences_count</td>"
 
         total_search_occurrences_count=$((search_occurrences_count + total_search_occurrences_count))
         total_search_occurrences_file_count=$((search_occurrences_file_count + total_search_occurrences_file_count))
-    
+
     done
-   
+
     summary_total_keyword_count=$((summary_total_keyword_count + total_search_occurrences_count))
     summary_total_file_count=$((summary_total_file_count + total_search_occurrences_file_count))
     summary_data+="<td>$total_search_occurrences_count</td><td>$total_search_occurrences_file_count</td></tr>"
 }
 
 _generate_summary_report(){
-        
+
     # Generate Summary Report
     total_search_occurrences_count=0
     total_search_occurrences_file_count=0
     summary_data+="<tr><td>Total</td><td>-</td>"
     for keywords in "${SEARCH_KEYWORDS[@]}"
     do
-
-        GREP_EXTRA_ARG="v"
-        for fullsearch_word in "${NEGATION_SEARCH_KEYWORDS[@]}"
-        do
-            if [ "$fullsearch_word" == "$keywords" ] ; then
-                GREP_EXTRA_ARG=""
-            fi
-        done
 
         search_occurrences_count=0
         search_occurrences_file_count=0
@@ -199,9 +200,9 @@ _generate_summary_report(){
         done
 
         summary_data+="<td>$search_occurrences_count</td>"
-    
+
     done
-    
+
     summary_data+="<td>$summary_total_keyword_count</td><td>$summary_total_file_count</td></tr>"
 
     summary_data=${SUMMARY_HTML/SUMMARY_DATA_SECTION/$summary_data}
