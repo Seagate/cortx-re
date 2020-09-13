b39c69976f0b72d1e55a0d2080675583ffbf1a27
Swati Magar <swati.magar@seagate.com>
2020-09-13 06:41:11 -0600

    Added double quotes in elif condition for changelog.sh file
    Signed-off-by: Swati Magar <swati.magar@seagate.com>

--- scripts/release_support/changelog.sh
+++ scripts/release_support/changelog.sh
@@ -78,7 +78,7 @@ do
                 if [ $component == cortx-hare ] || [ $component == cortx-sspl ] || [ $component == cortx-ha ] || [ $component == cortx-fs ]; then
                         start_hash=$(grep $component start_build_manifest.txt | head -1 | awk -F['_'] '{print $2}' | cut -d. -f1 |  sed 's/git//g'); echo $start_hash
                         target_hash=$(grep $component target_build_manifest.txt | head -1 | awk -F['_'] '{print $2}' | cut -d. -f1 |  sed 's/git//g'); echo $target_hash
-                elif [ $component == cortx-csm_agent ] || [ $component == cortx-csm-web ]; then
+                elif [ $component == "cortx-csm_agent" ] || [ $component == "cortx-csm-web" ]; then
                         start_hash=$(grep $component start_build_manifest.txt | head -1 | awk -F['_'] '{print $3}' |  cut -d. -f1); echo $start_hash
                         target_hash=$(grep $component target_build_manifest.txt | head -1 | awk -F['_'] '{print $3}' |  cut -d. -f1); echo $target_hash
                 else
