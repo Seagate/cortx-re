ebc4b0603ad49ddaa71e66a755c858290c76c20f
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-10-19 07:34:33 -0600

    Moved kickstart and script to custom-iso
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/custom-iso/custom-iso.sh
+++ scripts/custom-iso/custom-iso.sh
@@ -4,7 +4,7 @@ rm -rf /mnt/custom-iso/bootisoks && mkdir /mnt/custom-iso/bootisoks
 cp -r /mnt/custom-iso/test/* /mnt/custom-iso/bootisoks/
 umount /mnt/custom-iso/test/ && rm /mnt/custom-iso/test/ -rf
 chmod -R u+w /mnt/custom-iso/bootisoks/
-cp /root/kickstart_centos-7.8.2003.cfg /mnt/custom-iso/bootisoks/isolinux/ks.cfg
+cp ./kickstart_centos-7.8.2003.cfg /mnt/custom-iso/bootisoks/isolinux/ks.cfg
 sed -i 's/append\ initrd\=initrd.img/append initrd=initrd.img\ ks\=cdrom:\/ks.cfg/' /mnt/custom-iso/bootisoks/isolinux/isolinux.cfg
 cd /mnt/custom-iso/bootisoks/ && mkisofs -o /mnt/custom-iso/custom-boot.iso -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V "CentOS 7 x86_64" -R -J -v -T isolinux/. .
 implantisomd5 /mnt/custom-iso/custom-boot.iso
