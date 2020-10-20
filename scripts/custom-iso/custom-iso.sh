f90cb801c4f1577f5d151a2b0807dfd706d74281
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-10-20 00:49:30 -0600

    Improved custom-iso.sh and kickstart_centos-7.8.2003.cfg
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/custom-iso/custom-iso.sh
+++ scripts/custom-iso/custom-iso.sh
@@ -1,10 +1,34 @@
-mkdir -p /mnt/custom-iso/test
-mount -o loop /mnt/custom-iso/CentOS-7-x86_64-Minimal-2003.iso /mnt/custom-iso/test
-rm -rf /mnt/custom-iso/bootisoks && mkdir /mnt/custom-iso/bootisoks
-cp -r /mnt/custom-iso/test/* /mnt/custom-iso/bootisoks/
-umount /mnt/custom-iso/test/ && rm /mnt/custom-iso/test/ -rf
-chmod -R u+w /mnt/custom-iso/bootisoks/
-cp ./kickstart_centos-7.8.2003.cfg /mnt/custom-iso/bootisoks/isolinux/ks.cfg
-sed -i 's/append\ initrd\=initrd.img/append initrd=initrd.img\ ks\=cdrom:\/ks.cfg/' /mnt/custom-iso/bootisoks/isolinux/isolinux.cfg
-cd /mnt/custom-iso/bootisoks/ && mkisofs -o /mnt/custom-iso/custom-boot.iso -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V "CentOS 7 x86_64" -R -J -v -T isolinux/. .
-implantisomd5 /mnt/custom-iso/custom-boot.iso
+#!/bin/bash
+
+ISO_PATH="/mnt/custom-iso/"
+ISO_VERSION="CentOS-7-x86_64-Minimal-2003.iso"
+CUSTOM_ISO_VERSION="cortx-custom.iso"
+ISO_MOUNT_PATH="/mnt/custom-iso/test"
+LOCAL_BOOT_PATH="/mnt/custom-iso/bootisoks"
+KICKSTART_FILE="./kickstart_centos-7.8.2003.cfg"
+
+#Mount ISO locally
+mkdir -p $ISO_MOUNT_PATH && mount -o loop $ISO_PATH/$ISO_VERSION $ISO_MOUNT_PATH
+
+#create local folder and copy ISO folders
+rm -rf $LOCAL_FOLDER_PATH && mkdir $LOCAL_FOLDER_PATH
+cp -r $ISO_MOUNT_PATH/* $LOCAL_FOLDER_PATH
+
+#umount ISO and remove folder
+umount $ISO_MOUNT_PATH && rm $ISO_MOUNT_PATH -rf
+
+#set permissions for local folder
+chmod -R u+w $LOCAL_BOOT_PATH
+
+#validate kickstart file
+ksvalidator $KICKSTART_FILE
+
+#copy kickstart file and update isolinux.cfg
+cp $KICKSTART_FILE $LOCAL_BOOT_PATH/isolinux/ks.cfg
+sed -i 's/append\ initrd\=initrd.img/append initrd=initrd.img\ ks\=cdrom:\/ks.cfg/' $LOCAL_BOOT_PATH/isolinux/isolinux.cfg
+
+#generate custom iso file
+cd $LOCAL_BOOT_PATH && mkisofs -o $ISO_PATH/$CUSTOM_ISO_VERSION -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V "CORTX CentOS 7.8.2003 x86_64" -R -J -v -T isolinux/. .
+
+#Add md5sum for custom iso
+implantisomd5 $ISO_PATH/$CUSTOM_ISO_VERSION
