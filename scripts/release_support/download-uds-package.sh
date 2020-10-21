e473448db1ef0b2e1710cc12d7e9fa59e638ccbc
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-10-21 14:17:14 +0530

    Appened / in URL if not present
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/download-uds-package.sh
+++ scripts/release_support/download-uds-package.sh
@@ -7,11 +7,12 @@ UDS_DOWNLOAD_DIR="/root/uds"
 ARTIFACTORY_USER="$1"
 ARTIFACTORY_PASSWORD="$2"
 
+[ ${UDS_DOWNLOAD_URL#"${UDS_DOWNLOAD_URL%?}"} != "/" ] && UDS_DOWNLOAD_URL+="/"
 export UDS_DOWNLOAD_FOLDER=$(echo $UDS_DOWNLOAD_URL | awk -F '/' '{ print $(NF-1) }')
 mkdir -p $UDS_DOWNLOAD_DIR
 pushd $UDS_DOWNLOAD_DIR
 rm -rf ud*.rpm
-/bin/wget -r --no-parent -nd -R index.html --http-user=udx_jenkins_ro --http-password=Testit123 $UDS_DOWNLOAD_URL -P $UDS_DOWNLOAD_FOLDER
+bin/wget -r --no-parent -nd -R index.html --http-user=udx_jenkins_ro --http-password=Testit123 $UDS_DOWNLOAD_URL -P $UDS_DOWNLOAD_FOLDER
 
 popd
 
