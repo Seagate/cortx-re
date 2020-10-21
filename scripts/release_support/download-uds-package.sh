35d0c9b3dbe266f9add4b25ad1cac8ea5cea446f
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-10-21 14:24:47 +0530

    Fixed syntax issue
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/download-uds-package.sh
+++ scripts/release_support/download-uds-package.sh
@@ -12,7 +12,7 @@ export UDS_DOWNLOAD_FOLDER=$(echo $UDS_DOWNLOAD_URL | awk -F '/' '{ print $(NF-1
 mkdir -p $UDS_DOWNLOAD_DIR
 pushd $UDS_DOWNLOAD_DIR
 rm -rf ud*.rpm
-bin/wget -r --no-parent -nd -R index.html --http-user=udx_jenkins_ro --http-password=Testit123 $UDS_DOWNLOAD_URL -P $UDS_DOWNLOAD_FOLDER
+/bin/wget -r --no-parent -nd -R index.html --http-user=udx_jenkins_ro --http-password=Testit123 $UDS_DOWNLOAD_URL -P $UDS_DOWNLOAD_FOLDER
 
 popd
 
