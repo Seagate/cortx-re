fa05b128a5b7a5cf162f3cef550f277bf8ba88a4
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-10-28 11:51:45 +0530

    Fixed Codacy errors
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/download-uds-package.sh
+++ scripts/release_support/download-uds-package.sh
@@ -7,18 +7,19 @@ UDS_DOWNLOAD_DIR="/mnt/bigstorage/releases/cortx/uds_uploads/"
 ARTIFACTORY_PASSWORD="$1"
 COMPONNET_DIR="$3"
 ls -ltr $UDS_DOWNLOAD_DIR
-[ ${UDS_DOWNLOAD_URL#"${UDS_DOWNLOAD_URL%?}"} != "/" ] && UDS_DOWNLOAD_URL+="/"
-export UDS_DOWNLOAD_FOLDER=$(echo $UDS_DOWNLOAD_URL | awk -F '/' '{ print $(NF-1) }')
+[ "${UDS_DOWNLOAD_URL#"${UDS_DOWNLOAD_URL%?}"}" != "/" ] && UDS_DOWNLOAD_URL+="/"
+UDS_DOWNLOAD_FOLDER=$(echo $UDS_DOWNLOAD_URL | awk -F '/' '{ print $(NF-1) }')
 mkdir -p $UDS_DOWNLOAD_DIR
 pushd $UDS_DOWNLOAD_DIR
-	/bin/wget -r --no-parent -nd -nc -R index.html --http-user=udx_jenkins_ro --http-password=$ARTIFACTORY_PASSWORD $UDS_DOWNLOAD_URL -P $UDS_DOWNLOAD_FOLDER
-	echo "Downloaded UDS Packages at $UDS_DOWNLOAD_FOLDER"
+        /bin/wget -r --no-parent -nd -nc -R index.html --http-user=udx_jenkins_ro --http-password="$ARTIFACTORY_PASSWORD" $UDS_DOWNLOAD_URL -P "$UDS_DOWNLOAD_FOLDER"
+        echo "Downloaded UDS Packages at $UDS_DOWNLOAD_FOLDER"
 popd
 
 echo "Linking UDS packages"
-ls -ltr $COMPONNET_DIR/uds/
-pushd $COMPONNET_DIR/uds/
-	rm -f last_successful
-	ls -ltr $UDS_DOWNLOAD_DIR/$UDS_DOWNLOAD_FOLDER
-	ln -s $UDS_DOWNLOAD_DIR/$UDS_DOWNLOAD_FOLDER last_successful
+ls -ltr "$COMPONNET_DIR"/uds/
+pushd "$COMPONNET_DIR"/uds/
+        rm -f last_successful
+        ls -ltr $UDS_DOWNLOAD_DIR/"$UDS_DOWNLOAD_FOLDER"
+        ln -s $UDS_DOWNLOAD_DIR/"$UDS_DOWNLOAD_FOLDER" last_successful
 popd
+
