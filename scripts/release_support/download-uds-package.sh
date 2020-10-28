991d9b936ccd64324b250d9cfdf8696d16a8d0ad
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-10-28 11:20:30 +0530

    Added steps to symlink UDS directory to component specific directory
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/download-uds-package.sh
+++ scripts/release_support/download-uds-package.sh
@@ -2,18 +2,23 @@
 
 set -euf -o pipefail
 
-UDS_DOWNLOAD_URL="$3"
+UDS_DOWNLOAD_URL="$2"
 UDS_DOWNLOAD_DIR="/root/uds"
-ARTIFACTORY_USER="$1"
-ARTIFACTORY_PASSWORD="$2"
-
+ARTIFACTORY_PASSWORD="$1"
+COMPONNET_DIR="$3"
+ls -ltr $UDS_DOWNLOAD_DIR
 [ ${UDS_DOWNLOAD_URL#"${UDS_DOWNLOAD_URL%?}"} != "/" ] && UDS_DOWNLOAD_URL+="/"
 export UDS_DOWNLOAD_FOLDER=$(echo $UDS_DOWNLOAD_URL | awk -F '/' '{ print $(NF-1) }')
 mkdir -p $UDS_DOWNLOAD_DIR
 pushd $UDS_DOWNLOAD_DIR
-rm -rf ud*.rpm
-/bin/wget -r --no-parent -nd -R index.html --http-user=udx_jenkins_ro --http-password=Testit123 $UDS_DOWNLOAD_URL -P $UDS_DOWNLOAD_FOLDER
-
+	/bin/wget -r --no-parent -nd -nc -R index.html --http-user=udx_jenkins_ro --http-password=$ARTIFACTORY_PASSWORD $UDS_DOWNLOAD_URL -P $UDS_DOWNLOAD_FOLDER
+	echo "Downloaded UDS Packages at $UDS_DOWNLOAD_FOLDER"
 popd
 
-
+echo "Linking UDS packages"
+ls -ltr $COMPONNET_DIR/uds/
+pushd $COMPONNET_DIR/uds/
+	rm -f last_successful
+	ls -ltr $UDS_DOWNLOAD_DIR/$UDS_DOWNLOAD_FOLDER
+	ln -s $UDS_DOWNLOAD_DIR/$UDS_DOWNLOAD_FOLDER last_successful
+popd
