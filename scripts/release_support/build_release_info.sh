55ea431211664c03190eb31564fe779280e34cf3
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-10-28 13:42:24 +0530

    EOS-14638 Fixed Codacy errors
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/build_release_info.sh
+++ scripts/release_support/build_release_info.sh
@@ -31,7 +31,7 @@ OS: $(cat /etc/redhat-release | sed -e 's/ $//g' -e 's/^/\"/g' -e 's/$/\"/g')
 DATETIME: $(date +"%d-%b-%Y %H:%M %Z" | sed -e 's/^/\"/g' -e 's/$/\"/g')
 KERNEL: $(ls cortx-motr-[0-9]*.rpm | sed -e  's/.*3/3/g' -e 's/.x86_64.rpm//g' -e 's/^/\"/g' -e 's/$/\"/g')
 COMPONENTS:
-$(ls -1 *.rpm | grep -v $EXCLUDE_PACKAGES |  awk '{ print "    - \""$1"\""}')
+$(ls -1 *.rpm | grep -v "$EXCLUDE_PACKAGES" |  awk '{ print "    - \""$1"\""}')
 EOF
 popd || exit
 
