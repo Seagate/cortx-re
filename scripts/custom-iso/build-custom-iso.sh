82eaea04c9ece3f630f4788b0bf32a6b25f32fc8
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-11-02 05:08:15 -0700

    Imprved build-custom-iso.sh, Added missing packages in additional-packages.txt
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/custom-iso/build-custom-iso.sh
+++ scripts/custom-iso/build-custom-iso.sh
@@ -12,14 +12,14 @@ CORTX_DEPS_PATH="/mnt/bigstorage/releases/cortx/third-party-deps/"
 OS="centos"
 OS_VERSION="centos-7.8.2003"
 
-rm -rf /mnt/custom-iso/custom-packages/ && mkdir -p /mnt/custom-iso/custom-packages/
+rm -rf /mnt/custom-iso/custom-packages/ && mkdir -p /mnt/custom-iso/custom-packages/ 
 
 #Mount ISO locally
+echo -e "-------------------------[ Mount CentOS-7-x86_64-Minimal-2003.iso ]----------------------------------------" 
 mkdir -p $ISO_MOUNT_PATH && mount -o loop $ISO_PATH/$ISO_VERSION $ISO_MOUNT_PATH
 
-
 #Install required packages 
-
+echo -e "-------------------------[ Install required package ]------------------------------------------------------" 
 yum install pykickstart createrepo genisoimage isomd5sum -y
 
 #create local folder and copy ISO folders
@@ -33,9 +33,11 @@ umount $ISO_MOUNT_PATH && rm $ISO_MOUNT_PATH -rf
 chmod -R u+w $LOCAL_BOOT_PATH
 
 #validate kickstart file
+echo -e "-------------------------[ Validate kickstart file ]--------------------------------" 
 ksvalidator $KICKSTART_FILE
 
 #copy kickstart file and update isolinux.cfg
+echo -e "-------------------------[ update isolinux.cfg ]------------------------------------------------------------" 
 cp $KICKSTART_FILE $LOCAL_BOOT_PATH/isolinux/ks.cfg
 sed -i 's/append\ initrd\=initrd.img/append initrd=initrd.img\ ks\=cdrom:\/ks.cfg/' $LOCAL_BOOT_PATH/isolinux/isolinux.cfg
 
@@ -45,11 +47,12 @@ sed -i 's/append\ initrd\=initrd.img/append initrd=initrd.img\ ks\=cdrom:\/ks.cf
 #Downloading additional packages. Need to optimize
 while read -r line
 do
-echo "Downloading $line package along with dependencies" 
+echo -e "-------------------------[ Downloading $line package along with dependencies ]--------------------------------" 
 yumdownloader --installroot=/mnt/custom-iso/$line-install-root  --destdir=/mnt/custom-iso/custom-packages --resolve $line
 rm -rf /mnt/custom-iso/$line-install-root
 cp -n /mnt/custom-iso/custom-packages/* $LOCAL_BOOT_PATH/Packages/.
 ls -ltr $LOCAL_BOOT_PATH/Packages/$line-*
+echo -e "----------------------------------------------[ Done ]---------------------------------------------------------" 
 done < additional-packages.txt
 
 #Copy files. 
@@ -61,7 +64,6 @@ ls -ltr $LOCAL_BOOT_PATH/Packages/$line*
 done < custom-packages.txt
 
 #Copy Mellanox Packages
-
 cp $CORTX_DEPS_PATH/$OS/$OS_VERSION/performance/linux.mellanox.com/public/repo/mlnx_ofed/4.9-0.1.7.0/rhel7.8/x86_64/MLNX_LIBS/*.rpm $LOCAL_BOOT_PATH/Packages/
 
 pushd $LOCAL_BOOT_PATH
@@ -77,3 +79,10 @@ cd $LOCAL_BOOT_PATH && mkisofs -o $ISO_PATH/$CUSTOM_ISO_VERSION -b isolinux.bin 
 
 #Add md5sum for custom iso
 implantisomd5 $ISO_PATH/$CUSTOM_ISO_VERSION
+
+#print ISO location
+echo -e "---------------------------------[ Custom ISO locations ]------------------------------------------------------" 
+ls -lh /mnt/custom-iso/cortx-custom.iso
+md5sum /mnt/custom-iso/cortx-custom.iso
+echo -e "-----------------------------------------------[  Done  ]------------------------------------------------------" 
+echo "ISO is available at /mnt/custom-iso/cortx-custom.iso"
