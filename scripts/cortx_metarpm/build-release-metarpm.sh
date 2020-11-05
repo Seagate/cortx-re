e69f757c4a2365d1665e474d9857909d8450c50d
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2020-11-05 12:09:25 -0700

    fix: error handling in metarpm script
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- scripts/cortx_metarpm/build-release-metarpm.sh
+++ scripts/cortx_metarpm/build-release-metarpm.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-set -x
+
+release_folder=$1
+OS_version=$2
+build_num=$3
 release_type="CORTX-1.0.0"
+
+if [ -z $release_folder ] && [ -z $OS_version ] && [ -z $build_num ]; then
+   echo "Usage: build-release-metarpm.sh <release folder> <OS name> <release build number>"
+   echo "e.g. build-release-metarpm.sh cortx-1.0 centos-7.7.1908 3"
+   exit 1
+fi
+
 pushd  scripts/cortx_metarpm/
 metainfo=$(curl "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/" | sed -n "/rpm/p" | cut -d'"' -f 2 | tr ' ' '\n')
 rpm --import "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/RPM-GPG-KEY-Seagate"
