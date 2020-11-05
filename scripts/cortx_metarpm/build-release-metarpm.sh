53efa7d460066d6fadf735fcd2959299c0ae05fd
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2020-11-05 04:00:39 -0700

    refactor: metarpm script
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- scripts/cortx_metarpm/build-release-metarpm.sh
+++ scripts/cortx_metarpm/build-release-metarpm.sh
@@ -1,21 +1,21 @@
+#!/bin/bash
 set -x
 release_type="CORTX-1.0.0"
 pushd  scripts/cortx_metarpm/
-`curl http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/ | sed -n "/rpm/p" | cut -d'"' -f 2 > metainfo.txt`
-rpm --import http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/RPM-GPG-KEY-Seagate
+metainfo=$(curl "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/" | sed -n "/rpm/p" | cut -d'"' -f 2 | tr ' ' '\n')
+rpm --import "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/RPM-GPG-KEY-Seagate"
 rm -rf dependencies.txt
-for i in $(cat metainfo.txt)
+for i in $metainfo
 do
-rpm_name=$(rpm -qp http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/$i --qf '%{NAME}')
-rpm_version=$(rpm -qp http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/$i --qf '%{VERSION}')
-rpm_release=$(rpm -qp http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/$i --qf '%{RELEASE}')
-echo $rpm_name = $rpm_version-$rpm_release >> dependencies.txt
+rpm_name=$(rpm -qp "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/$i" --qf '%{NAME}')
+rpm_version=$(rpm -qp "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/$i" --qf '%{VERSION}')
+rpm_release=$(rpm -qp "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/$i" --qf '%{RELEASE}')
+echo "$rpm_name" = "$rpm_version"-"$rpm_release" >> dependencies.txt
 done
-builddep=$(cat dependencies.txt | tr '\n' ' ')
+builddep=$( < dependencies.txt tr '\n' ' ')
 sed -i "s/release-name/$release_type/g" cortx.spec
 sed -i "s/builddep/$builddep/g" cortx.spec
 relversion=$build_num
 sed -i "s/release-version/$relversion/" cortx.spec
-cat cortx.spec
 rpmbuild -bb cortx.spec
 rpm_location=/root/rpmbuild/RPMS/x86_64/$release_type-$relversion-1.x86_64.rpm
