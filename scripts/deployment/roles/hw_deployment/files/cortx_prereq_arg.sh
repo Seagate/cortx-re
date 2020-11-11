2c730c9731d289159b1fa54657253672ca11764f
Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>
2020-11-11 14:53:11 +0530

    Subscription manager check centos disable
    Signed-off-by: Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>

--- scripts/deployment/roles/hw_deployment/files/cortx_prereq_arg.sh
+++ scripts/deployment/roles/hw_deployment/files/cortx_prereq_arg.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
-SUB_LIST=$(subscription-manager list | grep Status: | awk '{ print $2 }' && subscription-manager status | grep 'Overall Status:' | awk '{ print $3 }')
-SUB_REPO=$(subscription-manager repos --list | grep rhel-ha-for-rhel-7-server-rpms)
 
 OS_FAMILY=$(cat /etc/redhat-release)
 
-if [[ "${OS_FAMILY}" == *"Red Hat"* ]] && [[ "${SUB_LIST}" == *"Subscribed"* ]] && [[ "${SUB_LIST}" == *"Current"* ]] && [[ "${SUB_REPO}" == *" rhel-ha-for-rhel-7-server-rpms"* ]];then	
-	echo ""
-elif [[ "${OS_FAMILY}" != *"Red Hat"* ]]; then
-	echo ""
+if [[ "${OS_FAMILY}" == *"Red Hat"* ]]; then
+	
+	SUB_LIST=$(subscription-manager list | grep Status: | awk '{ print $2 }' && subscription-manager status | grep 'Overall Status:' | awk '{ print $3 }')
+	SUB_REPO=$(subscription-manager repos --list | grep rhel-ha-for-rhel-7-server-rpms)
+	
+	if [[ "${OS_FAMILY}" == *"Red Hat"* ]] && [[ "${SUB_LIST}" == *"Subscribed"* ]] && [[ "${SUB_LIST}" == *"Current"* ]] && [[ "${SUB_REPO}" == *" rhel-ha-for-rhel-7-server-rpms"* ]];then	
+		echo ""
+	else
+		echo "--disable-sub-mgr"
+	fi
 else
-	echo "--disable-sub-mgr"
+	echo ""
 fi
