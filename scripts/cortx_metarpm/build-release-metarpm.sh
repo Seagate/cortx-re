6fda2d3ec3fbdb4899fb390466e4f502a73bd018
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2020-11-11 03:09:49 -0700

    refactor: edit script
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- scripts/cortx_metarpm/build-release-metarpm.sh
+++ scripts/cortx_metarpm/build-release-metarpm.sh
@@ -15,6 +15,7 @@ pushd  scripts/cortx_metarpm/
 metainfo=$(curl "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/" | sed -n "/rpm/p" | cut -d'"' -f 2 | tr ' ' '\n')
 rpm --import "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/RPM-GPG-KEY-Seagate"
 rm -rf dependencies.txt
+
 for i in $metainfo
 do
 rpm_name=$(rpm -qp "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/$i" --qf '%{NAME}')
@@ -22,6 +23,7 @@ rpm_version=$(rpm -qp "http://cortx-storage.colo.seagate.com/releases/eos/github
 rpm_release=$(rpm -qp "http://cortx-storage.colo.seagate.com/releases/eos/github/$release_folder/$OS_version/$build_num/prod/$i" --qf '%{RELEASE}')
 echo "$rpm_name" = "$rpm_version"-"$rpm_release" >> dependencies.txt
 done
+
 builddep=$( < dependencies.txt tr '\n' ' ')
 sed -i "s/release-name/$release_type/g" cortx.spec
 sed -i "s/builddep/$builddep/g" cortx.spec
