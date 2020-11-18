5b49a16e9f9a168a1fb4cabf86fc1e278a0c8281
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2020-11-18 03:55:38 -0700

    refactor: metarpm script for rpm versioning
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- scripts/cortx_metarpm/build-release-metarpm.sh
+++ scripts/cortx_metarpm/build-release-metarpm.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 release_folder=$1
-OS_version=$2
+os_version=$2
 build_num=$3
-release_type="CORTX-1.0.0"
-build_location="http://cortx-storage.colo.seagate.com/releases/cortx/github/$release_folder/$OS_version/$build_num/prod"
+release_type="CORTX"
+build_location="http://cortx-storage.colo.seagate.com/releases/cortx/github/$release_folder/$os_version/$build_num/prod"
 
-if [ -z "$release_folder" ] && [ -z "$OS_version" ] && [ -z "$build_num" ]; then
+if [ -z "$release_folder" ] && [ -z "$os_version" ] && [ -z "$build_num" ]; then
    echo "Usage: build-release-metarpm.sh <release folder> <OS name> <release build number>"
    echo "e.g. build-release-metarpm.sh cortx-1.0 centos-7.7.1908 3"
    exit 1
 fi
 
-pushd  scripts/cortx_metarpm/
 metainfo=$(curl "$build_location/" | sed -n "/rpm/p" | cut -d'"' -f 2 | tr ' ' '\n')
 rpm --import "$build_location/RPM-GPG-KEY-Seagate"
 rm -rf dependencies.txt
@@ -29,5 +28,5 @@ builddep=$( < dependencies.txt tr '\n' ' ')
 sed -i "s/release-name/$release_type/g" cortx.spec
 sed -i "s/builddep/$builddep/g" cortx.spec
 relversion=$build_num
-sed -i "s/release-version/$relversion/" cortx.spec
+sed -i "s/build-number/$relversion/" cortx.spec
 rpmbuild -bb cortx.spec
