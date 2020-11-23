99e8fd218a4ed0d2d7c9ea505399e4fd676f496a
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-11-23 16:50:42 +0530

    Added BUILD_NUMBER details, Added license text
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/build-3rdParty-release-info.sh
+++ scripts/release_support/build-3rdParty-release-info.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
 BUILD_LOCATION=$1
 
 if [ -z "$BUILD_LOCATION" ]; then
@@ -15,6 +34,7 @@ cat <<EOF > THIRD_PARTY_RELEASE.INFO
 ---
 NAME: "CORTX"
 VERSION: "1.0.0"
+BUILD: $(echo "$BUILD_NUMBER" | sed -e 's/^/\"/g' -e 's/$/\"/g')
 OS: $(cat /etc/redhat-release | sed -e 's/ $//g' -e 's/^/\"/g' -e 's/$/\"/g')
 DATETIME: $(date +"%d-%b-%Y %H:%M %Z" | sed -e 's/^/\"/g' -e 's/$/\"/g')
 THIRD_PARTY_COMPONENTS:
