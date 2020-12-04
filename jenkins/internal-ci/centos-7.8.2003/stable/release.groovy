413855acfb8f28820cbc87597f323e19c159320b
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2020-12-04 14:03:39 +0530

    Fix build failure
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/internal-ci/centos-7.8.2003/stable/release.groovy
+++ jenkins/internal-ci/centos-7.8.2003/stable/release.groovy
@@ -226,19 +226,16 @@ pipeline {
 				
 				genisoimage -input-charset iso8859-1 -f -J -joliet-long -r -allow-lowercase -allow-multidot -publisher Seagate -o cortx-$version-$BUILD_NUMBER-single.iso $cortx_build_dir/$release_tag
 				
-                mkdir $cortx_os_iso $integration_dir/$release_tag/prod
+                mkdir $integration_dir/$release_tag/prod
                 ln -s $cortx_os_iso $integration_dir/$release_tag/prod/$(basename $cortx_os_iso)
 
                 cortx_prvsnr_preq=$(ls "$cortx_build_dir/$release_tag/cortx_iso" | grep "python36-cortx-prvsnr" | cut -d- -f5 | cut -d_ -f2 | cut -d. -f1 | sed s/"git"//)
                 # cortx_prep.sh is not available in stable branch yet
                 #wget -O $cortx_build_dir/$release_tag/iso/cortx-prep-$version-$BUILD_NUMBER.sh https://raw.githubusercontent.com/Seagate/cortx-prvsnr/$cortx_prvsnr_preq/cli/src/cortx_prep.sh
-
-
-            
+           
                 mkdir -p $integration_dir/$release_tag/prod/
 				cp cortx-$version-$BUILD_NUMBER-single.iso $integration_dir/$release_tag/prod/
 				cp cortx-$version-$BUILD_NUMBER.iso $integration_dir/$release_tag/prod/
-
 				
 				ls $cortx_build_dir/$release_tag/3rd_party/THIRD_PARTY_RELEASE.INFO
 				ls $cortx_build_dir/$release_tag/cortx_iso/RELEASE.INFO
