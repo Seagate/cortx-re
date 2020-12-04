6fabb165a33e42e902bb7efe3a6c98ddaae3a4ac
Balaji Ramachandran <64014432+balajiramachandran-seagate@users.noreply.github.com>
2020-12-04 12:19:56 +0530

    Create thresholdalert.groovy
    Signed-off-by: Balaji Ramachandran <balaji.ramachandran@seagate.com>

---
+++ jenkins/automation/thresholdalert.groovy
@@ -0,0 +1,80 @@
+pipeline {
+
+	agent {
+		node {
+			label 'Test-node-ssc-vm-c-456'
+		}
+	}
+	 triggers {
+        cron('H/5 * * * *')
+    }
+	stages {
+		stage ('Check Mount') {
+		    steps {
+				
+				sh label: 'Check Mount',script:'''#!/bin/bash
+		        mount="cortx-storage.colo.seagate.com:/mnt/data1/releases"
+				if grep -qs "$mount" /proc/mounts; then
+				echo "cortx-storage.colo.seagate.com:/mnt/data1/releases is mounted."
+				else
+				echo "cortx-storage.colo.seagate.com:/mnt/data1/releases is not mounted."
+				mount -t nfs4 "$mount" /mnt/data1/releases
+					if [ $? -eq 0 ]; then
+					echo "Mount success!"
+					else
+					echo "Something went wrong with the mount..."
+					fi
+				fi
+			'''	
+		}
+	}
+		
+		stage ('Threshold alert') {
+					steps {
+				
+						sh label: 'Threshold alert',script:'''#!/bin/bash
+						CURRENT=$(df -h | grep /mnt/data1/releases | awk '{ print $5}' | sed 's/%//g')
+						THRESHOLD=75
+						if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
+						echo Your /mnt/data1/releases partition remaining free space is critically low. Used: $CURRENT%. Threshold: $THRESHOLD%  So, 30 days older files will be deleted $(date)
+						prev_count=0
+						fpath=/mnt/data1/releases
+						find $fpath -type f -mtime +30  -exec ls -ltr {} + > /mnt/data1/releases/file.out
+						###find $fpath -type f -mtime +30  -exec rm -rf {} \\;
+						count=$(cat /mnt/data1/releases/file.out | wc -l)
+						
+							if [ "$prev_count" -lt "$count" ] ; then
+							MESSAGE="/mnt/data1/releases/file1.out"
+							TO="balaji.ramachandran@seagate.com"
+							echo "Files older than 30 days are listed" >> $MESSAGE
+							echo "+--------------------------------------------- +" >> $MESSAGE
+					    	echo "" >> $MESSAGE
+							cat /mnt/data1/releases/file.out | awk '{print $6,$7,$9}' >> $MESSAGE
+							echo "" >> $MESSAGE
+							#SUBJECT="WARNING: Your /mnt/data1/releases partition remaining free space is critically low. Used: $CURRENT%.  So, 50 days older files have been deleted $(date)"
+							#mailx -s "$SUBJECT" "$TO" < $MESSAGE
+							cat $MESSAGE
+							rm $MESSAGE /mnt/data1/releases/file.out
+				
+							fi
+						fi
+					'''
+				}
+			}
+		}
+	post {
+		always {
+			script {
+			        emailext (
+					body: '''${SCRIPT, template="release-email.template"}''',
+					mimeType: 'text/html',
+					subject: "[Jenkins Build ${currentBuild.currentResult}] : ${env.JOB_NAME}",
+					attachLog: true,
+					to: ('priyank.p.dalal@seagate.com,balaji.ramachandran@seagate.com,shailesh.vaidya@seagate.com,mukul.malhotra@seagate.com'),
+					)
+			}
+		}
+	}
+    
+}
+	
