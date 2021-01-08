8c9fea31f00d9b8d466d02358c35c6789ed0fa65
Balaji Ramachandran <64014432+balajiramachandran-seagate@users.noreply.github.com>
2021-01-08 10:36:56 +0530

    Update buildretention.groovy

--- jenkins/automation/buildretention.groovy
+++ jenkins/automation/buildretention.groovy
@@ -11,19 +11,6 @@ pipeline {
 	parameters {
 		string(name: 'RELEASE_INFO_URL', defaultValue: 'http://cortx-storage.colo.seagate.com/releases/cortx_builds/centos-7.8.2003/552/RELEASE.INFO', description: 'RELEASE BUILD')
 	}
-	pipeline {
-
-	agent {
-		node {
-			label 'Test-node-ssc-vm-c-456'
-		}
-	}
-	 triggers {
-         cron('0 */6 * * *')
-    }
-	parameters {
-		string(name: 'RELEASE_INFO_URL', defaultValue: 'http://cortx-storage.colo.seagate.com/releases/cortx_builds/centos-7.8.2003/552/RELEASE.INFO', description: 'RELEASE BUILD')
-	}
 	stage ('commit hash') {
 			steps {
 				sh label: 'commit hash', script: '''#!/bin/bash
