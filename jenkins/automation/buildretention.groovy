03e2101113465c09ad7bb000b39b0650d7d29068
Balaji Ramachandran <64014432+balajiramachandran-seagate@users.noreply.github.com>
2021-01-08 10:34:40 +0530

    Update buildretention.groovy

--- jenkins/automation/buildretention.groovy
+++ jenkins/automation/buildretention.groovy
@@ -11,4 +11,57 @@ pipeline {
 	parameters {
 		string(name: 'RELEASE_INFO_URL', defaultValue: 'http://cortx-storage.colo.seagate.com/releases/cortx_builds/centos-7.8.2003/552/RELEASE.INFO', description: 'RELEASE BUILD')
 	}
-}
+	pipeline {
+
+	agent {
+		node {
+			label 'Test-node-ssc-vm-c-456'
+		}
+	}
+	 triggers {
+         cron('0 */6 * * *')
+    }
+	parameters {
+		string(name: 'RELEASE_INFO_URL', defaultValue: 'http://cortx-storage.colo.seagate.com/releases/cortx_builds/centos-7.8.2003/552/RELEASE.INFO', description: 'RELEASE BUILD')
+	}
+	stage ('commit hash') {
+			steps {
+				sh label: 'commit hash', script: '''#!/bin/bash
+				RELEASE_INFO=${params.RELEASE_INFO_URL}
+				GITHUB_ORG="Seagate"
+				declare -A COMPONENT_LIST=( 
+				[cortx-s3server]="https://${GIT_CRED}@github.com/${GITHUB_ORG}/cortx-s3server.git"
+				[cortx-motr]="https://${GIT_CRED}@github.com/${GITHUB_ORG}/cortx-motr.git"
+				[cortx-hare]="https://${GIT_CRED}@github.com/${GITHUB_ORG}/cortx-hare.git"
+				[cortx-ha]="https://${GIT_CRED}@github.com/${GITHUB_ORG}/cortx-ha.git"
+				[cortx-prvsnr]="https://${GIT_CRED}@github.com/${GITHUB_ORG}/cortx-prvsnr.git"
+				[cortx-sspl]="https://${GIT_CRED}@github.com/${GITHUB_ORG}/cortx-monitor.git"
+				[cortx-csm_agent]="https://${GIT_CRED}@github.com/${GITHUB_ORG}/cortx-manager.git"
+				[cortx-csm_web]="https://${GIT_CRED}@github.com/${GITHUB_ORG}/cortx-management-portal.git"
+				#[cortx-fs]="https://$GIT_CRED@github.com/$GITHUB_ORG/cortx-posix.git"
+			)
+				git config --global user.email "cortx-re@seagate.com"
+				git config --global user.name "cortx-re"
+				wget -q "$RELEASE_INFO" -O RELEASE.INFO
+				for component in "${!COMPONENT_LIST[@]}"
+					do
+						dir=$(echo "${COMPONENT_LIST[$component]}" |  awk -F'/' '{print $NF}')
+						git clone --quiet "${COMPONENT_LIST[$component]}" "$dir" > /dev/null
+						rc=$?
+						if [ $rc -ne 0 ]; then 
+							echo "ERROR:git clone failed for $component"
+							exit 1
+						fi
+						if [ "$component" == cortx-hare ] || [ "$component" == cortx-sspl ] || [ "$component" == cortx-ha ] || [ "$component" == cortx-fs ]; then
+							COMMIT_HASH=$(grep "$component" RELEASE.INFO | head -1 | awk -F['_'] '{print $2}' | cut -d. -f1 |  sed 's/git//g');
+						elif [ "$component" == "cortx-csm_agent" ] || [ "$component" == "cortx-csm_web" ]; then
+							COMMIT_HASH=$(grep "$component" RELEASE.INFO | head -1 | awk -F['_'] '{print $3}' |  cut -d. -f1);
+						else
+							COMMIT_HASH=$(grep "$component" RELEASE.INFO | head -1 | awk -F['_'] '{print $2}' | sed 's/git//g');
+						fi
+						echo "Component: $component , Repo:  ${COMPONENT_LIST[$component]}, Commit Hash: ${COMMIT_HASH}"
+					done
+				'''	
+			}		
+		}	
+	}		
