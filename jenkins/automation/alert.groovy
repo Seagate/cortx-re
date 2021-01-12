9fdf6f9ae64d47df14ebf27d32f3f22a9449e5f7
Balaji Ramachandran <64014432+balajiramachandran-seagate@users.noreply.github.com>
2021-01-12 16:00:26 +0530

    Update alert.groovy

--- jenkins/automation/alert.groovy
+++ jenkins/automation/alert.groovy
@@ -45,7 +45,7 @@ pipeline {
 						#touch /mnt/data1/releases/file.out
 						#find $fpath -type f -mtime +30  -exec ls -ltr {} + > /mnt/data1/releases/file.out
 						find $fpath -maxdepth 1 ! -type l -print | cut -c3- | grep -v "\\#" &&  find /mnt/data1/releases/cortx/github/release/rhel-7.7.1908/2750 -path /mnt/data1/releases/cortx/github/release/rhel-7.7.1908 -prune -false -o -name '*' && find $fpath ! -name '*.INFO*' && find $fpath -type f -mtime +30  -exec ls -ltr {} + > /mnt/data1/releases/file1.out
-						find $fpath -maxdepth 1 -type l -print | cut -c3- | grep -v "\\#" &&  find /mnt/data1/releases/cortx/github/release/rhel-7.7.1908/2750 -path /mnt/data1/releases/cortx/github/release/rhel-7.7.1908 -prune -false -o -name '*' && find $fpath -name '*.INFO*' && find $fpath -type f -mtime +30  -exec cp {} /mnt/data1/releases/ \\;
+						#find $fpath -maxdepth 1 -type l -print | cut -c3- | grep -v "\\#" &&  find /mnt/data1/releases/cortx/github/release/rhel-7.7.1908/2750 -path /mnt/data1/releases/cortx/github/release/rhel-7.7.1908 -prune -false -o -name '*' && find $fpath -name '*.INFO*' && find $fpath -type f -mtime +30  -exec cp {} /mnt/data1/releases/backups/cortx_build_backup/custom_build_backup \\;
 						#find $fpath -maxdepth 1 -type l -print | cut -c3- | grep -v "\\#" && find $fpath -name '*.INFO*' && find $fpath -type f -mtime +30  -exec ls -ltr {} + > /mnt/data1/releases/file1.out
 						#find $fpath -maxdepth 1 ! -type l -print | cut -c3- | grep -v "\\#" && find $fpath ! -name '*.INFO*' && find $fpath -type f -mtime +30  -exec rm -rf {} \\;
 						count=$(cat /mnt/data1/releases/file.out | wc -l)
@@ -74,7 +74,7 @@ pipeline {
 					body: "Current Disk Space is ${env.SPACE}",
 					subject: "[Jenkins Build ${currentBuild.currentResult}] : ${env.JOB_NAME}",
 					attachLog: true,
-					to: 'balaji.ramachandran@seagate.com',
+					to: ('priyank.p.dalal@seagate.com,balaji.ramachandran@seagate.com,shailesh.vaidya@seagate.com,mukul.malhotra@seagate.com'),
 					)
 			}
 		}
