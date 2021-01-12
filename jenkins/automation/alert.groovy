71161ccf997e4ed4947467988a4805d24c7dea28
Balaji Ramachandran <64014432+balajiramachandran-seagate@users.noreply.github.com>
2021-01-12 18:46:18 +0530

    Update alert.groovy

--- jenkins/automation/alert.groovy
+++ jenkins/automation/alert.groovy
@@ -72,7 +72,6 @@ pipeline {
 		always {
 			script {
 			        emailext (
-					attachmentsPattern: file1.out,
 					body: "${Current Disk Space is ${env.SPACE} currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
 					subject: "[Jenkins Build ${currentBuild.currentResult}] : ${env.JOB_NAME}",
 					to: 'balaji.ramachandran@seagate.com',
