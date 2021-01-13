7ce61282e6776481ab2ac290407f579090ee2e9f
Balaji Ramachandran <64014432+balajiramachandran-seagate@users.noreply.github.com>
2021-01-13 14:18:16 +0530

    Update alert.groovy

--- jenkins/automation/alert.groovy
+++ jenkins/automation/alert.groovy
@@ -42,11 +42,13 @@ pipeline {
 						echo Your /mnt/data1/releases partition remaining free space is critically low. Used: $CURRENT%. Threshold: $THRESHOLD%  So, 30 days older files will be deleted $(date)
 						prev_count=0
 						fpath=/mnt/data1/releases
+						source /mnt/data1/releases/exclude_build.txt
+						find $build -path $path -prune -false -o -name '*' -exec ls -ltr {} + > /mnt/data1/releases/file1.out
 						#touch /mnt/data1/releases/file.out
 						#find $fpath -type f -mtime +30  -exec ls -ltr {} + > /mnt/data1/releases/file.out
 						#find $fpath -maxdepth 1 ! -type l -print | cut -c1- | grep -v "\\#" -exec ls -ltr {} + > /mnt/data1/releases/file1.out
-						find $fpath -maxdepth 1 ! -type l -print | cut -c1- | grep -v "\\#" &&  find /mnt/data1/releases/cortx/github/release/rhel-7.7.1908/2750 -path /mnt/data1/releases/cortx/github/release/rhel-7.7.1908 -prune -false -o -name '*' && find $fpath ! -name '*.INFO*' && find $fpath -type f -mtime +30  -exec ls -ltr {} + > /mnt/data1/releases/file1.out
-						#find $fpath -maxdepth 1 -type l -print | cut -c1- | grep -v "\\#" &&  find /mnt/data1/releases/cortx/github/release/rhel-7.7.1908/2750 -path /mnt/data1/releases/cortx/github/release/rhel-7.7.1908 -prune -false -o -name '*' && find $fpath -name '*.INFO*' && find $fpath -type f -mtime +30  -exec cp {} /mnt/data1/releases/backups/cortx_build_backup/custom_build_backup \\;
+						##find $fpath -maxdepth 1 ! -type l -print | cut -c1- | grep -v "\\#" &&  find /mnt/data1/releases/cortx/github/release/rhel-7.7.1908/2750 -path /mnt/data1/releases/cortx/github/release/rhel-7.7.1908 -prune -false -o -name '*' && find $fpath ! -name '*.INFO*' && find $fpath -type f -mtime +30  -exec ls -ltr {} + > /mnt/data1/releases/file1.out
+						#find $fpath -maxdepth 1 -type l -print | cut -c1- | grep -v "\\#" &&  find /mnt/data1/releases/cortx/github/release/rhel-7.7.1908/2750 -path /mnt/data1/releases/cortx/github/release/rhel-7.7.1908 -prune -false -o -name '*' && find $fpath -name '*.INFO*' && find $fpath -type f -mtime -30  -exec cp {} /mnt/data1/releases/backups/cortx_build_backup/custom_build_backup \\;
 						#find $fpath -maxdepth 1 -type l -print | cut -c1- | grep -v "\\#" && find $fpath -name '*.INFO*' && find $fpath -type f -mtime +30  -exec ls -ltr {} + > /mnt/data1/releases/file1.out
 						#find $fpath -maxdepth 1 ! -type l -print | cut -c1- | grep -v "\\#" && find $fpath ! -name '*.INFO*' && find $fpath -type f -mtime +30  -exec rm -rf {} \\;
 						count=$(cat /mnt/data1/releases/file1.out | wc -l)
