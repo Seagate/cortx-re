9ecf0ed341e335866b0121776251cf4b34b264c1
Balaji Ramachandran <64014432+balajiramachandran-seagate@users.noreply.github.com>
2021-01-22 10:42:52 +0530

    Update buildretention.groovy

--- jenkins/automation/buildretention.groovy
+++ jenkins/automation/buildretention.groovy
@@ -28,8 +28,8 @@ pipeline {
 	stage ("THIRD PARTY RELEASE VERSION") {
 			steps {	
 				sh label: 'commit hash', script: '''#!/bin/bash
-				THIRD_PARTY_RELEASE_INFO=$(wget $THIRD_PARTY_RELEASE_INFO_URL)
-				THIRD_PARTY_VERSION=$(cat $THIRD_PARTY_RELEASE_INFO | grep THIRD_PARTY_VERSION | awk '{print $2}' | cut -b 18-24)
+				THIRD_PARTY_RELEASE_INFO=wget $THIRD_PARTY_RELEASE_INFO_URL
+				THIRD_PARTY_VERSION=cat $THIRD_PARTY_RELEASE_INFO | grep THIRD_PARTY_VERSION | awk '{print $2}' | cut -b 18-24
 				echo "THIRD_PARTY_VERSION = $THIRD_PARTY_VERSION"
 			'''
 			}
