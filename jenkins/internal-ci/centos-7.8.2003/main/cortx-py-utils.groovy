1e13659702d07bfda4e9992042ff10aac967a374
pujamudaliar <puja.mudaliar@seagate.com>
2021-02-11 00:00:25 +0530

    Update cortx-py-utils.groovy
    Signed-off-by: root <root@ssc-vm-2455.colo.seagate.com>

--- jenkins/internal-ci/centos-7.8.2003/main/cortx-py-utils.groovy
+++ jenkins/internal-ci/centos-7.8.2003/main/cortx-py-utils.groovy
@@ -105,7 +105,7 @@ pipeline {
 						jiraIssues.each { issue ->
 							def author =  getAuthor(issue)
 							jiraAddComment(
-								idOrKey: issite: "SEAGATE_JIRA",
+								idOrKey: issue,
 								site: "SEAGATE_JIRA",
 								comment: "{panel:bgColor=#c1c7d0}"+
 									"h2. ${component} - ${branch} branch build pipeline SUCCESS\n"+
