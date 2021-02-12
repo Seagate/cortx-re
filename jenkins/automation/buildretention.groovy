8a083e7799331e339dea20ca91a12e69de860529
Balaji Ramachandran <balaji.ramachandran@seagate.com>
2021-02-12 06:49:31 -0700

    Signedoff buildretention.groovy
    Signed-off-by: Balaji Ramachandran <balaji.ramachandran@seagate.com>

--- jenkins/automation/buildretention.groovy
+++ jenkins/automation/buildretention.groovy
@@ -45,7 +45,7 @@ pipeline {
              when { expression { true } }
             steps {
                 script { build_stage=env.STAGE_NAME }
-                build job: 'custom-ci-build', wait: true,
+                build job: 'cortx-custom-ci', wait: true,
                 parameters: [
 		    string(name: 'THIRD_PARTY_RELEASE_VERSION', value: "${THIRD_PARTY_RELEASE_VERSION}"),
                     string(name: 'CSM_AGENT_BRANCH', value: "${COMMIT_HASH_CORTX_CSM_AGENT}"),
