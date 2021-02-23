f76b67b8b110bf04ca5c59d7a7c205551578dfad
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-02-23 10:20:51 +0530

    Use yum-conifg-manager interfaces instead of shell commands.
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.8.2003/hare.groovy
+++ jenkins/custom-ci/centos-7.8.2003/hare.groovy
@@ -50,12 +50,15 @@ pipeline {
 		stage('Install Dependencies') {
 			steps {
 				script { build_stage = env.STAGE_NAME }
-				sh label: '', script: '''
+				sh label: 'Configure yum repositories', script: '''
+					set +x
 					yum-config-manager --disable cortx-C7.7.1908 motr_current_build
 					yum-config-manager --add-repo=http://cortx-storage.colo.seagate.com/releases/cortx/github/integration-custom-ci/$os_version/$release_tag/cortx_iso/
 					yum-config-manager --save --setopt=cortx-storage*.gpgcheck=1 cortx-storage* && yum-config-manager --save --setopt=cortx-storage*.gpgcheck=0 cortx-storage*
 					yum clean all;rm -rf /var/cache/yum
-					
+				'''	
+
+				sh label: 'Install packages', script: '''	
 					if [ "${HARE_BRANCH}" == "Cortx-v1.0.0_Beta" ]; then
 						yum install eos-core{,-devel} -y
 					else
