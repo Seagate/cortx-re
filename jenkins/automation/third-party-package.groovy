843c86187b81495b54721f96e922ebd4657eb7a2
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-02-25 19:17:21 +0530

    Upload package to remote location.
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/automation/third-party-package.groovy
+++ jenkins/automation/third-party-package.groovy
@@ -8,6 +8,7 @@ pipeline {
 	
 	environment {
         version = "2.0.0"
+        build_upload_dir = "/mnt/bigstorage/releases/cortx/third-party-deps/rpm"
     }
 
     options {
@@ -34,5 +35,31 @@ pipeline {
                 """
             }
         }
+
+        stage ('Upload') {
+			steps {
+				script { build_stage = env.STAGE_NAME }
+				sh label: 'Copy RPMS', script: '''
+                    mkdir -p $build_upload_dir/$BUILD_NUMBER
+                    cp /root/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir/$BUILD_NUMBER
+				'''
+                sh label: 'Repo Creation', script: '''pushd $build_upload_dir/$BUILD_NUMBER
+                    rpm -qi createrepo || yum install -y createrepo
+                    createrepo .
+                    popd
+				'''
+			}
+		}
+			
+		stage ('Tag last_successful') {
+			steps {
+				script { build_stage = env.STAGE_NAME }
+				sh label: 'Tag last_successful', script: '''pushd $build_upload_dir/
+                    test -d $build_upload_dir/last_successful && rm -f last_successful
+                    ln -s $build_upload_dir/$BUILD_NUMBER last_successful
+                    popd
+				'''
+			}
+		}
 	}
 }
