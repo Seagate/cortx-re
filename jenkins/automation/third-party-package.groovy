cc14a5a7039a01b3b663994ac282dd688bd596ab
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-02-25 19:08:58 +0530

    Add jenkinsfile for third-party-rpm
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

---
+++ jenkins/automation/third-party-package.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/env groovy
+pipeline { 
+    agent {
+        node {
+            label 'docker-cp-centos-7.8.2003-node'
+        }
+    }
+	
+	environment {
+        version = "2.0.0"
+    }
+
+    options {
+        timeout(time: 120, unit: 'MINUTES')
+        timestamps()
+        ansiColor('xterm') 
+        disableConcurrentBuilds()   
+    }
+    
+    stages {
+        stage('Checkout') {
+            steps {
+				script { build_stage = env.STAGE_NAME }
+					checkout([$class: 'GitSCM', branches: [[name: "third-party-rpm"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/shailesh-vaidya/cortx-re']]])
+            }
+        }
+
+        stage('Build') {
+            steps {
+				script { build_stage = env.STAGE_NAME }
+                sh encoding: 'utf-8', label: 'Provisioner RPMS', script: """
+                    pushd ./scripts/third-party-rpm
+                        ./build-prerequisite-rpm.sh -v $version -r ${BUILD_NUMBER} -g \$(git rev-parse --short HEAD)
+                """
+            }
+        }
+	}
+}
