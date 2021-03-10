1a89066a147e4b702ce9aa3e284746b6a3aeb6f9
Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>
2021-03-10 13:02:03 +0530

    Disabled isa dep install steps
    Signed-off-by: Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>

--- jenkins/internal-ci/centos-7.8.2003/pr/stable_merge/hare.groovy
+++ jenkins/internal-ci/centos-7.8.2003/pr/stable_merge/hare.groovy
@@ -68,6 +68,7 @@ pipeline {
         stage('Build') {
             steps {
 				script { build_stage = env.STAGE_NAME }
+                script { manager.addHtmlBadge("&emsp;<b>Target Branch : ${HARE_BRANCH}</b>&emsp;")}
 
                 sh """
                     set +x
@@ -89,7 +90,7 @@ pipeline {
                         yum install cortx-motr{,-devel} -y
                     """
 
-                    sh label: 'Build', returnStatus: true, script: '''
+                    sh label: 'Build', script: '''
                         set -xe
                         echo "Executing build script"
                         export build_number=${BUILD_NUMBER}
