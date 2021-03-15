96f78aa3c547256409725f4a5158bde333235943
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-03-15 17:00:40 +0530

    Test Job for CSM Return Code check.
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/internal-ci/centos-7.8.2003/main/csm-agent.groovy
+++ jenkins/internal-ci/centos-7.8.2003/main/csm-agent.groovy
@@ -60,77 +60,7 @@ pipeline {
 				'''	
 			}
 		}
-		
-		stage ('Upload') {
-			steps {
-				script { build_stage = env.STAGE_NAME }
-				sh label: 'Copy RPMS', script: '''
-					mkdir -p $build_upload_dir/$BUILD_NUMBER
-					cp ./dist/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir/$BUILD_NUMBER
-				'''
-				sh label: 'Repo Creation', script: '''pushd $build_upload_dir/$BUILD_NUMBER
-					rpm -qi createrepo || yum install -y createrepo
-					createrepo .
-					popd
-				'''
-			}
-		}
 
-		stage ('Tag last_successful') {
-			steps {
-				script { build_stage = env.STAGE_NAME }
-				sh label: 'Tag last_successful', script: '''pushd $build_upload_dir/
-					test -d $build_upload_dir/last_successful && rm -f last_successful
-					ln -s $build_upload_dir/$BUILD_NUMBER last_successful
-					popd
-				'''
-			}
-		}
-		
-		stage ("Release") {
-            when { triggeredBy 'SCMTrigger' }
-            steps {
-                script { build_stage = env.STAGE_NAME }
-				script {
-                	def releaseBuild = build job: 'Main Release', propagate: true
-				 	env.release_build = releaseBuild.number
-                    env.release_build_location = "http://cortx-storage.colo.seagate.com/releases/cortx/github/$branch/$os_version/${env.release_build}"
-				}
-            }
-        }
-	
-        stage('Update Jira') {
-                when { expression { return env.release_build != null } }
-	                steps {
-				script { build_stage=env.STAGE_NAME }	
-					script {
-						def jiraIssues = jiraIssueSelector(issueSelector: [$class: 'DefaultIssueSelector'])
-						jiraIssues.each { issue ->
-							 def author =  getAuthor(issue)
-							 jiraAddComment(	
-							 	idOrKey: issue,
-								site: "SEAGATE_JIRA",
-								comment: "{panel:bgColor=#c1c7d0}"+
-									"h2. ${component} - ${branch} branch build pipeline SUCCESS\n"+
-									"h3. Build Info:  \n"+
-										author+
-											"* Component Build  :  ${BUILD_NUMBER} \n"+
-											"* Release Build    :  ${release_build}  \n\n  "+
-									"h3. Artifact Location  :  \n"+
-										"*  "+"${release_build_location} "+"\n"+
-										"{panel}",
-								failOnError: false,
-								auditLog: false
-							)
-							//def jiraFileds = jiraGetIssue idOrKey: issue, site: "SEAGATE_JIRA", failOnError: false
-							//if(jiraFileds.data != null){
-							//def labels_data =  jiraFileds.data.fields.labels + "cortx_stable_b${release_build}"
-						//jiraEditIssue idOrKey: issue, issue: [fields: [ labels: labels_data ]], site: "SEAGATE_JIRA", failOnError: false	
-						//} 
-						}
-					}
-			}
-	}
 	}
 	
 	post {
@@ -148,7 +78,7 @@ pipeline {
 				def toEmail = ""
 				def recipientProvidersClass = [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
 				if( manager.build.result.toString() == "FAILURE" ) {
-					toEmail = "CORTX.CSM@seagate.com,shailesh.vaidya@seagate.com"
+					toEmail = "shailesh.vaidya@seagate.com"
 					recipientProvidersClass = [[$class: 'DevelopersRecipientProvider'],[$class: 'RequesterRecipientProvider']]
 				}
 
@@ -163,24 +93,4 @@ pipeline {
 			}
 		}
     }
-}	
-
-@NonCPS
-def getAuthor(issue) {
-
-    def changeLogSets = currentBuild.rawBuild.changeSets
-    def author= ""
-    def response = ""
-    // Grab build information
-    for (int i = 0; i < changeLogSets.size(); i++){
-        def entries = changeLogSets[i].items
-        for (int j = 0; j < entries.length; j++) {
-            def entry = entries[j]
-            if((entry.msg).contains(issue)){
-                author = entry.author
-            }
-        }
-    }
-    response = "* Author: "+author+"\n"
-    return response
 }
