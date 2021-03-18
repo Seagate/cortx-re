53dabf09196bfead14d2547408b0bf7141c40a73
Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>
2021-03-18 13:25:09 +0530

    Adhoc : Add cortx-prereq in mini-provisioner (csm, sspl)
    Signed-off-by: Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>

--- jenkins/internal-ci/centos-7.8.2003/pr/cortx_deployment/motr.groovy
+++ jenkins/internal-ci/centos-7.8.2003/pr/cortx_deployment/motr.groovy
@@ -69,7 +69,7 @@ pipeline {
         stage('Build') {
             steps {
 				script { build_stage = env.STAGE_NAME }
-                script { manager.addHtmlBadge("&emsp;<b>Target Branch : ${BRANCH}</b>&emsp;")}
+                script { manager.addHtmlBadge("&emsp;<b>Target Branch : ${BRANCH}</b>&emsp;<br />")}
 
                  sh """
                     set +x
