af10a2b69890791c1d5a7defbcf97c029b1b0f80
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-03-31 17:54:59 +0530

    Added EXCLUDE_PACKAGES list
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/build_release_info.sh
+++ scripts/release_support/build_release_info.sh
@@ -51,6 +51,7 @@ fi
 
 
 echo -e "Generating RELEASE.INFO file"
+EXCLUDE_PACKAGES="cortx-motr-devel\|cortx-motr-tests-ut\|cortx-libsspl_sec-devel\|cortx-libsspl_sec-method_pki\|cortx-prvsnr-cli\|cortx-sspl-cli\|cortx-s3iamcli-devel\|cortx-sspl-test"
 pushd "$BUILD_LOCATION" || exit
 cat <<EOF > RELEASE.INFO
 ---
