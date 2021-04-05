d53a96b4b386e8a052c375d44f69588860eb2d7a
Gowthaman Chinnathambi <56022993+gowthamchinna@users.noreply.github.com>
2021-04-05 13:18:33 +0530

    EOS-19069 : Motr Mini Provisioner update (#338)
    Signed-off-by: Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>
    Signed-off-by: Puja Mudaliar <puja.mudaliar@seagate.com>

--- jenkins/internal-ci/centos-7.8.2003/pr/mini_provisioning/motr-mini_provisioning.groovy
+++ jenkins/internal-ci/centos-7.8.2003/pr/mini_provisioning/motr-mini_provisioning.groovy
@@ -74,18 +74,6 @@ pipeline {
                 dir("motr") {
 
                     checkout([$class: 'GitSCM', branches: [[name: "${MOTR_BRANCH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog'], [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: "${MOTR_URL}",  name: 'origin', refspec: "${MOTR_PR_REFSEPEC}"]]])
-
-                    //  sh label: 'isa dep', script: '''
-                    //     yum install -y https://www.nasm.us/pub/nasm/releasebuilds/2.14/linux/nasm-2.14-0.fc27.x86_64.rpm
-                    //     wget https://github.com/intel/isa-l/archive/v2.30.0.tar.gz
-                    //     tar -xf v2.30.0.tar.gz
-                    //     cd isa-l-2.30.0/
-                    //     sh autogen.sh
-                    //     ./configure --prefix=/usr --libdir=/usr/lib64
-                    //     make
-                    //     make install
-                    //     ldconfig
-					// '''
                 
                     sh label: '', script: '''
                         export build_number=${BUILD_ID}
@@ -113,6 +101,7 @@ pipeline {
                 // Build Hare
                 sh label: '', script: '''
                     pushd /root/build_rpms
+                        yum install cortx-py-utils -y
                         yum --disablerepo=* localinstall cortx-motr-1*.rpm cortx-motr-2*.rpm cortx-motr-devel*.rpm -y
                     popd
                 '''
@@ -235,7 +224,7 @@ pipeline {
             agent {
                 node {
                     // Run deployment on mini_provisioner nodes (vm deployment nodes)
-                    label params.HOST == "-" ? "mini_provisioner && !cleanup_req" : "mini_provisioner_user_host"
+                    label params.HOST == "-" ? "mini_provisioner_motr && !cleanup_req" : "mini_provisioner_user_host"
                     customWorkspace "/var/jenkins/mini_provisioner/${JOB_NAME}_${BUILD_NUMBER}"
                 }
             }
@@ -271,16 +260,16 @@ pipeline {
                     }
 
                     // Collect logs from test node
-                    catchError {
-
+                    try {
                         sh label: 'download_log_files', returnStdout: true, script: """ 
-                            sshpass -p '${NODE_PASS}' scp -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${NODE_USER}@${NODE1_HOST}:/root/*.json .
-                            sshpass -p '${NODE_PASS}' scp -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${NODE_USER}@${NODE1_HOST}:/root/*.yml .
-                            sshpass -p '${NODE_PASS}' scp -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${NODE_USER}@${NODE1_HOST}:/root/*.log .
+                            sshpass -p '${NODE_PASS}' scp -r -o StrictHostKeyChecking=no ${NODE_USER}@${NODE1_HOST}:/root/*.log . || true
+                            sshpass -p '${NODE_PASS}' scp -r -o StrictHostKeyChecking=no ${NODE_USER}@${NODE1_HOST}:/root/*.json . || true
                         """
-                        
-                        archiveArtifacts artifacts: "*.json, *.yml, *.log", onlyIfSuccessful: false, allowEmptyArchive: true 
+                    } catch (err) {
+                        echo err.getMessage()
                     }
+
+                    archiveArtifacts artifacts: "*.log, *.json", onlyIfSuccessful: false, allowEmptyArchive: true 
 
                     // Trigger cleanup VM
                     if( "${HOST}" == "-" ) {
