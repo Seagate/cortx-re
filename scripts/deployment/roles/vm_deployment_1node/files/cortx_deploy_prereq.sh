4aceb84186ccb11b462ebe76667765e3519998d3
Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>
2021-04-07 13:45:15 +0530

    Provisioner command updates
    Signed-off-by: Gowthaman Chinnathambi <gowthaman.chinnathambi@seagate.com>

--- scripts/deployment/roles/vm_deployment_1node/files/cortx_deploy_prereq.sh
+++ scripts/deployment/roles/vm_deployment_1node/files/cortx_deploy_prereq.sh
@@ -17,16 +17,29 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
 
-BUILD_URL="$1"
-NODE1="$2"
-CLUSTER_PASS="$3"
+CORTX_RELEASE_REPO="$1"
 
-yum install -y yum-utils  
-yum-config-manager --add-repo "${BUILD_URL}/3rd_party/"
-yum install --nogpgcheck -y python3 python36-m2crypto salt salt-master salt-minion  
-rm -rf /etc/yum.repos.d/*3rd_party*.repo  
-yum-config-manager --add-repo "${BUILD_URL}/cortx_iso/"
-yum install --nogpgcheck -y python36-cortx-prvsnr  
-rm -rf /etc/yum.repos.d/*cortx_iso*.repo  
+yum install -y yum-utils
+yum-config-manager --add-repo "${CORTX_RELEASE_REPO}/3rd_party/"
+yum-config-manager --add-repo "${CORTX_RELEASE_REPO}/cortx_iso/"
+
+cat <<EOF >/etc/pip.conf
+[global]
+timeout: 60
+index-url: $CORTX_RELEASE_REPO/python_deps/
+trusted-host: $(echo $CORTX_RELEASE_REPO | awk -F '/' '{print $3}')
+EOF
+
+# Cortx Pre-requisites
+yum install --nogpgcheck -y python3 cortx-prereq sshpass
+# Pre-reqs for Provisioner
+yum install --nogpgcheck -y python36-m2crypto salt salt-master salt-minion
+# Provisioner API
+yum install --nogpgcheck -y python36-cortx-prvsnr
+
+# Cleanup temporary repos
+rm -rf /etc/yum.repos.d/*3rd_party*.repo
+rm -rf /etc/yum.repos.d/*cortx_iso*.repo
 yum clean all
 rm -rf /var/cache/yum/
+rm -rf /etc/pip.conf
