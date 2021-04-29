f2cd083581f2c7e172bded8142b10057490c1d62
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-04-29 07:24:55 -0600

    chore: fixes
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- scripts/ova-r2-release/roles/vm-provisioner/files/provisioner.sh
+++ scripts/ova-r2-release/roles/vm-provisioner/files/provisioner.sh
@@ -3,13 +3,13 @@
 CORTX_RELEASE_REPO="$1"
 
 yum install -y yum-utils
-yum-config-manager --add-repo "${CORTX_RELEASE_REPO}3rd_party/"
-yum-config-manager --add-repo "${CORTX_RELEASE_REPO}cortx_iso/"
+yum-config-manager --add-repo "${CORTX_RELEASE_REPO}/3rd_party/"
+yum-config-manager --add-repo "${CORTX_RELEASE_REPO}/cortx_iso/"
 
 cat <<EOF >/etc/pip.conf
 [global]
 timeout: 60
-index-url: $CORTX_RELEASE_REPOpython_deps/
+index-url: $CORTX_RELEASE_REPO/python_deps/
 trusted-host: $(echo $CORTX_RELEASE_REPO | awk -F '/' '{print $3}')
 EOF
 
