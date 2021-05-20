0861dc4e4920a5d62c4dc157d12ae9274bb69e86
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-05-20 14:57:44 +0530

    chore: change repository
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/internal-ci/centos-7.8.2003/main/release.groovy
+++ jenkins/internal-ci/centos-7.8.2003/main/release.groovy
@@ -49,7 +49,7 @@ pipeline {
         stage('Checkout Release scripts') {
 			steps {
         	    script { build_stage = env.STAGE_NAME }
-                checkout([$class: 'GitSCM', branches: [[name: 'main']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/cortx-re']]])
+                checkout([$class: 'GitSCM', branches: [[name: 'main']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/gauravchaudhari02/cortx-re.git']]])
 			}
 		}
 			
