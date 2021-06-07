ae731d56906a152a97558371e507a2e9662ff3b8
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-06-07 12:39:28 +0530

    chore: repo checkout change
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/automation/vm_teardown_1n.groovy
+++ jenkins/automation/vm_teardown_1n.groovy
@@ -43,7 +43,7 @@ pipeline {
 
                     // Clone cortx-re repo
                     dir('cortx-re') {
-                        checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/cortx-re.git']]])                
+                        checkout([$class: 'GitSCM', branches: [[name: '*/main-teardown']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/gauravchaudhari02/cortx-re.git']]])                
                     }
 
                 }
