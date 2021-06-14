e5388d671d26ffb04f1aff7fa308fd23c2c771e8
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-06-14 14:25:36 +0530

    Use all Python packages
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.8.2003/custom-ci.groovy
+++ jenkins/custom-ci/centos-7.8.2003/custom-ci.groovy
@@ -404,7 +404,8 @@ pipeline {
                 mkdir -p $integration_dir/$release_tag/sw_upgrade/{3rd_party,cortx_iso,python_deps}
                 createrepo -v $integration_dir/$release_tag/sw_upgrade/3rd_party/
                 find $integration_dir/$release_tag/3rd_party/ -not -path '*repodata*' -type d  -printf '%P\n' | xargs -t -I % sh -c '{ mkdir -p $integration_dir/$release_tag/sw_upgrade/3rd_party/%; createrepo -v $integration_dir/$release_tag/sw_upgrade/3rd_party/%; }'
-		        cp -r $integration_dir/$release_tag/cortx_iso/* $integration_dir/$release_tag/sw_upgrade/cortx_iso
+		        cp -r $integration_dir/$release_tag/cortx_iso/* $integration_dir/$release_tag/sw_upgrade/cortx_iso/
+				cp -r $integration_dir/$release_tag/python_deps/* $integration_dir/$release_tag/sw_upgrade/python_deps/
                 cp $integration_dir/$release_tag/3rd_party/THIRD_PARTY_RELEASE.INFO $integration_dir/$release_tag/sw_upgrade/3rd_party
 				cp $integration_dir/$release_tag/cortx_iso/RELEASE.INFO $integration_dir/$release_tag/sw_upgrade/
 				cp $integration_dir/$release_tag/python_deps/index.html $integration_dir/$release_tag/sw_upgrade/python_deps/index.html
