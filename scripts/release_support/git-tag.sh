7694201a460179dcf5e4ec8bd9ab9c22d0cda38f
cortx admin <cortx-application@seagate.com>
2021-06-15 07:29:02 -0600

    Updated git-tag.sh
    Signed-off-by: cortx admin <cortx-application@seagate.com>

--- scripts/release_support/git-tag.sh
+++ scripts/release_support/git-tag.sh
@@ -11,35 +11,35 @@ declare -A COMPONENT_LIST=(
 			[cortx-csm_web]="https://$PASSWD@github.com/Seagate/cortx-management-portal.git"
         	)
 					
-			git config --global user.email "cortx-application@seagate.com"
-			git config --global user.name "cortx admin"
-			wget -q $RELEASE_INFO_URL -O RELEASE.INFO
+			//git config --global user.email "balaji.ramachandran@seagate.com"
+			//git config --global --reset-all user.name "Balaji Ramachandran"
+			wget -q "$RELEASE_INFO_URL" -O RELEASE.INFO
 	
 	for component in "${!COMPONENT_LIST[@]}"
 
 	do
-		dir=$(echo ${COMPONENT_LIST[$component]} |  awk -F'/' '{print $NF}')
-		git clone --quiet ${COMPONENT_LIST[$component]} $dir > /dev/null
+		dir="$(echo "${COMPONENT_LIST[$component]}" |  awk -F'/' '{print $NF}')"
+		git clone --quiet "${COMPONENT_LIST["$component"]}" "$dir" > /dev/null
 
 		rc=$?
-		if [ $rc -ne 0 ]; then 
-			echo "ERROR:git clone failed for $component"
+		if [ "$rc" -ne 0 ]; then 
+			echo "ERROR:git clone failed for "$component""
 		exit 1
 		fi
 
-		if [ $component == cortx-hare ] || [ $component == cortx-sspl ] || [ $component == cortx-ha ] || [ $component == cortx-fs ]; then
-			COMMIT_HASH=$(grep $component RELEASE.INFO | head -1 | awk -F['_'] '{print $2}' | cut -d. -f1 |  sed 's/git//g');
+		if [ "$component" == cortx-hare ] || [ "$component" == cortx-sspl ] || [ "$component" == cortx-ha ] || [ "$component" == cortx-fs ]; then
+			COMMIT_HASH="$(grep "$component" RELEASE.INFO | head -1 | awk -F['_'] '{print $2}' | cut -d. -f1 |  sed 's/git//g')";
 		elif [ "$component" == "cortx-csm_agent" ] || [ "$component" == "cortx-csm_web" ]; then
-			COMMIT_HASH=$(grep $component RELEASE.INFO | head -1 | awk -F['_'] '{print $3}' |  cut -d. -f1);
+			COMMIT_HASH="$(grep "$component" RELEASE.INFO | head -1 | awk -F['_'] '{print $3}' |  cut -d. -f1)";
 		else
-			COMMIT_HASH=$(grep $component RELEASE.INFO | head -1 | awk -F['_'] '{print $2}' | sed 's/git//g');
+			COMMIT_HASH="$(grep "$component" RELEASE.INFO | head -1 | awk -F['_'] '{print $2}' | sed 's/git//g')";
 		fi
 
-		echo "Component: $component , Repo:  ${COMPONENT_LIST[$component]}, Commit Hash: ${COMMIT_HASH}"
-		pushd $dir
+		echo "Component: "$component" , Repo:  "${COMPONENT_LIST[$component]}", Commit Hash: "${COMMIT_HASH}""
+		pushd "$dir"
 			git tag -a "$GIT_TAG" "${COMMIT_HASH}" -m "Latest Release"
 			git push origin "$GIT_TAG"
-			echo "Component: $component , Tag: git tag -l $GIT_TAG is Tagged Successfully"
+			echo "Component: "$component" , Tag: git tag -l "$GIT_TAG" is Tagged Successfully"
 			git push origin --delete "$GIT_TAG"
 		popd
 	
