6ce5d2b7769747d0b4143e79ae6e5837f2a93c74
-admin <cortx-application@seagate.com>
2021-06-15 02:25:20 -0600

    Updated tagging.groovy
    Signed-off-by: -admin <cortx-application@seagate.com>

--- jenkins/automation/tagging.groovy
+++ jenkins/automation/tagging.groovy
@@ -8,10 +8,9 @@ pipeline {
 
         parameters {
                 string(name: 'RELEASE_INFO_URL', defaultValue: '', description: 'RELEASE BUILD')
-                //string(name: 'THIRD_PARTY_RELEASE_INFO_URL', defaultValue: '', description: 'THIRD PARTY RELEASE BUILD')
-				string(name: 'GIT_TAG', defaultValue: '', description: 'Release Tag')
-				string(name: 'USER_NAME', defaultValue: '', description: 'User Name')
-				string(name: 'PASSWD', defaultValue: '', description: 'Token')
+                string(name: 'GIT_TAG', defaultValue: '', description: 'Release Tag')
+				
+				
 				
         }
 		
@@ -43,55 +42,21 @@ pipeline {
             }
         }
 		
+		stage('Checkout Script') {
+            steps {             
+                script {
+                    checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/cortx-re']]])                
+                }
+            }
+        }
 		stage ("Tagging") {
 			steps {
 				script { build_stage=env.STAGE_NAME }
-				script { 
-				sh label: 'Git Tagging', script: '''#!/bin/bash
-				declare -A COMPONENT_LIST=( 
-						[cortx-s3server]="https://$PASSWD@github.com/$USER_NAME/cortx-s3server.git"
-						[cortx-motr]="https://$PASSWD@github.com/$USER_NAME/cortx-motr.git"
-						[cortx-hare]="https://$PASSWD@github.com/$USER_NAME/cortx-hare.git"
-						[cortx-ha]="https://$PASSWD@github.com/$USER_NAME/cortx-ha.git"
-						[cortx-prvsnr]="https://$PASSWD@github.com/$USER_NAME/cortx-prvsnr.git"
-						[cortx-sspl]="https://$PASSWD@github.com/$USER_NAME/cortx-monitor.git"
-						[cortx-csm_agent]="https://$PASSWD@github.com/$USER_NAME/cortx-manager.git"
-						[cortx-csm_web]="https://$PASSWD@github.com/$USER_NAME/cortx-management-portal.git"
-					)
-					
-					git config --global user.email "cortx-application@seagate.com"
-					git config --global user.name "$cortx-admin"
-					wget -q $RELEASE_INFO_URL -O RELEASE.INFO
-					for component in "${!COMPONENT_LIST[@]}"
-					
-					do
-						dir=$(echo ${COMPONENT_LIST[$component]} |  awk -F'/' '{print $NF}')
-						git clone --quiet ${COMPONENT_LIST[$component]} $dir > /dev/null
-
-						rc=$?
-						if [ $rc -ne 0 ]; then 
-							echo "ERROR:git clone failed for $component"
-							exit 1
-						fi
-
-						if [ $component == cortx-hare ] || [ $component == cortx-sspl ] || [ $component == cortx-ha ] || [ $component == cortx-fs ]; then
-							COMMIT_HASH=$(grep $component RELEASE.INFO | head -1 | awk -F['_'] '{print $2}' | cut -d. -f1 |  sed 's/git//g');
-						elif [ "$component" == "cortx-csm_agent" ] || [ "$component" == "cortx-csm_web" ]; then
-							COMMIT_HASH=$(grep $component RELEASE.INFO | head -1 | awk -F['_'] '{print $3}' |  cut -d. -f1);
-						else
-							COMMIT_HASH=$(grep $component RELEASE.INFO | head -1 | awk -F['_'] '{print $2}' | sed 's/git//g');
-						fi
-
-						echo "Component: $component , Repo:  ${COMPONENT_LIST[$component]}, Commit Hash: ${COMMIT_HASH}"
-						pushd $dir
-							git tag -a "$GIT_TAG" "${COMMIT_HASH}" -m "Latest Release"
-							git push origin "$GIT_TAG"
-							echo "Component: $component , Tag: git tag -l $GIT_TAG is Tagged Successfully"
-						popd
-						
-					done
-				'''
-				}				
+					script { 
+						withCredentials([usernamePassword(credentialsId: 'cortx-admin-github', passwordVariable: 'PASSWD', usernameVariable: 'USER_NAME')]) {
+						sh """ bash scripts/release_support/git-tag.sh """
+					}
+				}			
 			}
 		}
 	}
@@ -115,6 +80,4 @@ def get_commit_hash(String component, String release_info) {
 			
 			""", returnStdout: true).trim()
 }
-
-
 
