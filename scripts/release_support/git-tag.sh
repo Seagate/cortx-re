68c32fc1abd646f51fb1d4917c08d85a4d1c9101
cortx admin <cortx-application@seagate.com>
2021-06-15 03:46:11 -0600

    Updated git-tag.sh
    Signed-off-by: cortx admin <cortx-application@seagate.com>

--- scripts/release_support/git-tag.sh
+++ scripts/release_support/git-tag.sh
@@ -9,7 +9,7 @@ declare -A COMPONENT_LIST=(
 			[cortx-sspl]="https://$PASSWD@github.com/Seagate/cortx-monitor.git"
 			[cortx-csm_agent]="https://$PASSWD@github.com/Seagate/cortx-manager.git"
 			[cortx-csm_web]="https://$PASSWD@github.com/Seagate/cortx-management-portal.git"
-	        	)
+        	)
 					
 			git config --global user.email "cortx-application@seagate.com"
 			git config --global user.name "cortx admin"
