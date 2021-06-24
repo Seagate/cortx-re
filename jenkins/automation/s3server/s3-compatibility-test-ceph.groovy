46e0e2574183d7d2d7220fdce6b9cdd42cc4d721
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-06-24 16:13:17 +0530

    Increase timeout to 4 Hours
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/automation/s3server/s3-compatibility-test-ceph.groovy
+++ jenkins/automation/s3server/s3-compatibility-test-ceph.groovy
@@ -11,7 +11,7 @@ pipeline {
 	}
 
 	options {
-        timeout(time: 120, unit: 'MINUTES')
+        timeout(time: 240, unit: 'MINUTES')
         timestamps()
         ansiColor('xterm') 
         buildDiscarder(logRotator(numToKeepStr: "30"))
