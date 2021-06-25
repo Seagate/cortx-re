6458938c15d2552cb7e2eb09cef63ff1c54563cc
Balaji Ramachandran <balaji.ramachandran@seagate.com>
2021-06-24 22:56:45 -0600

    Updated git-tag.sh
    Signed-off-by: Balaji Ramachandran <balaji.ramachandran@seagate.com>

--- scripts/release_support/git-tag.sh
+++ scripts/release_support/git-tag.sh
@@ -60,17 +60,23 @@ declare -A COMPONENT_LIST=(
                 else
                         echo "Tag is not successful. Please pass value to GIT_TAG";
                 fi
-	for component in "${!REPO_LIST[@]}"
+		
+		if [ "$REL_TAG" != "" ]; then
+			echo "Release will be set for all the components";
+
+		for component in "${!REPO_LIST[@]}"
 	
-	do				
-		if [ "$REL_TAG" != "" ] || [ "$component" == cortx-hare ] || [ "$component" == cortx-sspl ] || [ "$component" == cortx-ha ] || [ "$component" == cortx-fs ] || [ "$component" == cortx-py-utils ] || [ "$component" == cortx-prereq ] || [ "$component" == "cortx-csm_agent" ] || [ "$component" == "cortx-csm_web" ]; then
+		do				
+			if [ "$component" == cortx-hare ] || [ "$component" == cortx-sspl ] || [ "$component" == cortx-ha ] || [ "$component" == cortx-fs ] || [ "$component" == cortx-py-utils ] || [ "$component" == cortx-prereq ] || [ "$component" == "cortx-csm_agent" ] || [ "$component" == "cortx-csm_web" ]; then
 			echo "Component: "$component" , Repo:  "${REPO_LIST[$component]}"";
 			curl -H "Accept: application/vnd.github.v3+json"  "${REPO_LIST[$component]}" -d '{"tag_name":"Cred-Test4", "name":"Release4"}';                        
-                else
+                	else
                         echo "Release is not successful. Please pass value to REL_TAG";
+			fi
+		done			
+                
 		fi
-	done			
-                if [ "$DEBUG" = true ]; then
+		if [ "$DEBUG" = true ]; then
 			git push origin --delete "$GIT_TAG";
                 else
 			echo "Run in Debug mode if current Git tag needs to be deleted";
