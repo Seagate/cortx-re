da0f12e6fd7bb73dbd5ada073c20d83eab3f7dbe
Balaji Ramachandran <balaji.ramachandran@seagate.com>
2021-06-25 02:26:10 -0600

    Updated Release id
    Signed-off-by: Balaji Ramachandran <balaji.ramachandran@seagate.com>

--- jenkins/automation/tagging.groovy
+++ jenkins/automation/tagging.groovy
@@ -9,6 +9,8 @@ pipeline {
         parameters {
                 string(name: 'RELEASE_INFO_URL', defaultValue: '', description: 'RELEASE BUILD')
                 string(name: 'GIT_TAG', defaultValue: '', description: 'Release Tag')
+		string(name: 'REL_NAME', defaultValue: '', description: 'Release Name')
+		string(name: 'REL_ID', defaultValue: '', description: 'Release Id')
 		string(name: 'TAG_MESSAGE', defaultValue: '', description: 'Tag Message')
 		booleanParam(name: 'DEBUG', defaultValue: false, description: 'Select this if you want to Delete the current Tag')
             }
