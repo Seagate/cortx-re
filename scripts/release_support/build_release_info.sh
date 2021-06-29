cfcd26d2eb9e15ce5e2a75b6813deaa9600edd0c
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-06-29 16:25:31 +0000

    Fix Codacy issue
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/build_release_info.sh
+++ scripts/release_support/build_release_info.sh
@@ -65,7 +65,7 @@ OS: $(cat /etc/redhat-release | sed -e 's/ $//g' -e 's/^/\"/g' -e 's/$/\"/g')
 DATETIME: $(date +"%d-%b-%Y %H:%M %Z" | sed -e 's/^/\"/g' -e 's/$/\"/g')
 KERNEL: $(ls cortx-motr-[0-9]*.rpm | sed -e  's/.*3/3/g' -e 's/.x86_64.rpm//g' -e 's/^/\"/g' -e 's/$/\"/g')
 REQUIRES:
-$(sed 's/^/    /g' $SCRIPT_DIR/../versioning/compatibility.info)
+$(sed 's/^/    /g' "$SCRIPT_DIR"/../versioning/compatibility.info)
 COMPONENTS:
 $(ls -1 ./*.rpm | sed 's/\.\///g' | grep -v "$EXCLUDE_PACKAGES" |  awk '{ print "    - \""$1"\""}')
 EOF
