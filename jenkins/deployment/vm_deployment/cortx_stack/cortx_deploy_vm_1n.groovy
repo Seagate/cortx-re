1d2f639d8d2e312b359b9754b5946564025ca8b5
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-07-06 19:05:30 +0530

    chore: revert test changes
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/deployment/vm_deployment/cortx_stack/cortx_deploy_vm_1n.groovy
+++ jenkins/deployment/vm_deployment/cortx_stack/cortx_deploy_vm_1n.groovy
@@ -59,7 +59,7 @@ pipeline {
                         echo "-----------------------------------------------------------"
                     """
                     dir('cortx-re') {
-                        checkout([$class: 'GitSCM', branches: [[name: '*/fix-config-file']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/gauravchaudhari02/cortx-re.git']]])
+                        checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/cortx-re.git']]])
                     }
 
                     if ( NODE1.isEmpty() ) {
