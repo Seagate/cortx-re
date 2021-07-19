90576eb47c5a14eb4d4303ece454a92aa4780845
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-07-19 20:49:59 +0530

    Fromating
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/internal-ci/centos-7.9.2009/branch/main/cortx-prereq.groovy
+++ jenkins/internal-ci/centos-7.9.2009/branch/main/cortx-prereq.groovy
@@ -5,11 +5,11 @@ pipeline {
            label 'cortx-prereq-validation'
         }
     }
-	
+    
     environment {
-		version = "2.0.0"
+        version = "2.0.0"
         env = "dev"
-		component = "cortx-prereq"
+        component = "cortx-prereq"
         branch = "main"
         os_version = "centos-7.9.2009"
         release_dir = "/mnt/bigstorage/releases/cortx"
@@ -28,27 +28,27 @@ pipeline {
     }
 
     stages {
-	
-		stage('Prerequisite') {
+    
+        stage('Prerequisite') {
             steps {
                 sh encoding: 'utf-8', label: 'Install Prerequisite Packages', script: """
                     yum erase cortx-prereq -y
-					yum install rpm-build rpmdevtools -y 
-					rpmdev-setuptree		
+                    yum install rpm-build rpmdevtools -y 
+                    rpmdev-setuptree        
                 """
-			}
-		}
-	
+            }
+        }
+    
         stage('Checkout') {
             steps {
-				script { build_stage = env.STAGE_NAME }
+                script { build_stage = env.STAGE_NAME }
                 checkout([$class: 'GitSCM', branches: [[name: "${branch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PathRestriction', excludedRegions: '', includedRegions: 'scripts/third-party-rpm/.*']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/cortx-re']]])
             }
         }
 
         stage('Build') {
             steps {
-				script { build_stage = env.STAGE_NAME }
+                script { build_stage = env.STAGE_NAME }
                 sh encoding: 'utf-8', label: 'Build cortx-prereq package', script: """
                     pushd ./scripts/third-party-rpm
                         sh ./build-prerequisite-rpm.sh -v $version -r ${BUILD_NUMBER} -g \$(git rev-parse --short HEAD)
@@ -59,7 +59,7 @@ pipeline {
 
         stage('Test') {
             steps {
-				script { build_stage = env.STAGE_NAME }
+                script { build_stage = env.STAGE_NAME }
                 sh encoding: 'utf-8', label: 'Test cortx-prereq package', script: """
                 pushd ./scripts/third-party-rpm
                       sh ./install-cortx-prereq.sh -b "http://cortx-storage.colo.seagate.com/releases/cortx/github/$branch/$os_version/last_successful_prod/" -r local
@@ -69,56 +69,56 @@ pipeline {
         }
 
         stage ('Upload') {
-			steps {
-				script { build_stage = env.STAGE_NAME }
-				sh label: 'Copy RPMS', script: '''
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Copy RPMS', script: '''
                     mkdir -p $build_upload_dir/$BUILD_NUMBER
                     cp /root/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir/$BUILD_NUMBER
-				'''
+                '''
                 sh label: 'Repo Creation', script: '''pushd $build_upload_dir/$BUILD_NUMBER
                     rpm -qi createrepo || yum install -y createrepo
                     createrepo .
                     popd
-				'''
-			}
-		}
-			
-		stage ('Tag last_successful') {
-			steps {
-				script { build_stage = env.STAGE_NAME }
-				sh label: 'Clean-up', script: '''
+                '''
+            }
+        }
+            
+        stage ('Tag last_successful') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Clean-up', script: '''
                     pushd $build_upload_dir/
                     test -d $build_upload_dir/last_successful && rm -f last_successful
                     ln -s $build_upload_dir/$BUILD_NUMBER last_successful
                     popd
-				'''
-			}
-		}
+                '''
+            }
+        }
 
         stage ("Release") {
             when { triggeredBy 'SCMTrigger' }
             steps {
                 script { build_stage = env.STAGE_NAME }
-				script {
-                	def releaseBuild = build job: 'Main Release', propagate: true
-				 	env.release_build = releaseBuild.number
+                script {
+                    def releaseBuild = build job: 'Main Release', propagate: true
+                     env.release_build = releaseBuild.number
                     env.release_build_location = "http://cortx-storage.colo.seagate.com/releases/cortx/github/$branch/$os_version/${env.release_build}"
-				}
+                }
             }
         }
-	}
-	
-	post {
-	
-		success {
-				sh label: 'Clean-up', script: '''
-				set +x
-				rm -rf /etc/yum.repos.d/cortx-storage.colo.seagate.com* /etc/pip.conf /root/rpmbuild/RPMS/x86_64/*.rpm
+    }
+    
+    post {
+    
+        success {
+                sh label: 'Clean-up', script: '''
+                set +x
+                rm -rf /etc/yum.repos.d/cortx-storage.colo.seagate.com* /etc/pip.conf /root/rpmbuild/RPMS/x86_64/*.rpm
                 if [ -f /opt/seagate/cortx/python-deps/python-requirements.txt ]; then
                     pip3 uninstall -r /opt/seagate/cortx/python-deps/python-requirements.txt -y
                 fi
-				yum erase cortx-prereq -y
-				'''
-		}
+                yum erase cortx-prereq -y
+                '''
+        }
     }
 }
