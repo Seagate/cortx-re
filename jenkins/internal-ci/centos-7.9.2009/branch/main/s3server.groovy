9ecfa7bbedb4e2f7261e7da65fa3947c742183c4
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-07-19 20:51:32 +0530

    Formatting
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/internal-ci/centos-7.9.2009/branch/main/s3server.groovy
+++ jenkins/internal-ci/centos-7.9.2009/branch/main/s3server.groovy
@@ -1,33 +1,33 @@
 #!/usr/bin/env groovy
 pipeline {
-	agent {
-		node {
-			label 'docker-centos-7.9.2009-node'
-		}
-	}
-	
-	triggers {
+    agent {
+        node {
+            label 'docker-centos-7.9.2009-node'
+        }
+    }
+    
+    triggers {
         pollSCM '*/5 * * * *'
     }
 
-	environment {
+    environment {
         version = "2.0.0"
         env = "dev"
-		component = "s3server"
+        component = "s3server"
         branch = "main"
         os_version = "centos-7.9.2009"
         release_dir = "/mnt/bigstorage/releases/cortx"
         build_upload_dir = "$release_dir/components/github/$branch/$os_version/$env/$component"
     }
 
-	options {
-		timeout(time: 120, unit: 'MINUTES')
-		timestamps()
+    options {
+        timeout(time: 120, unit: 'MINUTES')
+        timestamps()
         ansiColor('xterm')   
         disableConcurrentBuilds()   
-	}
-	
-	stages {
+    }
+    
+    stages {
 
         stage('Checkout') {
             steps {
@@ -39,7 +39,7 @@ pipeline {
         }
 
         stage("Set Motr Build") {
-            stages {			
+            stages {            
                 stage("Motr Build - Last Successfull") {
                     when { not { triggeredBy 'UpstreamCause' } }
                     steps {
@@ -51,7 +51,7 @@ pipeline {
                             '''
                         }
                     }
-                }				
+                }                
                 stage("Motr Build - Current") {
                     when { triggeredBy 'UpstreamCause' }
                     steps {
@@ -69,7 +69,7 @@ pipeline {
         stage('Build') {
             steps {
                 script { build_stage = env.STAGE_NAME }
-                dir ('s3') {	
+                dir ('s3') {    
                     sh label: 'Build s3server RPM', script: '''
                         yum-config-manager --save --setopt=cortx-storage*.gpgcheck=1 cortx-storage* && yum-config-manager --save --setopt=cortx-storage*.gpgcheck=0 cortx-storage*
                         yum clean all;rm -rf /var/cache/yum
@@ -87,7 +87,7 @@ pipeline {
                         export build_number=${BUILD_ID}
                         ./rpms/s3test/buildrpm.sh -P $PWD
                     '''
-                }			
+                }            
             }
         }
 
@@ -95,7 +95,7 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 sh label: 'Copy RPMS', script: '''
-                    rm -rf $build_upload_dir/$BUILD_NUMBER 	
+                    rm -rf $build_upload_dir/$BUILD_NUMBER     
                     mkdir -p $build_upload_dir/$BUILD_NUMBER
                     cp /root/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir/$BUILD_NUMBER
                     cp /root/rpmbuild/RPMS/noarch/*.rpm $build_upload_dir/$BUILD_NUMBER
@@ -121,60 +121,60 @@ pipeline {
         }
 
         stage ("Release Build") {
-		    when { not { triggeredBy 'UpstreamCause' } }
+            when { not { triggeredBy 'UpstreamCause' } }
             steps {
                 script { build_stage = env.STAGE_NAME }
-				script {
-                	def releaseBuild = build job: 'Main Release', propagate: true
-				 	env.release_build = releaseBuild.number
+                script {
+                    def releaseBuild = build job: 'Main Release', propagate: true
+                     env.release_build = releaseBuild.number
                     env.release_build_location="http://cortx-storage.colo.seagate.com/releases/cortx/github/$branch/$os_version/${env.release_build}"
-				}
+                }
             }
         }
-	stage('Update Jira') {
-		when { expression { return env.release_build != null } }
-		steps {
-			script { build_stage=env.STAGE_NAME }
-				script {
-					def jiraIssues = jiraIssueSelector(issueSelector: [$class: 'DefaultIssueSelector'])
-					jiraIssues.each { issue ->
-						def author =  getAuthor(issue)
-						jiraAddComment(
-							idOrKey: issue,
-							site: "SEAGATE_JIRA",
-							comment: "{panel:bgColor=#c1c7d0}"+
-								"h2. ${component} - ${branch} branch build pipeline SUCCESS\n"+
-								"h3. Build Info:  \n"+
-									author+
-										"* Component Build  :  ${BUILD_NUMBER} \n"+
-										"* Release Build    :  ${release_build}  \n\n  "+
-								"h3. Artifact Location  :  \n"+
-									"*  "+"${release_build_location} "+"\n"+
-									"{panel}",
-							failOnError: false,
-							auditLog: false
-						)
-						//def jiraFileds = jiraGetIssue idOrKey: issue, site: "SEAGATE_JIRA", failOnError: false
-						//if(jiraFileds.data != null){
-						//def labels_data =  jiraFileds.data.fields.labels + "cortx_stable_b${release_build}"
-						//jiraEditIssue idOrKey: issue, issue: [fields: [ labels: labels_data ]], site: "SEAGATE_JIRA", failOnError: false
-						//}
-					}
-				}
-		}
-	}
-	}
+    stage('Update Jira') {
+        when { expression { return env.release_build != null } }
+        steps {
+            script { build_stage=env.STAGE_NAME }
+                script {
+                    def jiraIssues = jiraIssueSelector(issueSelector: [$class: 'DefaultIssueSelector'])
+                    jiraIssues.each { issue ->
+                        def author =  getAuthor(issue)
+                        jiraAddComment(
+                            idOrKey: issue,
+                            site: "SEAGATE_JIRA",
+                            comment: "{panel:bgColor=#c1c7d0}"+
+                                "h2. ${component} - ${branch} branch build pipeline SUCCESS\n"+
+                                "h3. Build Info:  \n"+
+                                    author+
+                                        "* Component Build  :  ${BUILD_NUMBER} \n"+
+                                        "* Release Build    :  ${release_build}  \n\n  "+
+                                "h3. Artifact Location  :  \n"+
+                                    "*  "+"${release_build_location} "+"\n"+
+                                    "{panel}",
+                            failOnError: false,
+                            auditLog: false
+                        )
+                        //def jiraFileds = jiraGetIssue idOrKey: issue, site: "SEAGATE_JIRA", failOnError: false
+                        //if(jiraFileds.data != null){
+                        //def labels_data =  jiraFileds.data.fields.labels + "cortx_stable_b${release_build}"
+                        //jiraEditIssue idOrKey: issue, issue: [fields: [ labels: labels_data ]], site: "SEAGATE_JIRA", failOnError: false
+                        //}
+                    }
+                }
+        }
+    }
+    }
 
-	post {
-		always {
-			script {    	
-				echo 'Cleanup Workspace.'
-				deleteDir() /* clean up our workspace */
+    post {
+        always {
+            script {        
+                echo 'Cleanup Workspace.'
+                deleteDir() /* clean up our workspace */
 
-				env.release_build = (env.release_build != null) ? env.release_build : "" 
-				env.release_build_location = (env.release_build_location != null) ? env.release_build_location : ""
-				env.component = (env.component).toUpperCase()
-				env.build_stage = "${build_stage}"
+                env.release_build = (env.release_build != null) ? env.release_build : "" 
+                env.release_build_location = (env.release_build_location != null) ? env.release_build_location : ""
+                env.component = (env.component).toUpperCase()
+                env.build_stage = "${build_stage}"
 
                 env.vm_deployment = (env.deployVMURL != null) ? env.deployVMURL : "" 
                 if ( env.deployVMStatus != null && env.deployVMStatus != "SUCCESS" && manager.build.result.toString() == "SUCCESS" ) {
@@ -198,8 +198,8 @@ pipeline {
                 }else {
                    echo 'Skipping Notification....' 
                 }
-			}
-		}	
+            }
+        }    
     }
 }
 
