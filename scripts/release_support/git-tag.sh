820de9e22e70536c16559137cf3e87c9639eda37
cortx-admin <cortx-application@seagate.com>
2021-07-23 11:35:00 -0600

    Updated git-tag.sh
    Signed-off-by: cortx-admin <cortx-application@seagate.com>

--- scripts/release_support/git-tag.sh
+++ scripts/release_support/git-tag.sh
@@ -31,22 +31,10 @@ declare -A COMPONENT_LIST=(
                         [cortx-prereq]="https://$PASSWD@github.com/Seagate/cortx-re.git"
                 )
 
-declare -A REPO_LIST=(
-                        [cortx-s3server]="https://$PASSWD@api.github.com/repos/Seagate/cortx-s3server/releases"
-                        [cortx-motr]="https://$PASSWD@api.github.com/repos/Seagate/cortx-motr/releases"
-                        [cortx-hare]="https://$PASSWD@api.github.com/repos/Seagate/cortx-hare/releases"
-                        [cortx-ha]="https://$PASSWD@api.github.com/repos/Seagate/cortx-ha/releases"
-                        [cortx-prvsnr]="https://$PASSWD@api.github.com/repos/Seagate/cortx-prvsnr/releases"
-                        [cortx-sspl]="https://$PASSWD@api.github.com/repos/Seagate/cortx-monitor/releases"
-                        [cortx-csm_agent]="https://$PASSWD@api.github.com/repos/Seagate/cortx-manager/releases"
-                        [cortx-csm_web]="https://$PASSWD@api.github.com/repos/Seagate/cortx-management-portal/releases"
-                        [cortx-py-utils]="https://$PASSWD@api.github.com/repos/Seagate/cortx-utils/releases"
-                        [cortx-prereq]="https://$PASSWD@api.github.com/repos/Seagate/cortx-re/releases"
-                )
 
-                        git config --global user.email "cortx-application@seagate.com"
-                        git config --global user.name "cortx-admin"
-                        wget -q $RELEASE_INFO_URL -O RELEASE.INFO
+        git config --global user.email "cortx-application@seagate.com"
+        git config --global user.name "cortx-admin"
+        wget -q $RELEASE_INFO_URL -O RELEASE.INFO
         
 	for component in "${!COMPONENT_LIST[@]}"
 
@@ -92,31 +80,4 @@ declare -A REPO_LIST=(
 
                 popd
         done
-                git config --global user.email "cortx-application@seagate.com"
-                git config --global user.name "cortx-admin"
-
-                if [ "$REL_NAME" != "" ]; then
-                        echo "Release will be set for all the components";
-
-                for component in "${!REPO_LIST[@]}"
-
-                do
-                        if [ "$component" == cortx-hare ] || [ "$component" == cortx-sspl ] || [ "$component" == cortx-ha ] || [ "$component" == cortx-py-utils ] || [ "$component" == cortx-prereq ] || [ "$component" == "cortx-csm_agent" ] || [ "$component" == "cortx-csm_web" ]; then
-
-                        echo "Component: "$component" , Repo:  "${REPO_LIST[$component]}"";
-
-                        curl -H "Accept: application/vnd.github.v3+json"  "${REPO_LIST[$component]}" -d '{"tag_name":""$GIT_TAG"", "name":""$REL_NAME""}';
-                        else
-                        echo "Release is not created.";
-                        fi
-
-                if [ "$DEBUG" = true ]; then
-                        curl -X DELETE -H "Accept: application/vnd.github.v3+json"  "${REPO_LIST[$component]}"/""$REL_ID"";
-                else
-                        echo "Run in Debug mode if Release needs to be deleted";
-                fi
-                done
-                else
-                        echo "Please pass the value of Release";
-                fi
 
