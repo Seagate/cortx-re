2f365162edbac22413235e829f4b3538722d1c19
Balaji Ramachandran <balaji.ramachandran@seagate.com>
2021-07-23 12:21:41 -0600

    Updated tagging.groovy
    Signed-off-by: Balaji Ramachandran <balaji.ramachandran@seagate.com>

--- jenkins/automation/tagging.groovy
+++ jenkins/automation/tagging.groovy
@@ -10,26 +10,26 @@ pipeline {
         parameters {
                 string(name: 'RELEASE_INFO_URL', defaultValue: '', description: 'RELEASE BUILD')
                 string(name: 'GIT_TAG', defaultValue: '', description: 'Tag Name')
-				string(name: 'TAG_MESSAGE', defaultValue: '', description: 'Tag Message')
-				booleanParam(name: 'DEBUG', defaultValue: false, description: 'Select this if you want to Delete the current Tag')
+		string(name: 'TAG_MESSAGE', defaultValue: '', description: 'Tag Message')
+		booleanParam(name: 'DEBUG', defaultValue: false, description: 'Select this if you want to Delete the current Tag')
             }
 		
 		
         stages {
 		
-		stage('Checkout Script') {
+	stage('Checkout Script') {
             steps {             
                 script {
                     checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/cortx-re']]])                
                 }
             }
         }
-		stage ("Tagging") {
-			steps {
-				script { build_stage=env.STAGE_NAME }
-					script { 
-						withCredentials([usernamePassword(credentialsId: 'cortx-admin-github', passwordVariable: 'PASSWD', usernameVariable: 'USER_NAME')]) {
-						sh label: 'Git-Tagging', script: '''sh scripts/release_support/git-tag.sh'''  
+	stage ("Tagging") {
+	    steps {
+		script { build_stage=env.STAGE_NAME }
+			script { 
+				withCredentials([usernamePassword(credentialsId: 'cortx-admin-github', passwordVariable: 'PASSWD', usernameVariable: 'USER_NAME')]) {
+				sh label: 'Git-Tagging', script: '''sh scripts/release_support/git-tag.sh'''  
 					}
 				}			
 			}
