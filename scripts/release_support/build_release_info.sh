6168797053d7ff4d705035f33b074f871dccce86
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-07-30 05:43:06 -0600

    Sort rpm names
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/build_release_info.sh
+++ scripts/release_support/build_release_info.sh
@@ -90,7 +90,7 @@ do
 echo "Adding rpms from $dir"
 cat <<EOF >> $FILE_NAME
     "$dir":
-$(find -L ./"$dir" -type f -not -path "./lustre/custom/tcp/*" -name '*.rpm' -or -name '*.tar.xz' -or -name '*.tgz' | awk -F '/' '{print $NF}' | awk '{ print "       - \""$1"\""}')
+$(find -L ./"$dir" -type f -not -path "./lustre/tcp/*" -name '*.rpm' -or -name '*.tar.xz' -or -name '*.tgz' | awk -F '/' '{print $NF}' | sort | awk '{ print "       - \""$1"\""}')
 EOF
 done
 }
