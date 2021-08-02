e6097700a7823f817e38e411fb2c1b06dfa8a462
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-08-02 13:56:45 +0530

    chore: update interface info tasks
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

---
+++ scripts/deployment/roles/factory_vm_deployment/files/itfinfo.py
@@ -0,0 +1,29 @@
+import netifaces as ni
+import re
+
+ipregex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+count = 0
+skipitf = False
+print(ni.interfaces())
+for iface in ni.interfaces():
+    if iface == 'lo' or iface.startswith('vbox') or skipitf:
+        skipitf = False
+        continue
+    try:
+      ipaddr = ni.ifaddresses(iface)[ni.AF_INET][0]['addr']
+      if ipaddr and re.search(ipregex, ipaddr) and count < 3:
+        count += 1
+        netmask = ni.ifaddresses(iface)[ni.AF_INET][0]['netmask']
+        if count == 1:
+          gateway = ni.gateways()['default'][ni.AF_INET][0]
+          print(iface+","+ipaddr+","+netmask+","+gateway)
+        elif count == 2:
+          print(iface+","+ipaddr+","+netmask)
+          skipitf = True
+        else:
+          print(iface+","+ipaddr+","+netmask)
+    except:
+      pass
+if count < 3:
+  print("FAILURE: No sufficient interfaces!! Minimum 3 interfaces with IP's are required.")
+  exit(1)
