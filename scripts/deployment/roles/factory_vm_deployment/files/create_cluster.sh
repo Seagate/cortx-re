38f22945ed53a4a062a31fc77a9b13a489da19d9
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-08-04 13:03:24 +0530

    chore: update command line vars
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- scripts/deployment/roles/factory_vm_deployment/files/create_cluster.sh
+++ scripts/deployment/roles/factory_vm_deployment/files/create_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/expect -f
 # ./create_cluster.sh node1 node2 node3 pass build_url
-spawn cortx_setup cluster create [lindex $argv 0] [lindex $argv 1] [lindex $argv 2] --name cortx_cluster --site_count 1 --storageset_count 1 --virtual_host [lindex $argv 4] --target_build [lindex $argv 5] 
+spawn cortx_setup cluster create [lindex $argv 0] [lindex $argv 1] [lindex $argv 2] --name cortx_cluster --site_count 1 --storageset_count 1 --virtual_host [lindex $argv 3] --target_build [lindex $argv 4] 
 set timeout 1200
 expect {
     timeout {
@@ -9,7 +9,7 @@ expect {
     }
 
     "assword:" {
-        send -- "[lindex $argv 3]\r"
+        send -- "[lindex $argv 5]\r"
         exp_continue
     }
 }
