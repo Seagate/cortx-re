f6902d6fc8b570be30de839fde13f420ed679d11
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-08-04 11:27:39 +0530

    chore: add create_cluster script
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

---
+++ scripts/deployment/roles/factory_vm_deployment/files/create_cluster.sh
@@ -0,0 +1,15 @@
+#!/bin/expect -f
+# ./create_cluster.sh node1 node2 node3 pass build_url
+spawn cortx_setup cluster create [lindex $argv 0] [lindex $argv 1] [lindex $argv 2] --name cortx_cluster --site_count 1 --storageset_count 1 --virtual_host [lindex $argv 4] --target_build [lindex $argv 5] 
+set timeout 1200
+expect {
+    timeout {
+        puts "Connection timed out"
+        exit 1
+    }
+
+    "assword:" {
+        send -- "[lindex $argv 3]\r"
+        exp_continue
+    }
+}
