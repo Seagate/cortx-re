bbad09fae0c5352859041d8a8997ebdb3a2efa19
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-08-06 14:38:22 +0530

    chore: update command
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/internal-ci/centos-7.9.2009/pr/cortx_deployment/motr.groovy
+++ jenkins/internal-ci/centos-7.9.2009/pr/cortx_deployment/motr.groovy
@@ -133,7 +133,6 @@ EOF
                         yum clean all;rm -rf /var/cache/yum
                         yum install cortx-py-utils -y
                         yum --disablerepo=* localinstall /root/rpmbuild/RPMS/x86_64/cortx-motr-*.rpm -y
-                        yum --disablerepo=* localinstall /root/rpmbuild/RPMS/x86_64/cortx-motr-devel-*.rpm -y
                     '''
 
                         sh label: 'Build', returnStatus: true, script: '''
