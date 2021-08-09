5ce9b46fd9b90ed8857dba2bd56d5ad217a10504
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-08-09 17:11:10 +0530

    chore: update branch
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/deployment/vm_deployment/cortx_stack/cortx_factory_deploy_vm_1n.groovy
+++ jenkins/deployment/vm_deployment/cortx_stack/cortx_factory_deploy_vm_1n.groovy
@@ -59,7 +59,7 @@ pipeline {
                         echo "-----------------------------------------------------------"
                     """
                     dir('cortx-re') {
-                        checkout([$class: 'GitSCM', branches: [[name: '*/vm-factory-deployment']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/gauravchaudhari02/cortx-re']]])
+                        checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/cortx-re']]])
                     }
 
                     if ( NODE1.isEmpty() ) {
