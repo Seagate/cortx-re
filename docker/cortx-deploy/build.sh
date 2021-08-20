faf7d79861529004eaa92b28f3d1d83ebea230c3
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-08-20 01:09:40 -0600

    Remove duplicate repo names, Use no-cache option
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- docker/cortx-deploy/build.sh
+++ docker/cortx-deploy/build.sh
@@ -55,7 +55,7 @@ else
 	export TAG=$VERSION-$DOCKER_BUILD_BUILD
 fi
 
-docker-compose -f docker/cortx-deploy/docker-compose.yml build --force-rm  --compress --build-arg GIT_HASH="$(git rev-parse --short HEAD)" --build-arg BUILD_URL=$BUILD_URL  cortx-all
+docker-compose -f docker/cortx-deploy/docker-compose.yml build --force-rm --no-cache --compress --build-arg GIT_HASH="$(git rev-parse --short HEAD)" --build-arg BUILD_URL=$BUILD_URL  cortx-all
 
 if [ $DOCKER_PUSH == "yes" ];then
         echo "Pushing Docker image to GitHub Container Registry"
