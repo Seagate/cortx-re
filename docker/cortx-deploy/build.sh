3f5962ad0945dd1ebffe7e3823a2da24969146ae
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-08-24 06:51:22 -0600

    Add latest tag creation
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- docker/cortx-deploy/build.sh
+++ docker/cortx-deploy/build.sh
@@ -20,15 +20,17 @@
 
 set -e -o pipefail
 
-usage() { echo "Usage: $0 [-b build url] [ -p push docker-image to GHCR yes/no. Default yes ]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-b build url] [-p push docker-image to GHCR yes/no. Default yes] [ -t tag latest yes/no. Default no" ] 1>&2; exit 1; }
 
 VERSION=2.0.0
 DOCKER_PUSH=yes
+TAG_LATEST=no
 
-while getopts "b:p:" opt; do
+while getopts "b:p:t:" opt; do
     case $opt in
         b ) BUILD_URL=$OPTARG;;
         p ) DOCKER_PUSH=$OPTARG;;
+        t ) TAG_LATEST=$OPTARG;;
         h ) usage
         exit 0;;
         *) usage
@@ -55,12 +57,19 @@ else
 	export TAG=$VERSION-$DOCKER_BUILD_BUILD
 fi
 
-docker-compose -f docker/cortx-deploy/docker-compose.yml build --force-rm --no-cache --compress --build-arg GIT_HASH="$(git rev-parse --short HEAD)" --build-arg BUILD_URL=$BUILD_URL  cortx-all
+docker-compose -f docker/cortx-deploy/docker-compose.yml build --force-rm --compress --build-arg GIT_HASH="$(git rev-parse --short HEAD)" --build-arg BUILD_URL=$BUILD_URL  cortx-all
 
-if [ $DOCKER_PUSH == "yes" ];then
+if [ "$DOCKER_PUSH" == "yes" ];then
         echo "Pushing Docker image to GitHub Container Registry"
 	docker-compose -f docker/cortx-deploy/docker-compose.yml push cortx-all
 else
 	echo "Docker Image push skipped"
 fi
 popd
+
+if [ "$TAG_LATEST" == "yes" ];then
+	echo "Tagging generated image as latest"
+        docker tag $(docker images ghcr.io/seagate/cortx-all --format='{{.Repository}}:{{.Tag}}' | head -1) ghcr.io/seagate/cortx-all:$(echo $TAG | sed 's|'$DOCKER_BUILD_BUILD'|latest|g')
+else 
+	echo "Latest tag creation skipped"
+fi
