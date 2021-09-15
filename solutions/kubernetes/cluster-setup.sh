ccf805504f23487820fa0c66a2b0aeea139f6acd
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-09-15 16:17:03 +0530

    chore: add cluster setup script
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

---
+++ solutions/kubernetes/cluster-setup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
+usage(){
+    cat << HEREDOC
+Usage : $0 [--prepare]
+where,
+    --prepare - Install prerequisites on nodes for kubernetes setup
+HEREDOC
+}
+
+install_prerequisites(){
+    # disable swap 
+    sudo swapoff -a
+    # keeps the swaf off during reboot
+    sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+    # disable selinux
+    setenforce 0
+    sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+    systemctl stop firewalld
+    systemctl disable firewalld
+    systemctl mask --now firewalld
+
+    cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+    yum install kubeadm docker -y
+
+    sudo systemctl enable docker
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker
+    echo "Docker Runtime Configured Successfully"
+
+    systemctl enable kubelet
+    systemctl restart kubelet
+    echo "kubelet Configured Successfully"
+
+}
+
+ACTION="$1"
+if [ -z "$ACTION" ]; then
+    echo "ERROR : No option provided"
+    usage
+    exit 1
+fi
+
+case $ACTION in
+    --prepare) 
+        install_prerequisites
+    ;;
+    *)
+        echo "ERROR : Please provide valid option"
+        usage
+        exit 1
+    ;;    
+esac
