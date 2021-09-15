f60182d81a4533079abf87d68b07fce317b02a93
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-09-15 00:11:09 -0600

    Removed Provisioner packages
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- docker/cortx-deploy/build.sh
+++ docker/cortx-deploy/build.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
 
 set -e -o pipefail
 
-usage() { echo "Usage: $0 [-b build ]  [-p push docker-image to GHCR yes/no. Default no] [ -t tag latest yes/no. Default no" ] [ -h print help] 1>&2; exit 1; }
+usage() { 
+echo "Generate cortx-all docker image from provided CORTX release build"
+echo "Usage: $0 [ -b build ] [ -p push docker-image to GHCR yes/no. Default no] [ -t tag latest yes/no. Default no" ] [ -h print help message ] 1>&2; exit 1; }
 
 VERSION=2.0.0
 DOCKER_PUSH=no
