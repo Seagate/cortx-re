006f686062cb591700369fd108908e3962b180cf
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-09-17 12:11:33 +0530

    Add shopt
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/provisioner.groovy
+++ jenkins/custom-ci/centos-7.9.2009/provisioner.groovy
@@ -83,6 +83,7 @@ pipeline {
                 sh label: 'Copy RPMS', script: '''
                     mkdir -p $build_upload_dir
                     cp /root/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir
+                    shopt -s extglob
                     cp ./dist/!(*.src.rpm|*.tar.gz) /root/test/ $build_upload_dir
                     createrepo -v --update $build_upload_dir
                 '''
