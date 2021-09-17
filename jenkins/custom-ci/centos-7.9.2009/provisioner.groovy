240a118405030afb6483ba935adc75154222ccd3
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-09-17 12:08:36 +0530

    Copy new packages
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/provisioner.groovy
+++ jenkins/custom-ci/centos-7.9.2009/provisioner.groovy
@@ -6,19 +6,19 @@ pipeline {
         }
     }
     
-	parameters {  
+    parameters {  
         string(name: 'PRVSNR_URL', defaultValue: 'https://github.com/Seagate/cortx-prvsnr', description: 'Repository URL for Provisioner.')
-		string(name: 'PRVSNR_BRANCH', defaultValue: 'stable', description: 'Branch for Provisioner.')
+        string(name: 'PRVSNR_BRANCH', defaultValue: 'stable', description: 'Branch for Provisioner.')
         string(name: 'CUSTOM_CI_BUILD_ID', defaultValue: '0', description: 'Custom CI Build Number')
-	}	
+    }    
 
-	environment {
+    environment {
         component = "provisioner"
         branch = "custom-ci"
         os_version = "centos-7.9.2009"
         release_dir = "/mnt/bigstorage/releases/cortx"
         release_tag = "custom-build-$CUSTOM_CI_BUILD_ID"
-		build_upload_dir = "$release_dir/github/integration-custom-ci/$os_version/$release_tag/cortx_iso"
+        build_upload_dir = "$release_dir/github/integration-custom-ci/$os_version/$release_tag/cortx_iso"
     }
 
     options {
@@ -31,20 +31,20 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-				script { build_stage = env.STAGE_NAME }
-					checkout([$class: 'GitSCM', branches: [[name: "${PRVSNR_BRANCH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: "${PRVSNR_URL}"]]])
+                script { build_stage = env.STAGE_NAME }
+                    checkout([$class: 'GitSCM', branches: [[name: "${PRVSNR_BRANCH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: "${PRVSNR_URL}"]]])
             }
         }
 
         stage('Build') {
             steps {
-				script { build_stage = env.STAGE_NAME }
+                script { build_stage = env.STAGE_NAME }
                 sh encoding: 'utf-8', label: 'Provisioner RPMS', returnStdout: true, script: """
                     rm -rf /etc/yum.repos.d/CentOS-*
                     sh ./devops/rpms/buildrpm.sh -g \$(git rev-parse --short HEAD) -e 2.0.0 -b ${CUSTOM_CI_BUILD_ID}
                 """
                 sh encoding: 'utf-8', label: 'Provisioner CLI RPMS', returnStdout: true, script: """
-				    sh ./cli/buildrpm.sh -g \$(git rev-parse --short HEAD) -e 2.0.0 -b ${CUSTOM_CI_BUILD_ID}
+                    sh ./cli/buildrpm.sh -g \$(git rev-parse --short HEAD) -e 2.0.0 -b ${CUSTOM_CI_BUILD_ID}
                 """
 
                 sh encoding: 'UTF-8', label: 'cortx-setup', script: """
@@ -54,38 +54,39 @@ pipeline {
                     echo "node_cli package creation is not implemented"
                 fi
                 """
-				
-				sh encoding: 'UTF-8', label: 'api', script: '''
-					bash ./devops/rpms/api/build_python_api.sh -vv --out-dir /root/rpmbuild/RPMS/x86_64/ --pkg-ver ${CUSTOM_CI_BUILD_ID}_git$(git rev-parse --short HEAD)
-				'''
+                
+                sh encoding: 'UTF-8', label: 'api', script: '''
+                    bash ./devops/rpms/api/build_python_api.sh -vv --out-dir /root/rpmbuild/RPMS/x86_64/ --pkg-ver ${CUSTOM_CI_BUILD_ID}_git$(git rev-parse --short HEAD)
+                '''
 
                 sh encoding: 'UTF-8', label: 'cortx-setup', script: '''
                 if [ -f "./devops/rpms/lr-cli/build_python_cortx_setup.sh" ]; then
-					bash ./devops/rpms/lr-cli/build_python_cortx_setup.sh -vv --out-dir /root/rpmbuild/RPMS/x86_64/ --pkg-ver ${CUSTOM_CI_BUILD_ID}_git$(git rev-parse --short HEAD)
+                    bash ./devops/rpms/lr-cli/build_python_cortx_setup.sh -vv --out-dir /root/rpmbuild/RPMS/x86_64/ --pkg-ver ${CUSTOM_CI_BUILD_ID}_git$(git rev-parse --short HEAD)
                 else
                     echo "cortx-setup package creation is not implemented"
                 fi        
-				'''
+                '''
 
                 sh encoding: 'UTF-8', label: 'cortx-provisioner', script: '''
                 if [ -f "./jenkins/build.sh" ]; then
-					bash ./jenkins/build.sh -v 2.0.0 -b ${CUSTOM_CI_BUILD_ID}
+                    bash ./jenkins/build.sh -v 2.0.0 -b ${CUSTOM_CI_BUILD_ID}
                 else
                     echo "cortx-provisioner package creation is not implemented"
                 fi        
-				'''
+                '''
             }
         }
 
-		stage ('Upload') {
-			steps {
-				script { build_stage = env.STAGE_NAME }
-				sh label: 'Copy RPMS', script: '''
+        stage ('Upload') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Copy RPMS', script: '''
                     mkdir -p $build_upload_dir
                     cp /root/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir
+                    cp ./dist/!(*.src.rpm|*.tar.gz) /root/test/ $build_upload_dir
                     createrepo -v --update $build_upload_dir
-				'''
-			}
-		}
-	}
+                '''
+            }
+        }
+    }
 }
