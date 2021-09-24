4b94fa56fc6cbf2d6d3ebafd527b36ea5a9c9abb
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-09-24 15:00:47 +0530

    chore: update email template
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/automation/branch-diff.groovy
+++ jenkins/automation/branch-diff.groovy
@@ -48,6 +48,7 @@ pipeline {
 
                 env.source_branch = "${SOURCE_BRANCH}"
                 env.target_branch = "${TARGET_BRANCH}"
+                env.changeset = readFile(file: 'artifacts/CHANGESET.txt')
                 toEmail = "gaurav.chaudhari@seagate.com"
 
                 emailext (
