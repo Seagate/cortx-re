1419a19c047cdd3dad5b48800911e59d7c7f5b3c
Saumitra Kulkarni <saumitra.kulkarni@seagate.com>
2021-09-27 15:24:21 +0530

    Update entrpoint (#596)
    Signed-off-by: Saumitra Kulkarni <saumitra.kulkarni@seagate.com>

--- docker/symas-openldap/entrypoint.sh
+++ docker/symas-openldap/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-USAGE="USAGE: bash $(basename "$0") [--config_dir <config_path>] [--data_dir <data_path>]
+USAGE="USAGE: bash $(basename "$0") [--rootdnpassword <rootdnpassword>][--config_dir <config_path>] [--data_dir <data_path>]
       [--help | -h]
 Configure LDAP Directories.
 
@@ -14,12 +14,16 @@ set -e
 # Default config and data directories
 CONFIG_PATH="/etc/cortx"
 DATA_PATH="/var/data/cortx"
+ROOTDN_PASSWORD="seagate"
 
 echo "Running entrypoint.sh script"
 
 while test $# -gt 0
 do
   case "$1" in
+    --rootdnpassword ) shift;
+        ROOTDN_PASSWORD=$1
+        ;;
     --config_dir ) shift;
         CONFIG_PATH=$1
         ;;
@@ -33,8 +37,6 @@ do
   esac
   shift
 done
-#echo "$CONFIG_PATH"
-#echo "$DATA_PATH"
 
 # Check if CONFIG_PATH exists else create
 if [ ! -d "$CONFIG_PATH/openldap/" ]           
@@ -48,11 +50,14 @@ then
         mkdir -p $DATA_PATH/ldap
 fi
 
+perform_base_config=false
+
 # Checks if ldap configuration already on pvc
 # If not present copy contents of /etc/openldap to CONFIG_PATH
 if [ -z "$(ls -A $CONFIG_PATH/openldap/)" ]
 then
 	cp -r /etc/openldap/ $CONFIG_PATH
+        perform_base_config=true
 fi
 
 # Change permissions of pvc directory
@@ -61,5 +66,14 @@ chown -R ldap.ldap $CONFIG_PATH/openldap/
 # Start slapd
 /usr/sbin/slapd -F $CONFIG_PATH/openldap/slapd.d -u ldap -h 'ldapi:/// ldap:///'
 
-# This is to keep container running
-sleep infinity
+# Check if configuration already present
+# Perform base config
+if [ "$perform_base_config" == true ]
+then
+  python3 -c "import sys;sys.path.insert(1, '/usr/lib/python3.6/site-packages/cortx/utils/setup/openldap/');from base_configure_ldap import BaseConfig;BaseConfig.perform_base_config('$ROOTDN_PASSWORD',True,{'base_dn':'dc=seagate,dc=com' , 'bind_base_dn':'cn=admin,dc=seagate,dc=com', 'install_dir': '$CONFIG_PATH', 'data_dir': '$DATA_PATH'})" 
+fi
+
+kill -15 $(pidof slapd)
+
+# Start slapd
+/usr/sbin/slapd -F $CONFIG_PATH/openldap/slapd.d -u ldap -h 'ldapi:/// ldap:///' -d 0
