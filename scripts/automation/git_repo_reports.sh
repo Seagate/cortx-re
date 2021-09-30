35e807e731e0deda01b18e67147daf67bb67626c
AbhijitPatil1992 <abhijit.patil@seagate.com>
2021-09-30 02:50:44 -0600

    Checking dco error
    Signed-off-by: AbhijitPatil1992 <abhijit.patil@seagate.com>

--- scripts/automation/git_repo_reports.sh
+++ scripts/automation/git_repo_reports.sh
@@ -17,7 +17,6 @@
 # For any questions about this software or licensing,
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
-
 Repo_Name=$(echo "$1"|tr "," " ")
 GitUserName=$2
 file_name=Repository_reports.csv
