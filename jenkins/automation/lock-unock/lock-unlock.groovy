c9772b303a28e83d28c1c446297aa969d7fdb3d0
Venkatesh K <venkatesh.k@seagate.com>
2021-10-04 17:11:52 +0530

    Added the lock-unlock script
    Signed-off-by: Venkatesh K <venkatesh.k@seagate.com>

---
+++ jenkins/automation/lock-unock/lock-unlock.groovy
@@ -0,0 +1,68 @@
+pipeline {
+	agent {
+        node {
+           // label "docker-${os_version}-node"
+           label "cortx-test-ssc-vm-4090"
+        }
+    }
+    parameters {
+        string(
+            defaultValue: 'centos-7.9.2009',
+            name: 'os_version',
+            description: 'OS version of the build',
+            trim: true
+        )
+        choice(
+            name: 'flag',
+            choices: ['Lock', 'Unlock'],
+            description: 'Branch Name'
+        )
+        extendedChoice(
+            defaultValue: 'main',
+            description: 'Branch name',
+            multiSelectDelimiter: ',',
+            name: 'branch',
+            quoteValue: false,
+            saveJSONParameterToFile: false,
+            type: 'PT_CHECKBOX',
+            value:'stable,main,cortx-1.0,kubernetes',
+            visibleItemCount: 4
+        )
+        extendedChoice(
+            defaultValue: 'alex-test',
+            description: 'Repo name',
+            multiSelectDelimiter: ',',
+            name: 'repo',
+            quoteValue: false,
+            saveJSONParameterToFile: false,
+            type: 'PT_CHECKBOX',
+            value:'alex-test,cortx-re,cortx-manager,cortx-management-portal,cortx-ha,cortx-hare,cortx-motr,cortx-prvsnr,cortx-monitor,cortx-s3server,cortx-utils',
+            visibleItemCount: 10
+        )
+
+    }
+    environment {
+        GIT_CRED = credentials('github-access')
+    }
+
+    stages {
+        stage('Checkout source code') {
+                steps {
+                    script {
+                        checkout([$class: 'GitSCM', branches: [[name: 'main']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'github-access', url: 'https://github.com/seagate/cortx-re/']]])
+                    }
+                }
+        }
+
+        stage('Running python script') {
+            steps{
+                echo "Executing the script..."
+                echo "Branchs: ${branch}, Repos: ${repo}"
+                sh "python3 ./scripts/env_setup/lock-unlock.py -f ${flag} -r ${repo} -b ${branch} -t ${GIT_CRED_PSW}"
+            }
+        }
+
+	}
+
+}
+
