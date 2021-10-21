af3844c7b1dd99452156949929244d60f804ad1d
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-10-21 13:38:25 +0530

    Increase timeout
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/automation/kubernetes/setup-third-party.groovy
+++ jenkins/automation/kubernetes/setup-third-party.groovy
@@ -6,7 +6,7 @@ pipeline {
     }
     
     options {
-        timeout(time: 30, unit: 'MINUTES')
+        timeout(time: 180, unit: 'MINUTES')
         timestamps()
         buildDiscarder(logRotator(daysToKeepStr: '30', numToKeepStr: '30'))
         ansiColor('xterm')
