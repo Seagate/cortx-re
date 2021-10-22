c30238838f4871cdf24e3167c7b095dbf761af5b
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-10-22 13:43:12 +0530

    Use nightly-k8-custom-ci pipeline
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/automation/kubernetes/nightly-cortx-image.groovy
+++ jenkins/automation/kubernetes/nightly-cortx-image.groovy
@@ -29,7 +29,7 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 script {
-                def custom_ci = build job: '../GitHub-custom-ci-builds/centos-7.9/cortx-all-image-custom-ci', wait: true,
+                def custom_ci = build job: '../GitHub-custom-ci-builds/centos-7.9/nightly-k8-custom-ci', wait: true,
                     parameters: [
                                 string(name: 'CSM_AGENT_BRANCH', value: "${COMPONENT_BRANCH}"),
                                 string(name: 'CSM_WEB_BRANCH', value: "${COMPONENT_BRANCH}"),
@@ -56,7 +56,7 @@ pipeline {
                     try {    
                         def docker_image_build = build job: 'cortx-all-docker-image', wait: true,
                             parameters: [
-                                string(name: 'CORTX_RE_URL', value: "https://github.com/shailesh-vaidya/cortx-re"),
+                                string(name: 'CORTX_RE_URL', value: "https://github.com/Seagate/cortx-re"),
                                 string(name: 'CORTX_RE_BRANCH', value: "kubernetes"),
                                 string(name: 'BUILD', value: "custom-build-${env.custom_ci_build_id}"),
                                 string(name: 'EMAIL_RECIPIENTS', value: "${EMAIL_RECIPIENTS}"),
