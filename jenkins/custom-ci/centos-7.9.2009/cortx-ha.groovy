2455f4ea0a9da3894b7a35a0ae7e27a150ce5140
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-10-28 10:43:27 +0530

    EOS-24404 Jenkins Pipeline for Docker Image Tagging, Use Separate location for K8 Builds. (#646)
    * Add k8-custom-ci
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Use k8 third-party by-default
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Use kubernetes branch as default
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Copy custom generated packages to k8 directory
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Use k8 branch to install python packages
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * use k8 branch for cortx-py-utils
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Fix cp command syntax issue
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Use nightly-k8-custom-ci pipeline
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * use kubernetes branch by default
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Use kubernetes-build build name
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Use custom re branch
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Add jo to tag image
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Fix job errors
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Fix syntax error
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Test fix
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Test fix
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Test fix
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Fix syntax issue
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Delete builds after two months only
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Correct build path
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Increased timeouts
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/cortx-ha.groovy
+++ jenkins/custom-ci/centos-7.9.2009/cortx-ha.groovy
@@ -44,7 +44,7 @@ pipeline {
             }
         }
     
-    
+
         // Install third-party dependencies. This needs to be removed once components move away from self-contained binaries 
         stage('Install python packages') {
             steps {
@@ -58,8 +58,8 @@ timeout: 60
 index-url: http://cortx-storage.colo.seagate.com/releases/cortx/third-party-deps/python-deps/python-packages-2.0.0-latest/
 trusted-host: cortx-storage.colo.seagate.com
 EOF
-                pip3 install -r https://raw.githubusercontent.com/Seagate/cortx-utils/main/py-utils/python_requirements.txt
-                pip3 install -r https://raw.githubusercontent.com/Seagate/cortx-utils/main/py-utils/python_requirements.ext.txt
+                pip3 install -r https://raw.githubusercontent.com/Seagate/cortx-utils/kubernetes/py-utils/python_requirements.txt
+                pip3 install -r https://raw.githubusercontent.com/Seagate/cortx-utils/kubernetes/py-utils/python_requirements.ext.txt
                 rm -rf /etc/pip.conf
             '''        
             }
@@ -72,7 +72,7 @@ EOF
                 sh label: '', script: '''
                     yum-config-manager --add-repo=http://cortx-storage.colo.seagate.com/releases/cortx/github/integration-custom-ci/$os_version/$release_tag/cortx_iso/
                     yum-config-manager --save --setopt=cortx-storage*.gpgcheck=1 cortx-storage* && yum-config-manager --save --setopt=cortx-storage*.gpgcheck=0 cortx-storage*
-                    
+
                     yum clean all && rm -rf /var/cache/yum
                     pushd $component
                         yum erase python36-PyYAML -y
