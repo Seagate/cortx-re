f766d3cff604e35a0f9c7d64ac8616f8ad045aab
AbhijitPatil1992 <abhijit.patil@seagate.com>
2021-11-08 11:58:34 +0000

    Added change logs generation groovy script
    Signed-off-by: AbhijitPatil1992 <abhijit.patil@seagate.com>

---
+++ jenkins/automation/changelog-generation.groovy
@@ -0,0 +1,49 @@
+pipeline {
+    agent {
+		node {
+			label 'docker-cp-centos-7.8.2003-node'
+		}
+	}
+	
+	options {
+		timeout(time: 60, unit: 'MINUTES')
+        ansiColor('xterm') 
+	}
+	
+	parameters {
+        string(name: 'BUILD_FROM', defaultValue: '', description: '''Please provide full build url till RELEASE.INFO. And give proper build url here from where you want to check logs.
+ <h6> e.g. http://cortx-storage.colo.seagate.com/releases/cortx/github/stable/centos-7.9.2009/105/dev/RELEASE.INFO </h6>''')
+        string(name: 'BUILD_TO', defaultValue: '', description: '''Please provide full build url till RELEASE.INFO. And give proper build url here till where you want to check logs.
+ <h6> e.g. http://cortx-storage.colo.seagate.com/releases/cortx/github/stable/centos-7.9.2009/110/dev/RELEASE.INFO </h6>''')
+    }
+
+    stages {	
+		stage ('Checkout Script') {
+			steps {
+			    checkout([$class: 'GitSCM', branches: [[name: '*/changelog-generation']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/AbhijitPatil1992/cortx-re']]])
+			}
+		}
+		
+		stage ('Build MANIFEST') {
+			steps {
+                withCredentials([string(credentialsId: 'shailesh-github-token', variable: 'ACCESS_TOKEN')]) {
+                    sh label: 'Build MANIFEST', script: """
+                        
+                        sh -x scripts/release_support/changelog.sh ${BUILD_FROM} ${BUILD_TO}
+                        cp /root/git_build_checkin_stats/clone/git-build-checkin-report.txt CHANGESET.txt 
+                    """
+                }   
+			}
+		}
+		
+	}
+
+    post {
+		always {
+            script {
+				archiveArtifacts artifacts: "CHANGESET.txt", onlyIfSuccessful: false, allowEmptyArchive: true
+            }
+        }
+    }
+}
+
