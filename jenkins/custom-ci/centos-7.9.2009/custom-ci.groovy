8701ed5b351d27bd2b52c2fcffdb2dd191d3d382
Nikhil Patil <nikhil.patil@seagate.com>
2021-11-15 03:13:29 +0000

    take repository name and python package directory name from jenkins job
    Signed-off-by: Nikhil Patil <nikhil.patil@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/custom-ci.groovy
+++ jenkins/custom-ci/centos-7.9.2009/custom-ci.groovy
@@ -52,6 +52,12 @@ pipeline {
 		string(name: 'CORTX_UTILS_URL', defaultValue: 'https://github.com/Seagate/cortx-utils', description: 'CORTX Utils Repository URL', trim: true)
 		string(name: 'CORTX_RE_BRANCH', defaultValue: 'main', description: 'Branch or GitHash for CORTX RE', trim: true)
 		string(name: 'CORTX_RE_URL', defaultValue: 'https://github.com/Seagate/cortx-re', description: 'CORTX RE Repository URL', trim: true)
+	
+		choice(
+			name: 'PYTHON_PACKAGE_VERSION',
+			choices: ['latest', 'stable', 'custom'],
+			description: 'Python packages version repo dir name'
+		)		
 
 		choice(
 			name: 'THIRD_PARTY_RPM_VERSION',
@@ -152,7 +158,9 @@ pipeline {
 														string(name: 'HARE_URL', value: "${HARE_URL}"),
 														string(name: 'HARE_BRANCH', value: "${HARE_BRANCH}"),
 														string(name: 'CUSTOM_CI_BUILD_ID', value: "${BUILD_NUMBER}"),
-														string(name: 'CORTX_UTILS_BRANCH', value: "${CORTX_UTILS_BRANCH}")
+														string(name: 'CORTX_UTILS_BRANCH', value: "${CORTX_UTILS_BRANCH}"),
+														string(name: 'CORTX_UTILS_REPO_OWNER', value: "${CORTX_UTILS_REPO_OWNER}"),
+														string(name: 'PYTHON_PACKAGE_VERSION', value: "${PYTHON_PACKAGE_VERSION}")
                                             		]
 							} catch (err) {
 								build_stage = env.STAGE_NAME 			
@@ -178,7 +186,9 @@ pipeline {
 									      	  string(name: 'HA_URL', value: "${HA_URL}"),
 									      	  string(name: 'HA_BRANCH', value: "${HA_BRANCH}"),
 										  string(name: 'CUSTOM_CI_BUILD_ID', value: "${BUILD_NUMBER}"),	
-										  string(name: 'CORTX_UTILS_BRANCH', value: "${CORTX_UTILS_BRANCH}")
+										  string(name: 'CORTX_UTILS_BRANCH', value: "${CORTX_UTILS_BRANCH}"),
+										  string(name: 'CORTX_UTILS_REPO_OWNER', value: "${CORTX_UTILS_REPO_OWNER}"),
+										  string(name: 'PYTHON_PACKAGE_VERSION', value: "${PYTHON_PACKAGE_VERSION}")
 									      ]
 							} catch (err) {
 								build_stage = env.STAGE_NAME
