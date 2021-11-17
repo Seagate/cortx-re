d483fa323096cf44887ebaad7a81dda30c1b7248
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2021-11-17 16:51:32 +0530

    chore: update destroy message
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- solutions/kubernetes/cortx-deploy.sh
+++ solutions/kubernetes/cortx-deploy.sh
@@ -87,13 +87,11 @@ function setup_cluster {
 
 
 function destroy-cluster(){
-
-        validation
-        generate_rsa_key
-        nodes_setup
-	
-	echo "---------------------------------------[ Destroying cluster ]----------------------------------------------"
+    validation
+    generate_rsa_key
+    nodes_setup
 	MASTER_NODE=$(head -1 "$HOST_FILE" | awk -F[,] '{print $1}' | cut -d'=' -f2)
+	echo "---------------------------------------[ Destroying cluster $MASTER_NODE ]----------------------------------------------"
         scp -q cortx-deploy-functions.sh functions.sh "$MASTER_NODE":/var/tmp/
         ssh -o 'StrictHostKeyChecking=no' "$MASTER_NODE" "export CORTX_SCRIPTS_REPO=$CORTX_SCRIPTS_REPO && export GITHUB_TOKEN=$GITHUB_TOKEN && export CORTX_SCRIPTS_BRANCH=$CORTX_SCRIPTS_BRANCH && /var/tmp/cortx-deploy-functions.sh --destroy"	
         ssh -o 'StrictHostKeyChecking=no' "$MASTER_NODE" '/var/tmp/cortx-deploy-functions.sh --status' | tee /var/tmp/cortx-cluster-status.txt
