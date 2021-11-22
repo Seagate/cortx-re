257cb65a18a2677363c2a6891dcf1b4a5ed8c05d
Nikhil Patil <nikhil.patil@seagate.com>
2021-11-22 22:47:10 +0000

    upgrade PyYAML library from  5.1 to 5.4
    Signed-off-by: Nikhil Patil <nikhil.patil@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/csm-agent.groovy
+++ jenkins/custom-ci/centos-7.9.2009/csm-agent.groovy
@@ -56,7 +56,7 @@ pipeline {
 				sh label: 'Install cortx-prereq package', script: """
 					export python_deps=$python_deps
 					pip3 uninstall pip -y && yum reinstall python3-pip -y && ln -s /usr/bin/pip3 /usr/local/bin/pip3
-					sh ./cortx-re/scripts/third-party-rpm/install-cortx-prereq.sh
+					sh ./cortx-re/scripts/third-party-rpm/install-cortx-prereq.sh -b http://cortx-storage.colo.seagate.com/releases/cortx/github/integration-custom-ci/$os_version/$release_tag/cortx_iso/
 				"""
 
 				sh label: 'Configure yum repositories', script: """
