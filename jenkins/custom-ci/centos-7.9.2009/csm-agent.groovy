efa98db33f085816159c2fb988554383e49f476b
nikhilpatil2995 <nikhil.patil@seagate.com>
2021-11-26 19:36:27 +0530

    set default version for third party python version and pass integration dir path from main custom-ci job
    Signed-off-by: nikhilpatil2995 <nikhil.patil@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/csm-agent.groovy
+++ jenkins/custom-ci/centos-7.9.2009/csm-agent.groovy
@@ -8,14 +8,15 @@ pipeline {
 	
 	environment { 
 		component = "csm-agent"
-        	branch = "custom-ci" 
-        	os_version = "centos-7.9.2009"
-        	release_dir = "/mnt/bigstorage/releases/cortx"
+        branch = "custom-ci" 
+        os_version = "centos-7.9.2009"
+        release_dir = "/mnt/bigstorage/releases/cortx"
 		release_tag = "custom-build-$CUSTOM_CI_BUILD_ID"
 		build_upload_dir = "$release_dir/github/integration-custom-ci/$os_version/$release_tag/cortx_iso"
 		python_deps = "${THIRD_PARTY_PYTHON_VERSION == 'cortx-2.0' ? "python-packages-2.0.0-latest" : THIRD_PARTY_PYTHON_VERSION == 'custom' ?  "python-packages-2.0.0-custom" : "python-packages-2.0.0-stable"}"
  		third_party_dir = "${THIRD_PARTY_RPM_VERSION == 'cortx-2.0' ? "$os_version-2.0.0-latest" : THIRD_PARTY_RPM_VERSION == 'cortx-1.0' ?  "$os_version-1.0.0-1" : "$os_version-custom"}"
-        }
+		integration_dir = "${INTEGRATION_DIR_PATH}"
+	}
 
 	options {
 		timeout(time: 60, unit: 'MINUTES')
@@ -25,11 +26,12 @@ pipeline {
 	}
 	
 	parameters {  
-	        string(name: 'CSM_AGENT_URL', defaultValue: 'https://github.com/Seagate/cortx-management.git', description: 'Repository URL for cortx-management build.')
+	    string(name: 'CSM_AGENT_URL', defaultValue: 'https://github.com/Seagate/cortx-management.git', description: 'Repository URL for cortx-management build.')
 		string(name: 'CSM_AGENT_BRANCH', defaultValue: 'stable', description: 'Branch for cortx-management build.')
 		string(name: 'CUSTOM_CI_BUILD_ID', defaultValue: '0', description: 'Custom CI Build Number')
-		string(name: 'THIRD_PARTY_PYTHON_VERSION', defaultValue: 'cortx-2.0', description: 'Third Party Python Version to use', trim: true)
+		string(name: 'THIRD_PARTY_PYTHON_VERSION', defaultValue: 'custom', description: 'Third Party Python Version to use', trim: true)
 		string(name: 'THIRD_PARTY_RPM_VERSION', defaultValue: 'cortx-2.0', description: 'Third Party RPM Version to use', trim: true)
+		string(name: 'INTEGRATION_DIR_PATH', defaultValue: '$release_dir/github/integration-custom-ci/$os_version/', description: 'Integration directory path', trim: true)
 	}	
 
 
@@ -55,22 +57,24 @@ pipeline {
 			steps {
 				script { build_stage = env.STAGE_NAME }
 
-				sh label: 'Install cortx-prereq package', script: """
+				sh label: 'Install cortx-prereq package', script: '''
+                    cortx_iso_path=$( echo $integration_dir | sed  's/[/]mnt[/]bigstorage[/]//g' )
+					echo $cortx_iso_path
 					pip3 uninstall pip -y && yum reinstall python3-pip -y && ln -s /usr/bin/pip3 /usr/local/bin/pip3
                                         yum install yum-utils -y
                                         yum-config-manager --add-repo=http://cortx-storage.colo.seagate.com/releases/cortx/third-party-deps/centos/$third_party_dir/
-                                        yum-config-manager --add-repo=http://ssc-vm-g2-rhev4-1603.colo.seagate.com/releases/cortx/github/integration-custom-ci/$os_version/$release_tag/cortx_iso/
+                                        yum-config-manager --add-repo=http://cortx-storage.colo.seagate.com/$cortx_iso_path$release_tag/cortx_iso/
                                         yum-config-manager --save --setopt=cortx-storage*.gpgcheck=1 cortx-storage* && yum-config-manager --save --setopt=cortx-storage*.gpgcheck=0 cortx-storage*
                                         yum clean all && rm -rf /var/cache/yum
                                         cat <<EOF >>/etc/pip.conf
 [global]
 timeout: 60
-index-url: http://cortx-storage.colo.seagate.com/releases/cortx/third-party-deps/python-deps/$python_deps/
+index-url: http://cortx-storage.colo.seagate.com/releases/cortx/third-party-deps/python-deps/$python_deps
 trusted-host: cortx-storage.colo.seagate.com
 EOF
                                         yum install java-1.8.0-openjdk-headless -y && yum install cortx-prereq -y --nogpgcheck 
                                         rm -rf  /etc/yum.repos.d/cortx-storage.colo.seagate.com_releases_cortx_* /etc/pip.conf
-                                """
+                                '''
 
 				sh label: 'Configure yum repositories', script: """
 					yum-config-manager --add-repo=http://cortx-storage.colo.seagate.com/releases/cortx/github/integration-custom-ci/$os_version/$release_tag/cortx_iso/
