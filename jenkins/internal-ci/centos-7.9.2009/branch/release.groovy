19c07c361a8951ba32fe56a0da909377cfea83ba
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-12-13 14:43:56 +0530

    Use prod build
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/internal-ci/centos-7.9.2009/branch/release.groovy
+++ jenkins/internal-ci/centos-7.9.2009/branch/release.groovy
@@ -243,7 +243,7 @@ pipeline {
                                     parameters: [
                                         string(name: 'CORTX_RE_URL', value: "https://github.com/Seagate/cortx-re"),
                                         string(name: 'CORTX_RE_BRANCH', value: "kubernetes"),
-                                        string(name: 'BUILD', value: "${release_tag}"),
+                                        string(name: 'BUILD', value: "${release_tag}/prod"),
                                         string(name: 'GITHUB_PUSH', value: "yes"),
                                         string(name: 'TAG_LATEST', value: "yes"),
                                         string(name: 'DOCKER_REGISTRY', value: "cortx-docker.colo.seagate.com"),
