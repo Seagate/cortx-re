acf8c4c2bf4dd40ce94ca3cf83c18d1174cd9a46
Nitish Singh <nitish.singh@seagate.com>
2021-12-14 12:42:24 +0530

    Kubernetes and Cortx deployment parallelization. (#726)
    * Parallelization for K8s to Cortx.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>
    
    * Removed download images..
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>

--- solutions/kubernetes/cluster-setup.sh
+++ solutions/kubernetes/cluster-setup.sh
@@ -18,60 +18,18 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
 
+source functions.sh
+
 HOST_FILE=$PWD/hosts
 SSH_KEY_FILE=/root/.ssh/id_rsa
 UNTAINT="$1"
 
-function validation {
-    if [ ! -f "$HOST_FILE" ]; then
-        echo "$HOST_FILE is not present"
-        exit 1
-    fi
-}
-
-function generate_rsa_key {
-    if [ ! -f "$SSH_KEY_FILE" ]; then
-        ssh-keygen -b 2048 -t rsa -f $SSH_KEY_FILE -q -N ""
-     else
-        echo $SSH_KEY_FILE already present
-    fi
-}
-
-function check_status {
-    return_code=$?
-    error_message=$1
-    if [ $return_code -ne 0 ]; then
-            echo "----------------------[ ERROR: $error_message ]--------------------------------------"
-            exit 1
-    fi
-    echo "----------------------[ SUCCESS ]--------------------------------------"
-}
-
-function passwordless_ssh {
-    local NODE=$1
-    local USER=$2
-    local PASS=$3
-    ping -c1 -W1 -q $NODE
-    check_status
-    yum install sshpass openssh-clients -y
-    sshpass -p "$PASS" ssh-copy-id -f -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub "$USER"@"$NODE"
-    check_status "Passwordless ssh setup failed for $NODE. Please validate provided credentails"
-}
-
-function nodes_setup {
-    for ssh_node in $(cat "$HOST_FILE")
-    do
-        local NODE=$(echo "$ssh_node" | awk -F[,] '{print $1}' | cut -d'=' -f2)
-        local USER=$(echo "$ssh_node" | awk -F[,] '{print $2}' | cut -d'=' -f2)
-        local PASS=$(echo "$ssh_node" | awk -F[,] '{print $3}' | cut -d'=' -f2)
-
-        echo "----------------------[ Setting up passwordless ssh for $NODE ]--------------------------------------"
-        passwordless_ssh "$NODE" "$USER" "$PASS"
-        scp -q cluster-functions.sh "$NODE":/var/tmp/
-    done
-}
-
 function setup_cluster {
+
+    validation
+    generate_rsa_key
+    nodes_setup
+
     ALL_NODES=$(cat "$HOST_FILE" | awk -F[,] '{print $1}' | cut -d'=' -f2)
     MASTER_NODE=$(head -1 "$HOST_FILE" | awk -F[,] '{print $1}' | cut -d'=' -f2)
     WORKER_NODES=$(cat "$HOST_FILE" | grep -v "$MASTER_NODE" | awk -F[,] '{print $1}' | cut -d'=' -f2)
@@ -82,14 +40,16 @@ function setup_cluster {
     echo "------------------------------------------------------------------------------------------------------"
 
     for node in $ALL_NODES
-    do
-        echo "---------------------------------------[ Cleanup Node $node ]--------------------------------------"
-        ssh -o 'StrictHostKeyChecking=no' "$node" '/var/tmp/cluster-functions.sh --cleanup'
-        check_status
-        echo "---------------------------------------[ Preparing Node $node ]--------------------------------------"
-        ssh -o 'StrictHostKeyChecking=no' "$node" '/var/tmp/cluster-functions.sh --prepare'
-        check_status "Node preparation failed on $node"
-    done
+	do 
+	    scp -q cluster-functions.sh functions.sh "$node":/var/tmp/
+	done
+
+    echo $ALL_NODES > /var/tmp/pdsh-hosts
+
+    # Cleanup nodes:
+    pdsh -w ^/var/tmp/pdsh-hosts '/var/tmp/cluster-functions.sh --cleanup'
+    # Prepare nodes:
+    pdsh -w ^/var/tmp/pdsh-hosts '/var/tmp/cluster-functions.sh --prepare'
 
     echo "---------------------------------------[ Preparing Master Node $MASTER_NODE ]--------------------------------------"
     ssh -o 'StrictHostKeyChecking=no' "$MASTER_NODE" "/var/tmp/cluster-functions.sh --master ${UNTAINT}"
@@ -97,11 +57,15 @@ function setup_cluster {
     sleep 10 #To be replaced with status check
     JOIN_COMMAND=$(ssh -o 'StrictHostKeyChecking=no' "$MASTER_NODE" 'kubeadm token create --print-join-command --description "Token to join worker nodes"')
     check_status "Failed fetch cluster join command"
+
+    echo $WORKER_NODES > /var/tmp/pdsh-hosts
+    # Join worker nodes.
+    JOIN_WORKER="/var/tmp/cluster-functions.sh --join-worker-nodes $JOIN_COMMAND"
+    pdsh -w ^/var/tmp/pdsh-hosts "$JOIN_WORKER"
+
+    # Label worker nodes.
     for worker_node in $WORKER_NODES
-        do
-        echo "---------------------------------------[ Joining Worker Node $worker_node ]--------------------------------------"
-        ssh -o 'StrictHostKeyChecking=no' "$worker_node" "echo "y" | kubeadm reset && $JOIN_COMMAND"
-        check_status "Failed to join $worker_node node to cluster"
+    do
         ssh -o 'StrictHostKeyChecking=no' "$MASTER_NODE" "kubectl label node $worker_node" node-role.kubernetes.io/worker=worker
         check_status "Failed to lable $worker_node"
     done
@@ -121,8 +85,5 @@ function print_status {
 }
 
 #Execution
-validation
-generate_rsa_key
-nodes_setup
 setup_cluster
 print_status
