24b95dd10747e0d1f4a4edc33d9d1c7470076d12
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-12-20 10:31:26 +0530

    Fixed syntax issue
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/provisioner.groovy
+++ jenkins/custom-ci/centos-7.9.2009/provisioner.groovy
@@ -55,7 +55,6 @@ pipeline {
                 script { build_stage = env.STAGE_NAME }
                 sh label: 'Copy RPMS', script: '''
                     mkdir -p $build_upload_dir
-                    cp /root/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir
                     shopt -s extglob
                     if ls ./dist/*.rpm; then
                         cp ./dist/!(*.src.rpm|*.tar.gz) $build_upload_dir
