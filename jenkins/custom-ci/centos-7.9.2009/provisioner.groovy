90c62d18e8f02627dc70ccb16904a5445534d851
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2021-12-23 10:10:08 +0530

    Add old rpms again to support main branch
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/provisioner.groovy
+++ jenkins/custom-ci/centos-7.9.2009/provisioner.groovy
@@ -40,6 +40,13 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
 
+                sh encoding: 'utf-8', label: 'Provisioner RPMS', returnStdout: true, script: """
+                    rm -rf /etc/yum.repos.d/CentOS-*
+                    sh ./devops/rpms/buildrpm.sh -g \$(git rev-parse --short HEAD) -e 2.0.0 -b ${CUSTOM_CI_BUILD_ID}
+                """
+                sh encoding: 'utf-8', label: 'Provisioner CLI RPMS', returnStdout: true, script: """
+                    sh ./cli/buildrpm.sh -g \$(git rev-parse --short HEAD) -e 2.0.0 -b ${CUSTOM_CI_BUILD_ID}
+                """
                 sh encoding: 'UTF-8', label: 'cortx-provisioner', script: '''
                 if [ -f "./jenkins/build.sh" ]; then
                     bash ./jenkins/build.sh -v 2.0.0 -b ${CUSTOM_CI_BUILD_ID}
