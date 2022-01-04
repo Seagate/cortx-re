ef51897e5cb684ba1bbfaf1d31672e82b4446725
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-01-04 13:25:21 +0530

    chore: fix path
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/internal-ci/centos-7.9.2009/pr/cortx_deployment/csm_web.groovy
+++ jenkins/internal-ci/centos-7.9.2009/pr/cortx_deployment/csm_web.groovy
@@ -89,8 +89,7 @@ pipeline {
                         BUILD=$(git rev-parse --short HEAD)
                         echo "Executing build script"
                         echo "Python:$(python --version)"
-                        ls -la
-                        ./cortx-management-portal/cicd/build.sh $VERSION -b $BUILD_NUMBER -t
+                        ./cicd/build.sh $VERSION -b $BUILD_NUMBER -t
                     '''
 
                     sh label: 'Collect Release Artifacts', script: '''
