95fa642aa53e646af97ec4adf09b2eae40b698f4
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-01-05 14:08:15 +0530

    EOS-27281 - Custom build is failing in csm-agent build (#799)
    * custom-ci failing for forked cortx-utils repo
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Fix syntax issue
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/csm-agent.groovy
+++ jenkins/custom-ci/centos-7.9.2009/csm-agent.groovy
@@ -2,45 +2,38 @@
 pipeline {
     agent {
         node {
-            label 'docker-centos-7.9.2009-node'
+            label "docker-${os_version}-node"
         }
     }
-    
+
+    triggers {
+        pollSCM '*/5 * * * *'
+    }
+
     environment { 
+        version = "2.0.0"     
+        env = "dev"
         component = "csm-agent"
-        branch = "custom-ci" 
-        os_version = "centos-7.9.2009"
         release_dir = "/mnt/bigstorage/releases/cortx"
-        release_tag = "custom-build-$CUSTOM_CI_BUILD_ID"
-        build_upload_dir = "$release_dir/github/integration-custom-ci/$os_version/$release_tag/cortx_iso"
-        python_deps = "${THIRD_PARTY_PYTHON_VERSION == 'cortx-2.0' ? "python-packages-2.0.0-latest" : THIRD_PARTY_PYTHON_VERSION == 'custom' ?  "python-packages-2.0.0-custom" : "python-packages-2.0.0-stable"}"
+        release_tag = "last_successful_prod"
+        build_upload_dir = "$release_dir/components/github/$branch/$os_version/$env/$component"
     }
 
     options {
         timeout(time: 60, unit: 'MINUTES')
         timestamps ()
-            ansiColor('xterm')
-        buildDiscarder(logRotator(daysToKeepStr: '5', numToKeepStr: '10'))
+        ansiColor('xterm')
+        disableConcurrentBuilds()
     }
-    
-    parameters {  
-        string(name: 'CSM_AGENT_URL', defaultValue: 'https://github.com/Seagate/cortx-management.git', description: 'Repository URL for cortx-management build.')
-        string(name: 'CSM_AGENT_BRANCH', defaultValue: 'main', description: 'Branch for cortx-management build.')
-        string(name: 'CUSTOM_CI_BUILD_ID', defaultValue: '0', description: 'Custom CI Build Number')
-        string(name: 'CORTX_UTILS_BRANCH', defaultValue: 'main', description: 'Branch or GitHash for CORTX Utils', trim: true)
-        string(name: 'CORTX_UTILS_URL', defaultValue: 'https://github.com/Seagate/cortx-utils', description: 'CORTX Utils Repository URL', trim: true)
-        string(name: 'THIRD_PARTY_PYTHON_VERSION', defaultValue: 'cortx-2.0', description: 'Third Party Python Version to use', trim: true)
-    }    
-
 
     stages {
 
         stage('Checkout') {
-            
             steps {
                 script { build_stage = env.STAGE_NAME }
-                dir('cortx-manager') {
-                    checkout([$class: 'GitSCM', branches: [[name: "${CSM_AGENT_BRANCH}"]], doGenerateSubmoduleConfigurations: false,  extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: "${CSM_AGENT_URL}"]]])
+
+                dir ('cortx-csm-agent') {
+                    checkout([$class: 'GitSCM', branches: [[name: "${branch}"]], doGenerateSubmoduleConfigurations: false,  extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/cortx-manager']]])
                 }
                 dir('seagate-ldr') {
                     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: 'https://github.com/Seagate/seagate-ldr.git']]])
@@ -56,32 +49,29 @@ pipeline {
                 script { build_stage = env.STAGE_NAME }
 
                 sh label: 'Configure yum repository for cortx-py-utils', script: """
-                    CORTX_UTILS_REPO_OWNER=$(echo $CORTX_UTILS_URL | cut -d "/" -f4)
-                    yum-config-manager --nogpgcheck --add-repo=http://cortx-storage.colo.seagate.com/releases/cortx/github/kubernetes/centos-7.9.2009/last_successful_prod/cortx_iso/
+            yum-config-manager --add-repo=http://cortx-storage.colo.seagate.com/releases/cortx/github/$branch/$os_version/$release_tag/cortx_iso/
 
-                    pip3 install --no-cache-dir --trusted-host cortx-storage.colo.seagate.com -i http://cortx-storage.colo.seagate.com/releases/cortx/third-party-deps/python-deps/$python_deps/ -r https://raw.githubusercontent.com/$CORTX_UTILS_REPO_OWNER/cortx-utils/$CORTX_UTILS_BRANCH/py-utils/python_requirements.txt -r https://raw.githubusercontent.com/$CORTX_UTILS_REPO_OWNER/cortx-utils/$CORTX_UTILS_BRANCH/py-utils/python_requirements.ext.txt
+                    pip3 install --no-cache-dir --trusted-host cortx-storage.colo.seagate.com -i http://cortx-storage.colo.seagate.com/releases/cortx/third-party-deps/python-deps/python-packages-2.0.0-latest/ -r https://raw.githubusercontent.com/Seagate/cortx-utils/$branch/py-utils/python_requirements.txt -r https://raw.githubusercontent.com/Seagate/cortx-utils/$branch/py-utils/python_requirements.ext.txt
+            yum install cortx-py-utils -y --nogpgcheck
                 """
 
                 sh label: 'Install pyinstaller', script: """
                         pip3.6 install  pyinstaller==3.5
                 """
             }
-        }    
+        }           
         
         stage('Build') {
             steps {
                 script { build_stage = env.STAGE_NAME }
-                // Exclude return code check for csm_setup and csm_test
-                sh label: 'Build', returnStatus: true, script: '''
-                pushd cortx-manager
+                sh label: 'Build', script: '''
+                pushd cortx-csm-agent
                     BUILD=$(git rev-parse --short HEAD)
-                    VERSION="2.0.0"
                     echo "Executing build script"
-                    echo "VERSION:$VERSION"
                     echo "Python:$(python --version)"
-                    ./cicd/build.sh -v $VERSION -b $CUSTOM_CI_BUILD_ID -t -n ldr -l $WORKSPACE/seagate-ldr
-                popd    
-                '''    
+                    ./cicd/build.sh -v $version -b $BUILD_NUMBER -t -n ldr -l $WORKSPACE/seagate-ldr
+                popd
+                '''
             }
         }
         
@@ -89,10 +79,125 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 sh label: 'Copy RPMS', script: '''
-                    mkdir -p $build_upload_dir
-                    cp ./cortx-manager/dist/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir
+                    mkdir -p $build_upload_dir/$BUILD_NUMBER
+                    cp ./cortx-csm-agent/dist/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir/$BUILD_NUMBER
+                '''
+                sh label: 'Repo Creation', script: '''pushd $build_upload_dir/$BUILD_NUMBER
+                    rpm -qi createrepo || yum install -y createrepo
+                    createrepo .
+                    popd
                 '''
             }
         }
+
+        stage ('Tag last_successful') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Tag last_successful', script: '''pushd $build_upload_dir/
+                    test -d $build_upload_dir/last_successful && rm -f last_successful
+                    ln -s $build_upload_dir/$BUILD_NUMBER last_successful
+                    popd
+                '''
+            }
+        }
+        
+        stage ("Release") {
+            when { triggeredBy 'SCMTrigger' }
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                script {
+                    def releaseBuild = build job: 'Release', propagate: true
+                    env.release_build = releaseBuild.number
+                    env.release_build_location = "http://cortx-storage.colo.seagate.com/releases/cortx/github/$branch/$os_version/${env.release_build}"
+                    env.cortx_images = releaseBuild.buildVariables.cortx_all_image + "\n" + releaseBuild.buildVariables.cortx_rgw_image + "\n" + releaseBuild.buildVariables.cortx_data_image + "\n" + releaseBuild.buildVariables.cortx_control_image
+                }
+            }
+        }
+    
+        stage('Update Jira') {
+            when { expression { return env.release_build != null } }
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                script {
+                    def jiraIssues = jiraIssueSelector(issueSelector: [$class: 'DefaultIssueSelector'])
+                    jiraIssues.each { issue ->
+                        def author =  getAuthor(issue)
+                        jiraAddComment(    
+                            idOrKey: issue,
+                            site: "SEAGATE_JIRA",
+                            comment: "{panel:bgColor=#c1c7d0}" +
+                                "h2. ${component} - ${branch} branch build pipeline SUCCESS\n" +
+                                "h3. Build Info:  \n" +
+                                        author +
+                                        "* Component Build  :  ${BUILD_NUMBER} \n" +
+                                        "* Release Build    :  ${release_build}  \n\n  " +
+                                "h3. Artifact Location  :  \n" +
+                                    "*  " + "${release_build_location}" + "\n\n" +
+                                "h3. Image Location  :  \n" +
+                                    "*  " + "${cortx_images}" + "\n" +
+                                "{panel}",
+                            failOnError: false,
+                            auditLog: false
+                        )
+                        //def jiraFileds = jiraGetIssue idOrKey: issue, site: "SEAGATE_JIRA", failOnError: false
+                        //if(jiraFileds.data != null){
+                        //def labels_data =  jiraFileds.data.fields.labels + "cortx_stable_b${release_build}"
+                    //jiraEditIssue idOrKey: issue, issue: [fields: [ labels: labels_data ]], site: "SEAGATE_JIRA", failOnError: false    
+                    //} 
+                    }
+                }
+            }
+        }
     }
+    
+    post {
+        always {
+            script {
+                
+                echo 'Cleanup Workspace.'
+                deleteDir() /* clean up our workspace */
+
+                env.release_build = (env.release_build != null) ? env.release_build : "" 
+                env.release_build_location = (env.release_build_location != null) ? env.release_build_location : ""
+                env.component = (env.component).toUpperCase()
+                env.build_stage = "${build_stage}"
+
+                def toEmail = ""
+                def recipientProvidersClass = [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
+                if ( manager.build.result.toString() == "FAILURE" ) {
+                    toEmail = "CORTX.DevOps.RE@seagate.com"
+                    recipientProvidersClass = [[$class: 'DevelopersRecipientProvider'],[$class: 'RequesterRecipientProvider']]
+                }
+
+                emailext (
+                    body: '''${SCRIPT, template="component-email.template"}''',
+                    mimeType: 'text/html',
+                    subject: "[Jenkins Build ${currentBuild.currentResult}] : ${env.JOB_NAME}",
+                    attachLog: true,
+                    to: toEmail,
+                    recipientProviders: recipientProvidersClass
+                )
+            }
+        }
+    }
+}    
+
+@NonCPS
+def getAuthor(issue) {
+
+    def changeLogSets = currentBuild.rawBuild.changeSets
+    def author = ""
+    def response = ""
+    // Grab build information
+    for (int i = 0; i < changeLogSets.size(); i++) {
+        def entries = changeLogSets[i].items
+        for (int j = 0; j < entries.length; j++) {
+            def entry = entries[j]
+            if ((entry.msg).contains(issue)) {
+                author = entry.author
+            }
+        }
+    }
+    response = "* Author: " + author + "\n"
+    return response
 }
