0bf7f5994cf7d5c44d13bafffa9f44a823d4da3c
nikhilpatil2995 <nikhil.patil@seagate.com>
2022-01-10 15:30:06 +0530

    use script for 3 node deployment
    Signed-off-by: nikhilpatil2995 <nikhil.patil@seagate.com>

--- jenkins/internal-ci/centos-7.9.2009/pr/component_test/pyutils-premerge.groovy
+++ jenkins/internal-ci/centos-7.9.2009/pr/component_test/pyutils-premerge.groovy
@@ -20,12 +20,18 @@ pipeline {
         string(name: 'CORTX_RE_BRANCH', defaultValue: 'main', description: 'Branch for cortx-re')
         string(name: 'PY_UTILS_URL', defaultValue: 'https://github.com/Seagate/cortx-utils', description: 'Repo for Py-Utils Agent')
         string(name: 'PY_UTILS_BRANCH', defaultValue: 'main', description: 'Branch for Py-Utils Agent')     
-        string(name: 'HOST', defaultValue: '-', description: 'Host=FQDN,user=USERNAME,pass=PASSWORD',  trim: true)
+        // string(name: 'HOST', defaultValue: '-', description: 'Host=FQDN,user=USERNAME,pass=PASSWORD',  trim: true)
         string(name: 'COMPONENTS_BRANCH', defaultValue: 'main', description: 'Branch for other components',  trim: true)
         choice(
             name: 'OS_VERSION',
             choices: ['centos-7.9.2009', 'centos-7.8.2003'],
             description: 'OS version of HOST'
+        )
+        choice(
+            name: 'DEPLOY_BUILD_ON_NODES',
+            choices: ["Both", "1node", "3node" ],
+            description: '''<pre>If you select Both then build will be deploy on 1 node as well as 3 node. If you select 1 node then build will be deploy on 1 node only. 
+                            If you select 3 node then build will be deploy on 3 node only.</pre>'''
         )
     }
 
@@ -40,7 +46,7 @@ pipeline {
         PY_UTILS_PR_REFSPEC = "${ghprbPullId != null ? PY_UTILS_REFSPEC : PY_UTILS_BRANCH_REFSEPEC}"
 
         //////////////////////////////// BUILD VARS //////////////////////////////////////////////////
-        // OS_VERSION , host and COMPONENTS_BRANCH are manually created parameters in jenkins job.
+        // OS_VERSION, singlenode_host, threenode_hosts and COMPONENTS_BRANCH are manually created parameters in jenkins job.
 
         COMPONENT_NAME = "cortx-utils".trim()
         BRANCH = "${ghprbTargetBranch != null ? ghprbTargetBranch : COMPONENTS_BRANCH}"
@@ -208,31 +214,73 @@ pipeline {
             }
         }
 
-        stage ("Deploy") {
-            steps {
-                script { build_stage = env.STAGE_NAME }
-                script {
-                    build job: "/Cortx-PR-Build/Cortx-Deployment/Kubernetes/K8s-1N-deployment", wait: true,
-                    parameters: [
-                        string(name: 'CORTX_RE_REPO', value: "https://github.com/seagate/cortx-re"),
-                        string(name: 'CORTX_RE_BRANCH', value: "main"),
-                        string(name: 'CORTX_IMAGE', value: "${env.cortx_all_image}"),
-                        string(name: 'hosts', value: "${HOST}")
-                    ]
+        stage ('Deploy Cortx Cluster') {
+             parallel {
+                  stage ("Deploy 1Node") {
+                       when { expression { params.DEPLOY_BUILD_ON_NODES ==~ /Both|1node/ } }
+                       steps {
+                             script { build_stage = env.STAGE_NAME }
+                             script {
+                                  build job: "/Cortx-PR-Build/Cortx-Deployment/Kubernetes/K8s-1N-deployment", wait: true,
+                                  parameters: [
+                                       string(name: 'CORTX_RE_REPO', value: "https://github.com/Seagate/cortx-re/"),
+                                       string(name: 'CORTX_RE_BRANCH', value: "main"),
+                                       string(name: 'CORTX_IMAGE', value: "${env.cortx_all_image}"),
+                                       string(name: 'hosts', value: "${singlenode_host}")
+                                 ] 
+                             }
+                       }
                 }
-            }
+         	stage ("Deploy 3Node") {
+                     when { expression { params.DEPLOY_BUILD_ON_NODES ==~ /Both|3node/ } }
+                     steps {
+                          script { build_stage = env.STAGE_NAME }
+                          script {
+                                build job: '/Cortx-PR-Build/Cortx-Deployment/Kubernetes/K8s-3N-deployment', wait: true,
+                                parameters: [
+                                     string(name: 'CORTX_RE_BRANCH', value: "main"),
+                                     string(name: 'CORTX_RE_REPO', value: "https://github.com/Seagate/cortx-re/"),
+                                     string(name: 'CORTX_IMAGE', value: "${env.cortx_all_image}"),
+                                     text(name: 'hosts', value: "${threenode_hosts}"),
+                                ]
+                          }
+                      }
+                 } 
+             }
         }
+        
+        // stage ("Deploy") {
+        //     steps {
+        //         script { build_stage = env.STAGE_NAME }
+        //         script {
+        //             build job: "/Cortx-PR-Build/Cortx-Deployment/Kubernetes/K8s-1N-deployment", wait: true,
+        //             parameters: [
+        //                 string(name: 'CORTX_RE_REPO', value: "https://github.com/seagate/cortx-re"),
+        //                 string(name: 'CORTX_RE_BRANCH', value: "main"),
+        //                 string(name: 'CORTX_IMAGE', value: "${env.cortx_all_image}"),
+        //                 string(name: 'hosts', value: "${HOST}")
+        //             ]
+        //         }
+        //     }
+        // }
 
         stage ("Test RPM") {
             steps {
                 script {
                     sh label: 'RUN command to test rpm', script: '''
-                        echo $HOST | sed 's/,/ /g' | sed 's/=/ /g' |  awk -F ' ' '{ print $2" "$4" "$6 }' > server_info.txt
-                        NODE_HOST=$( cat server_info.txt | awk -F ' ' '{ print $1 }')
-                        NODE_USER=$( cat server_info.txt | awk -F ' ' '{ print $2 }')
-                        NODE_USER=$( cat server_info.txt | awk -F ' ' '{ print $2 }')
+                        if [ $DEPLOY_BUILD_ON_NODES == '1node' ]; then
+                            echo $singlenode_host | sed 's/,/ /g' | sed 's/=/ /g' |  awk -F ' ' '{ print $2" "$4" "$6 }' > server_info.txt
+                            NODE_HOST=$( cat server_info.txt | awk -F ' ' '{ print $1 }')
+                            NODE_USER=$( cat server_info.txt | awk -F ' ' '{ print $2 }')
+                            NODE_PASS=$( cat server_info.txt | awk -F ' ' '{ print $3 }')
+                        else
+                            echo $threenode_hosts | head -n1 | sed 's/,/ /g' | sed 's/=/ /g' |  awk -F ' ' '{ print $2" "$4" "$6 }' > server_info.txt
+                            NODE_HOST=$( cat server_info.txt | awk -F ' ' '{ print $1 }')
+                            NODE_USER=$( cat server_info.txt | awk -F ' ' '{ print $2 }')
+                            NODE_PASS=$( cat server_info.txt | awk -F ' ' '{ print $3 }')
+                        fi
                         yum install sshpass -y
-                        sshpass -p ${NODE_PASS} ssh -o StrictHostKeyChecking=no ${NODE_USER}@${NODE_HOST} kubectl exec '$(kubectl get pods | grep "data-pod" | awk '{print$1}')' --container cortx-motr-hax --  yum install http://cortx-storage.colo.seagate.com/releases/cortx/github/pr-build/${BRANCH}/${COMPONENT_NAME}/${BUILD_NUMBER}/cortx_iso/cortx-py-utils-test-1.0.0-1.noarch.rpm
+                        sshpass -p ${NODE_PASS} ssh -o StrictHostKeyChecking=no ${NODE_USER}@${NODE_HOST} kubectl exec '$(kubectl get pods | grep "data-pod" | awk '{print$1}')' --container cortx-motr-hax --  yum install http://cortx-storage.colo.seagate.com/releases/cortx/github/pr-build/${BRANCH}/${COMPONENT_NAME}/${BUILD_NUMBER}/cortx_iso/cortx-py-utils-test-1.0.0-1.noarch.rpm -y
                         sshpass -p ${NODE_PASS} ssh -o StrictHostKeyChecking=no ${NODE_USER}@${NODE_HOST} kubectl exec '$(kubectl get pods | grep "data-pod" | awk '{print$1}')' --container cortx-motr-hax -- /opt/seagate/cortx/utils/bin/utils_setup test --config yaml:///etc/cortx/cluster.conf --plan sanity
                     '''
                 }
