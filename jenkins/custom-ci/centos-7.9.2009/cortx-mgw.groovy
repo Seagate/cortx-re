40f44af5d8fc15c833029d7609c6b49c48df49af
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-01-18 11:18:37 +0530

    Update cortx-mgw.groovy
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/cortx-mgw.groovy
+++ jenkins/custom-ci/centos-7.9.2009/cortx-mgw.groovy
@@ -9,12 +9,10 @@ pipeline {
     parameters {  
         string(name: 'CEPH_URL', defaultValue: 'https://github.com/ceph/ceph', description: 'Repository URL for ceph build')
         string(name: 'CEPH_BRANCH', defaultValue: 'master', description: 'Branch for ceph build')
-        string(name: 'CUSTOM_CI_BUILD_ID', defaultValue: '0', description: 'Custom CI Build Number')
     }
     
     options {
         timestamps() 
-        buildDiscarder(logRotator(daysToKeepStr: '5', numToKeepStr: '10'))
     }
 
     stages {
@@ -27,18 +25,25 @@ pipeline {
                 }
             }
         }
+
+        stage ('Clean up') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Clean up', script: '''
+                rm -rf /mnt/rgw-build/rpmbuild/SOURCES/ceph*.tar.bz2
+                rm -rf  /mnt/rgw-build/rpmbuild/BUILD/ceph*
+                '''
+            }
+        }
     
         stage('Build') {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 sh label: 'Build', script: '''
 
-                rm -rf /mnt/rgw-build/rpmbuild/SOURCES/ceph*.tar.bz2
-                rm -rf  /mnt/rgw-build/rpmbuild/BUILD/ceph*
-
                 pushd ceph
                     sed -i 's/centos|/rocky|centos|/' install-deps.sh
-                    #./install-deps.sh
+                    ./install-deps.sh
                     ./make-dist
                     mkdir -p /mnt/rgw-build/rpmbuild/$BUILD_NUMBER/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
                     mv ceph*.tar.bz2 /mnt/rgw-build/rpmbuild/$BUILD_NUMBER/SOURCES/
@@ -46,25 +51,20 @@ pipeline {
                 popd
 
                 pushd /mnt/rgw-build/rpmbuild/$BUILD_NUMBER
-                     rpmbuild --define "_unpackaged_files_terminate_build 0" --define "build_type rgw" --define "debug_package %{nil}" --without prometheus-alerts --without grafana-dashboards --without  mgr-dashboard --without cmake_verbose_logging --without jaeger --without lttng --without seastar --without kafka_endpoint --without zbd --without cephfs_java --without cephfs_shell --without ocf --without selinux --without ceph_test_package --without make_check --define "_binary_payload w2T16.xzdio" --define "_topdir `pwd`" -ba ./SPECS/ceph.spec
+                     rpmbuild --clean --rmsource --define "_unpackaged_files_terminate_build 0" --define "debug_package %{nil}" --without cmake_verbose_logging --without jaeger --without lttng --without seastar --without kafka_endpoint --without zbd --without cephfs_java --without cephfs_shell --without ocf --without selinux --without ceph_test_package --without make_check --define "_binary_payload w2T16.xzdio" --define "_topdir `pwd`" -ba ./SPECS/ceph.spec
                 popd
-
-                echo "Packages are available at "
-                ls -la /mnt/rgw-build/rpmbuild/$BUILD_NUMBER/RPMS/*/*.rpm
             '''    
             }
         }    
 
         stage ('Copy RPMS') {
-            when { expression { false } }
             steps {
                 script { build_stage = env.STAGE_NAME }
                 sh label: 'Copy RPMS', script: '''
-                    mkdir -p $build_upload_dir
-                    shopt -s extglob
-                    cp ./py-utils/dist/!(*.src.rpm|*.tar.gz) $build_upload_dir
-                    cp ./statsd-utils/dist/rpmbuild/RPMS/x86_64/*.rpm $build_upload_dir
-                    createrepo -v --update $build_upload_dir
+                    ls -la /mnt/rgw-build/rpmbuild/$BUILD_NUMBER/RPMS/*/*.rpm
+                    createrepo -v /mnt/rgw-build/rpmbuild/$BUILD_NUMBER/RPMS/
+                    echo "Ceph Packages are available at "
+                    echo "http://cortx-storage.colo.seagate.com/releases/cortx/rgw-build/rpmbuild/$BUILD_NUMBER/RPMS/"
                 '''
             }
         }
