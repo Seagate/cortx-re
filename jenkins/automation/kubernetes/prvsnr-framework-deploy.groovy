56ae5371e211c38878b7b64c9abbc2e0d9e3c842
Nitish Singh <nitish.singh@seagate.com>
2022-01-17 21:52:43 -0700

    Added update for hosts parameter nightly configuration.
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>

--- jenkins/automation/kubernetes/prvsnr-framework-deploy.groovy
+++ jenkins/automation/kubernetes/prvsnr-framework-deploy.groovy
@@ -25,7 +25,6 @@ pipeline {
         string(name: 'CORTX_PRVSNR_BRANCH', defaultValue: 'main', description: 'Branch or GitHash for Cluster Setup scripts', trim: true)
         string(name: 'CORTX_PRVSNR_REPO', defaultValue: 'Seagate/cortx-prvsnr', description: 'Repository for Cluster Setup scripts', trim: true)
         string(name: 'CORTX_IMAGE', defaultValue: 'ghcr.io/seagate/cortx-all:2.0.0-latest-custom-ci', description: 'CORTX-ALL image', trim: true)
-        //text(defaultValue: '''hostname=<hostname>,user=<user>,pass=<password>''', description: 'VM details to be used for CORTX cluster setup. First node will be used as Master', name: 'hosts')       
         // Please configure hosts parameter in the Jenkins job configuration.
     }    
 
