0d05ab4917e719134972dc5ef6fa35bb43e704d5
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-01-20 11:14:58 +0530

    Add Motr Build stage
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/centos-7.9.2009/cortx-mgw.groovy
+++ jenkins/custom-ci/centos-7.9.2009/cortx-mgw.groovy
@@ -6,9 +6,11 @@ pipeline {
         }
     }
 
-    parameters {  
+    parameters {
+        string(name: 'MOTR_URL', defaultValue: 'https://github.com/Seagate/cortx-motr', description: 'Repository URL for Motr build')
+        string(name: 'MOTR_BRANCH', defaultValue: 'main', description: 'Branch for Motr build')
         string(name: 'CEPH_URL', defaultValue: 'https://github.com/ceph/ceph', description: 'Repository URL for ceph build')
-        string(name: 'CEPH_BRANCH', defaultValue: 'master', description: 'Branch for ceph build')
+        string(name: 'CEPH_BRANCH', defaultValue: 'main', description: 'Branch for ceph build')
     }
     
     options {
@@ -17,7 +19,7 @@ pipeline {
 
     stages {
     
-        stage('Checkout ceph') {
+        stage('Checkout Ceph') {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 dir ('ceph') {
@@ -26,15 +28,45 @@ pipeline {
             }
         }
 
+        stage('Checkout Motr') {
+            steps {
+                dir ('motr') {
+                    checkout([$class: 'GitSCM', branches: [[name: "$MOTR_BRANCH"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'AuthorInChangelog'], [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: "$MOTR_URL"]]])
+                }    
+            }
+        }
+
         stage ('Clean up') {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 sh label: 'Clean up', script: '''
+                rm -rf /root/rpmbuild/RPMS/x86_64/*.rpm
                 rm -rf /mnt/rgw-build/rpmbuild/SOURCES/ceph*.tar.bz2
                 rm -rf  /mnt/rgw-build/rpmbuild/BUILD/ceph*
+                yum erase cortx-motr{,-devel} -y
                 '''
             }
         }
+
+        stage ('Build Motr Packages') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Install Motr', script: '''
+                pushd motr
+                        yum install castxml perl-File-Find-Rule perl-IO-All asciidoc libedit-devel python2-devel -y
+                        cp cortx-motr.spec.in cortx-motr.spec
+                        sed -i -e 's/@.*@/111/g' -e '/^111/d' cortx-motr.spec
+                        #yum-builddep -y cortx-motr.spec
+                        ./autogen.sh
+                        ./configure --with-user-mode-only
+                        export build_number=${BUILD_ID}
+                        make rpms
+                        rm -rf /root/rpmbuild/RPMS/x86_64/*debug*.rpm
+                        yum install /root/rpmbuild/RPMS/x86_64/cortx-motr{,-devel}*.rpm -y --nogpgcheck
+                '''
+            }
+        }
+
     
         stage('Build') {
             steps {
