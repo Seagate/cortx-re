94835860fc79a5acc757a61d17f95596fb3275a4
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-01-28 15:30:48 +0530

    Add cortx-py-utils fix
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/generic/csm-agent.groovy
+++ jenkins/custom-ci/generic/csm-agent.groovy
@@ -61,6 +61,9 @@ pipeline {
                     yum-config-manager --nogpgcheck --add-repo=http://cortx-storage.colo.seagate.com/releases/cortx/github/kubernetes/centos-7.9.2009/last_successful_prod/cortx_iso/
 
                     pip3 install --no-cache-dir --trusted-host cortx-storage.colo.seagate.com -i http://cortx-storage.colo.seagate.com/releases/cortx/third-party-deps/python-deps/$python_deps/ -r https://raw.githubusercontent.com/$CORTX_UTILS_REPO_OWNER/cortx-utils/$CORTX_UTILS_BRANCH/py-utils/python_requirements.txt -r https://raw.githubusercontent.com/$CORTX_UTILS_REPO_OWNER/cortx-utils/$CORTX_UTILS_BRANCH/py-utils/python_requirements.ext.txt
+
+                    #Install cortx-py-utils package with --nodeps    
+                    wget -r -np -nH --cut-dirs=7 -A "cortx-py-utils*.rpm" http://cortx-storage.colo.seagate.com/releases/cortx/github/integration-custom-ci/$os_version/$release_tag/cortx_iso/
                 '''
 
                 sh label: 'Install pyinstaller', script: """
