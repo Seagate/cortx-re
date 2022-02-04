387aa8edbe58e84e864a1085cb7bfe5648746455
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-02-04 11:17:23 +0530

    Increase timeout
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/generic/custom-ci.groovy
+++ jenkins/custom-ci/generic/custom-ci.groovy
@@ -19,7 +19,7 @@ pipeline {
     }
 
     options {
-        timeout(time: 120, unit: 'MINUTES')
+        timeout(time: 300, unit: 'MINUTES')
         timestamps()
         ansiColor('xterm')
         parallelsAlwaysFailFast()
