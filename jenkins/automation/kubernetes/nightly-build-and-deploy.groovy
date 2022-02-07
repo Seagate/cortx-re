c03d385ea0da49ba45a3e297d7384af7ad1500a1
nikhilpatil2995 <nikhil.patil@seagate.com>
2022-02-07 18:19:58 +0530

    update condition to send sucessfull build status to cortx-all
    Signed-off-by: nikhilpatil2995 <nikhil.patil@seagate.com>

--- jenkins/automation/kubernetes/nightly-build-and-deploy.groovy
+++ jenkins/automation/kubernetes/nightly-build-and-deploy.groovy
@@ -193,31 +193,29 @@ pipeline {
                     mailRecipients = "CORTX.All@seagate.com"
                     //mailRecipients = "cortx.sme@seagate.com, manoj.management.team@seagate.com, CORTX.SW.Architecture.Team@seagate.com, CORTX.DevOps.RE@seagate.com"
                 }
-                else {
-                    mailRecipients = "Cortx.Perf@seagate.com, akhil.bhansali@seagate.com, amit.kapil@seagate.com, amol.j.kongre@seagate.com, deepak.choudhary@seagate.com, jaikumar.gidwani@seagate.com, mandar.joshi@seagate.com, neerav.choudhari@seagate.com, pranay.kumar@seagate.com, swarajya.pendharkar@seagate.com, taizun.a.kachwala@seagate.com, trupti.patil@seagate.com, ujjwal.lanjewar@seagate.com, shailesh.vaidya@seagate.com, abhijit.patil@seagate.com, sonal.kalbende@seagate.com, gaurav.chaudhari@seagate.com, mukul.malhotra@seagate.com, swanand.s.gadre@seagate.com, don.r.bloyer@seagate.com, kalpesh.chhajed@seagate.com"
-                } 
-
-                if ( params.EMAIL_RECIPIENTS == "ALL" && ( "${env.cortxCluster_status}" == "UNSTABLE" && "${env.qaSanity_status}" == "UNSTABLE" ) ) {
-                    mailRecipients = "Cortx.Perf@seagate.com"
+                else if ( params.EMAIL_RECIPIENTS == "ALL" && ( "${env.cortxCluster_status}" == "UNSTABLE" || "${env.qaSanity_status}" == "UNSTABLE" ) ) {
+                    mailRecipients = "Cortx.Perf@seagate.com, akhil.bhansali@seagate.com, amit.kapil@seagate.com, amol.j.kongre@seagate.com, deepak.choudhary@seagate.com, jaikumar.gidwani@seagate.com, mandar.joshi@seagate.com, neerav.choudhari@seagate.com, pranay.kumar@seagate.com, swarajya.pendharkar@seagate.com, taizun.a.kachwala@seagate.com, trupti.patil@seagate.com, ujjwal.lanjewar@seagate.com, shailesh.vaidya@seagate.com, abhijit.patil@seagate.com, sonal.kalbende@seagate.com, gaurav.chaudhari@seagate.com, mukul.malhotra@seagate.com, swanand.s.gadre@seagate.com, don.r.bloyer@seagate.com, kalpesh.chhajed@seagate.com "
                     //mailRecipients = "cortx.sme@seagate.com, manoj.management.team@seagate.com, CORTX.SW.Architecture.Team@seagate.com, CORTX.DevOps.RE@seagate.com"
                 }
-                else {
-                    mailRecipients = "Cortx.Perf@seagate.com, akhil.bhansali@seagate.com, amit.kapil@seagate.com, amol.j.kongre@seagate.com, deepak.choudhary@seagate.com, jaikumar.gidwani@seagate.com, mandar.joshi@seagate.com, neerav.choudhari@seagate.com, pranay.kumar@seagate.com, swarajya.pendharkar@seagate.com, taizun.a.kachwala@seagate.com, trupti.patil@seagate.com, ujjwal.lanjewar@seagate.com, shailesh.vaidya@seagate.com, abhijit.patil@seagate.com, sonal.kalbende@seagate.com, gaurav.chaudhari@seagate.com, mukul.malhotra@seagate.com, swanand.s.gadre@seagate.com, don.r.bloyer@seagate.com, kalpesh.chhajed@seagate.com"
+                else if ( params.EMAIL_RECIPIENTS == "ALL" && ( "${env.cortxCluster_status}" == "FAILURE" || "${env.qaSanity_status}" == "FAILURE" ) ) {
+                    mailRecipients = "akhil.bhansali@seagate.com, amit.kapil@seagate.com, amol.j.kongre@seagate.com, deepak.choudhary@seagate.com, jaikumar.gidwani@seagate.com, mandar.joshi@seagate.com, neerav.choudhari@seagate.com, pranay.kumar@seagate.com, swarajya.pendharkar@seagate.com, taizun.a.kachwala@seagate.com, trupti.patil@seagate.com, ujjwal.lanjewar@seagate.com, shailesh.vaidya@seagate.com, abhijit.patil@seagate.com, sonal.kalbende@seagate.com, gaurav.chaudhari@seagate.com, mukul.malhotra@seagate.com, swanand.s.gadre@seagate.com, don.r.bloyer@seagate.com, kalpesh.chhajed@seagate.com "
+                    //mailRecipients = "cortx.sme@seagate.com, manoj.management.team@seagate.com, CORTX.SW.Architecture.Team@seagate.com, CORTX.DevOps.RE@seagate.com"
                 }
-
-                if ( params.EMAIL_RECIPIENTS == "DEBUG" ) {
-                    mailRecipients = "shailesh.vaidya@seagate.com, gaurav.chaudhari@seagate.com, nikhil.patil@seagate.com"
-                }
-
-                if ( params.EMAIL_RECIPIENTS == "DEVOPS" && ( "${env.cortxCluster_status}" == "SUCCESS" && "${env.qaSanity_status}" == "SUCCESS" ) ) {
+                else if ( params.EMAIL_RECIPIENTS == "DEVOPS" && ( "${env.cortxCluster_status}" == "SUCCESS" && "${env.qaSanity_status}" == "SUCCESS" ) ) {
                     mailRecipients = "CORTX.All@seagate.com, CORTX.DevOps.RE@seagate.com"
                 }
-                else if ( params.EMAIL_RECIPIENTS == "DEVOPS" && ( "${env.cortxCluster_status}" == "UNSTABLE" && "${env.qaSanity_status}" == "UNSTABLE" ) ) {
+                else if ( params.EMAIL_RECIPIENTS == "DEVOPS" && ( "${env.cortxCluster_status}" == "UNSTABLE" || "${env.qaSanity_status}" == "UNSTABLE" ) ) {
                      mailRecipients = "Cortx.Perf@seagate.com, CORTX.DevOps.RE@seagate.com"
                 }
-                else {
+                else if ( params.EMAIL_RECIPIENTS == "DEVOPS" && ( "${env.cortxCluster_status}" == "FAILURE" || "${env.qaSanity_status}" == "FAILURE" ) ) {
                     mailRecipients = "CORTX.DevOps.RE@seagate.com"
                 }
+                else if ( params.EMAIL_RECIPIENTS == "DEBUG" ) {
+                    mailRecipients = "shailesh.vaidya@seagate.com"
+                }
+                else {
+                    mailRecipients = "shailesh.vaidya@seagate.com"
+                }                
 
                 catchError(stageResult: 'FAILURE') {
                     archiveArtifacts allowEmptyArchive: true, artifacts: 'log/*report.xml, log/*report.html, support_bundle/*.tar, crash_files/*.gz', followSymlinks: false
