1963d02e46b78bbd384b20132007d02f5bcb4159
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-02-08 21:02:12 +0530

    EOS-27895 : Create granular images (#842)
    * chore: initial commit for granular images
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: update optimized folder structure for granular images
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: update package installations
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: add files to remove merge conflicts
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: remove files added for resolving merge conflicts
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: fix codacy issues
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: remove whitespaces
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: remove quotes
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: remove quotes
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: resolve codacy issue
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * test: package removal
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * test: package removal
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * test: regex for comment removal
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: revert tests
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * test: codacy fix
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: revert test
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: remove kafka package and update logic to fetch git hash
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: update requirement files
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: update dockerfile to accept generic os name
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: update dockerfiles for generic os input
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: add OS vars
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
    
    * chore: remove s3server
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- docker/cortx-deploy/build.sh
+++ docker/cortx-deploy/build.sh
@@ -20,37 +20,39 @@
 
 set -e -o pipefail
 
-usage() { 
-echo "Generate cortx-all docker image from provided CORTX release build"
-echo "Usage: $0 [ -b build ] [ -p push docker-image to GHCR yes/no. Default no] [ -t tag latest yes/no. Default no" ] [ -r registry location ] [ -e environment ] [ -h print help message ] 1>&2; exit 1; }
+usage() 
+{ 
+        echo "Generate CORTX components docker images from provided CORTX release build"
+        echo "Usage: $0 [ -b build ] [ -p push docker-image to GHCR yes/no. Default no] [ -t tag latest yes/no. Default no ] [ -r registry location ] [ -e environment ] [ -s service-name ] [ -h print help message ]"
+        exit 1 
+}
 
 VERSION=2.0.0
 DOCKER_PUSH=no
 TAG_LATEST=no
 OS=centos-7.9.2009
 ENVIRONMENT=opensource-ci
+BUILD=last_successful_prod
 REGISTRY="cortx-docker.colo.seagate.com"
 PROJECT="seagate"
 ARTFACT_URL="http://cortx-storage.colo.seagate.com/releases/cortx/github/"
 SERVICE=cortx-all
+COMPONENTS=""
 
-while getopts "b:p:t:r:e:h:" opt; do
+while getopts "b:p:t:r:e:s:h:" opt; do
     case $opt in
         b ) BUILD=$OPTARG;;
         p ) DOCKER_PUSH=$OPTARG;;
         t ) TAG_LATEST=$OPTARG;;
         e ) ENVIRONMENT=$OPTARG;;
         r ) REGISTRY=$OPTARG;;
+        s ) SERVICE=$OPTARG;;
         h ) usage
         exit 0;;
         *) usage
         exit 1;;
     esac
 done
-
-if [ -z "${BUILD}" ] ; then
-    BUILD=last_successful_prod
-fi
 
 if echo $BUILD | grep -q http;then
 BUILD_URL="$BUILD"
@@ -63,12 +65,15 @@ echo "Building $SERVICE image from $BUILD_URL"
 sleep 5
 
 function get_git_hash {
-sed -i '/KERNEL/d' RELEASE.INFO
-for component in cortx-py-utils cortx-s3server cortx-motr cortx-hare cortx-provisioner
-do
-    echo $component:"$(awk -F['_'] '/'$component'-'$VERSION'/ { print $2 }' RELEASE.INFO | cut -d. -f1 | sed 's/git//g')",
-done
-echo cortx-csm_agent:"$(awk -F['_'] '/cortx-csm_agent-'$VERSION'/ { print $3 }' RELEASE.INFO | cut -d. -f1)",
+        sed -i '/KERNEL/d' RELEASE.INFO
+        COMPONENTS="$(tr '\n' ' ' < ./$SERVICE/cortx-componenet-rpms.txt)"
+        for component in $COMPONENTS
+        do
+                echo $component:"$(awk -F['_'] '/'$component'-'$VERSION'/ { print $2 }' RELEASE.INFO | cut -d. -f1 | sed 's/git//g')",
+        done
+        if [ "$SERVICE" == "cortx-control" ]; then
+                echo cortx-csm_agent:"$(awk -F['_'] '/cortx-csm_agent-'$VERSION'/ { print $3 }' RELEASE.INFO | cut -d. -f1)",
+        fi
 }
 
 
@@ -93,7 +98,7 @@ CREATED_DATE=$(date -u +'%Y-%m-%d %H:%M:%S%:z')
 docker-compose -f ./docker-compose.yml build --force-rm --compress --build-arg GIT_HASH="$CORTX_VERSION" --build-arg VERSION="$VERSION-$DOCKER_BUILD_BUILD" --build-arg CREATED_DATE="$CREATED_DATE" --build-arg BUILD_URL=$BUILD_URL --build-arg ENVIRONMENT=$ENVIRONMENT $SERVICE
 
 if [ "$DOCKER_PUSH" == "yes" ];then
-        echo "Pushing Docker image to GitHub Container Registry"
+        echo "Pushing Docker image to $REGISTRY"
         docker tag $SERVICE:$TAG $REGISTRY/$PROJECT/$SERVICE:$TAG
         docker push $REGISTRY/$PROJECT/$SERVICE:$TAG 
 else
