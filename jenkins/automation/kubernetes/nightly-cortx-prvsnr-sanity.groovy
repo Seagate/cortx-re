0e7e6b60e87662630d46430115ff4ece03d97a25
Nitish Singh <nitish.singh@seagate.com>
2022-02-09 03:37:46 -0700

    Added parameters.
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>
    
    Added parameters.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>

--- jenkins/automation/kubernetes/nightly-cortx-prvsnr-sanity.groovy
+++ jenkins/automation/kubernetes/nightly-cortx-prvsnr-sanity.groovy
@@ -13,20 +13,20 @@ pipeline {
         buildDiscarder(logRotator(daysToKeepStr: '20', numToKeepStr: '20'))
     }
 
-    environment {
-        CORTX_RE_BRANCH = "main"
-        CORTX_RE_REPO = "https://github.com/Seagate/cortx-re/"
-    }
+    parameters {
+        string(name: 'CORTX_RE_BRANCH', defaultValue: 'main', description: 'Branch or GitHash for Cluster Setup scripts', trim: true)
+        string(name: 'CORTX_RE_REPO', defaultValue: 'https://github.com/Seagate/cortx-re/', description: 'Repository for Cluster Setup scripts', trim: true)
+        // Please configure CORTX_IMAGE, hosts, SNS_CONFIG, DIX_CONFIG, CORTX_SCRIPTS_BRANCH and CORTX_SCRIPTS_REPO parameter in Jenkins job configuration.
+        // Please configure M_NODE, HOST_PASS  and EMAIL_RECEPIENTS parameter in Jenkins job configuration for prvsnr sanity test.
 
-    // Please configure CORTX_IMAGE, hosts, SNS_CONFIG, DIX_CONFIG, CORTX_SCRIPTS_BRANCH and CORTX_SCRIPTS_REPO parameter in Jenkins job configuration.
-    // Please configure M_NODE, HOST_PASS  and EMAIL_RECEPIENTS parameter in Jenkins job configuration for prvsnr sanity test.
+    }
 
     stages {
         stage ("Deploy CORTX-Prvsnr") {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 script {
-                    def cortxCluster = build job: '/Cortx-kubernetes/setup-cortx-cluster', wait: true,
+                    build job: '/Cortx-kubernetes/setup-cortx-cluster', wait: true,
                     parameters: [
                         string(name: 'CORTX_RE_BRANCH', value: "${CORTX_RE_BRANCH}"),
                         string(name: 'CORTX_RE_REPO', value: "${CORTX_RE_REPO}"),
@@ -37,8 +37,6 @@ pipeline {
                         string(name: 'CORTX_SCRIPTS_BRANCH', value: "${CORTX_SCRIPTS_BRANCH}"),
                         string(name: 'CORTX_SCRIPTS_REPO', value: "${CORTX_SCRIPTS_REPO}")
                     ]
-                    env.cortxcluster_build_url = cortxCluster.absoluteUrl
-                    env.cortxCluster_status = cortxCluster.currentResult
                 }
             }
         }
@@ -48,14 +46,12 @@ pipeline {
                 script { build_stage = env.STAGE_NAME }
                 script {
                     catchError(stageResult: 'FAILURE') {
-                        def prvsnrSanity = build job: '/Provisioner/Prvsnr-Sanity-Test', wait: true,
+                        build job: '/Provisioner/Prvsnr-Sanity-Test', wait: true,
                         parameters: [
                             string(name: 'M_NODE', value: "${M_NODE}"),
                             string(name: 'HOST_PASS', value: "${HOST_PASS}"),
                             string(name: 'EMAIL_RECEPIENTS', value: "${EMAIL_RECEPIENTS}")
                         ]
-                        env.prvsnrSanity_build_url = prvsnrSanity.absoluteUrl
-                        env.prvsnrSanity_status = prvsnrSanity.currentResult
                     }
                 }
             }
