4d7add8255fd6fc2c067bf78f2dff46ce5eca324
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-02-12 00:18:13 +0530

    Increase timeout
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/internal-ci/generic/cortx-rgw.groovy
+++ jenkins/internal-ci/generic/cortx-rgw.groovy
@@ -16,7 +16,7 @@ pipeline {
     }
 
     options {
-        timeout(time: 120, unit: 'MINUTES')
+        timeout(time: 300, unit: 'MINUTES')
         timestamps()
         ansiColor('xterm')   
         disableConcurrentBuilds()   
