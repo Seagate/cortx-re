0b2d86630ca130fe4e505f315967bf1d675a8243
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-02-18 15:30:27 +0530

    Install libfabric from third-party
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/generic/motr.groovy
+++ jenkins/custom-ci/generic/motr.groovy
@@ -65,6 +65,11 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 sh label: '', script: '''
+                    yum-config-manager --add-repo=http://cortx-storage.colo.seagate.com/releases/cortx/third-party-deps/rockylinux/rockylinux-8.4-2.0.0-latest/
+                    yum --nogpgcheck -y --disablerepo="EOS_Rocky_8_OS_x86_64_Rocky_8" install libfabric-1.11.2 libfabric-devel-1.11.2
+                    '''
+
+                sh label: '', script: '''
                         export build_number=${BUILD_ID}
                         kernel_src=$(ls -1rd /lib/modules/*/build | head -n1)
                         cp cortx-motr.spec.in cortx-motr.spec
