c320f9336078119f3889d6f1fe64a9dc5f2a6f31
nikhilpatil2995 <nikhil.patil@seagate.com>
2022-02-21 11:22:26 +0530

    change changelogset job path
    Signed-off-by: nikhilpatil2995 <nikhil.patil@seagate.com>

--- jenkins/automation/kubernetes/nightly-build-and-deploy.groovy
+++ jenkins/automation/kubernetes/nightly-build-and-deploy.groovy
@@ -146,11 +146,10 @@ pipeline {
             }
             steps {
                 script {
-                    def changelog = build job: '/Release_Engineering/re-workspace/ap_space/changelog-generation', wait: true, propagate: false,
+                    def changelog = build job: '/Release_Engineering/Cortx-Automation/changelog-generation', wait: true, propagate: false,
                     parameters: [
                         string(name: 'BUILD_FROM', value: "ghcr.io/seagate/cortx-all:${VERSION}-${BUILD_NUMBER}-${GITHUB_TAG_SUFFIX}"),
                         string(name: 'BUILD_TO', value: "ghcr.io/seagate/cortx-all:${VERSION}-${last_success_build_number}-${GITHUB_TAG_SUFFIX}"),
-                        //   string(name: 'BUILD_LOCATION', value: "ghcr.io/seagate/cortx-all:${VERSION}-${BUILD_NUMBER}-${GITHUB_TAG_SUFFIX}"),
                     ]
                     // env.changeset_log_url = changelog.absoluteUrl
                     copyArtifacts filter: 'CHANGESET.txt', fingerprintArtifacts: true, flatten: true, optional: true, projectName: '/Release_Engineering/re-workspace/ap_space/changelog-generation', selector: lastCompleted(), target: ''
