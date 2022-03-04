a604e90c7d95e0bf51a0d200a877f57b755cc9d4
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-03-04 13:54:12 +0530

    Corrected syntax and variable details
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/custom-ci/generic/custom-ci.groovy
+++ jenkins/custom-ci/generic/custom-ci.groovy
@@ -174,7 +174,7 @@ pipeline {
                         script { build_stage = env.STAGE_NAME }
                         script {
                             try {
-                                def habuild = build job: '/GitHub-custom-ci-builds/generic/cortx-rgw-integration-build', wait: true,
+                                def rgwintegrationbuild = build job: '/GitHub-custom-ci-builds/generic/cortx-rgw-integration-build', wait: true,
                                           parameters: [
                                               string(name: 'CORTX_RGW_INTEGRATION_URL', value: "${CORTX_RGW_INTEGRATION_URL}"),
                                               string(name: 'CORTX_RGW_INTEGRATION_BRANCH', value: "${CORTX_RGW_INTEGRATION_BRANCH}"),
@@ -182,7 +182,7 @@ pipeline {
                                                 ]
                             } catch (err) {
                                 build_stage = env.STAGE_NAME
-                                error "Failed to Build HA"
+                                error "Failed to Build RGW Integration"
                             }
                         }
                     }
