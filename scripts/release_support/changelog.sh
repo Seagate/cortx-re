1a66b582745341f66ce25f47c9c05403eec4f704
Mukul Malhotra <mukul.malhotra@seagate.com>
2022-03-10 04:24:19 -0700

    addition of rgw
    Signed-off-by: Mukul Malhotra <mukul.malhotra@seagate.com>

--- scripts/release_support/changelog.sh
+++ scripts/release_support/changelog.sh
@@ -46,6 +46,8 @@ declare -A COMPONENT_LIST=(
 [cortx-csm_agent]="https://github.com/Seagate/cortx-manager.git"
 [cortx-csm_web]="https://github.com/Seagate/cortx-management-portal.git"
 [cortx-py-utils]="https://github.com/Seagate/cortx-utils.git"
+[cortx-rgw]="https://github.com/Seagate/cortx-rgw.git"
+[cortx-rgw-integration]="https://github.com/Seagate/cortx-rgw-integration.git"
 )
 
 clone_dir="/root/git_build_checkin_stats"
@@ -105,7 +107,7 @@ do
                 elif [ "$component" == "cortx-csm_agent" ] || [ "$component" == "cortx-csm_web" ]; then
                         start_hash=$(grep "$component-" start_build_manifest.txt | head -1 | awk -F['_'] '{print $3}' |  cut -d. -f1); echo "$start_hash"
                         target_hash=$(grep "$component-" target_build_manifest.txt | head -1 | awk -F['_'] '{print $3}' |  cut -d. -f1); echo "$target_hash"
-                elif [ "$component" == "cortx-provisioner" ]; then
+                elif [ "$component" == "cortx-provisioner" ] || [ "$component" == "cortx-rgw" ] || [ "$component" == "cortx-rgw-integration" ]; then
                         start_hash=$(grep "$component-" start_build_manifest.txt | tail -1 | awk -F['_'] '{print $2}' | sed 's/git//g' | cut -d. -f1); echo "$start_hash"
                         target_hash=$(grep "$component-" target_build_manifest.txt | tail -1 | awk -F['_'] '{print $2}' | sed 's/git//g' | cut -d. -f1); echo "$target_hash"
                 else
