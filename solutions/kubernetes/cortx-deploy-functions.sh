6f4d9f4cd92391c5136c7d59a8785c3e7b490244
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-04-18 16:14:16 +0530

    chore: change motr client instance
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- solutions/kubernetes/cortx-deploy-functions.sh
+++ solutions/kubernetes/cortx-deploy-functions.sh
@@ -106,7 +106,7 @@ function update_solution_config(){
         yq e -i '.solution.common.s3.start_port_num = 28051' solution.yaml
         yq e -i '.solution.common.s3.max_start_timeout = 240' solution.yaml
         yq e -i '.solution.common.s3.extra_configuration = ""' solution.yaml
-        yq e -i '.solution.common.motr.num_client_inst = 2' solution.yaml   
+        yq e -i '.solution.common.motr.num_client_inst = 1' solution.yaml   
         yq e -i '.solution.common.motr.start_port_num = 29000' solution.yaml
         yq e -i '.solution.common.motr.extra_configuration = ""' solution.yaml
         yq e -i '.solution.common.hax.protocol = "https"' solution.yaml
