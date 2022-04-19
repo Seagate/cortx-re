a18fed068f5fb7c44bb95565cc30ae1197d93c03
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-04-19 19:42:54 +0530

    Increased timeout
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/performance-ci/performance-sanity.groovy
+++ jenkins/performance-ci/performance-sanity.groovy
@@ -6,7 +6,7 @@ pipeline {
     }
     
     options {
-        timeout(time: 240, unit: 'MINUTES')
+        timeout(time: 1440, unit: 'MINUTES')
         timestamps()
         buildDiscarder(logRotator(daysToKeepStr: '30', numToKeepStr: '30'))
         ansiColor('xterm')
