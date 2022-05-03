8cf59580d462dcd7a78c122909cc1f0edc6f7ee2
Abhijit Patil <abhijit.patil@seagate.com>
2022-05-03 14:08:11 +0530

    Changes for cortx credetails
    Signed-off-by: Abhijit Patil <abhijit.patil@seagate.com>

--- scripts/automation/s3-test/create_account.sh
+++ scripts/automation/s3-test/create_account.sh
@@ -36,3 +36,4 @@ fi
 
 curl -k -v -d "{\"uid\":\"${S3_USER}\", \"display_name\":\"${S3_USER}\", \"email\":\"${S3_USER}@seagate.com\"}" https://s3.seagate.com:31169/api/v2/s3/iam/users --header "Authorization: Bearer $TOKEN" > ${S3_USER}_${BUILD_NUMBER}.log
 check_status "Access key & Secret key not created properly please look into that."
+cat ${S3_USER}_${BUILD_NUMBER}.log
