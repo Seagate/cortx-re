580edd640face4b3ab407aeba13f2521ff688dad
Abhijit Patil <abhijit.patil@seagate.com>
2022-05-11 10:46:05 +0530

    Changes for s3 url
    Signed-off-by: Abhijit Patil <abhijit.patil@seagate.com>

--- jenkins/automation/s3server/s3_ceph_testcase.groovy
+++ jenkins/automation/s3server/s3_ceph_testcase.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent {
 		node {
             // Agent created with 4GB ram/16GB memory in EOS_SVC_RE1 account 
-			label "s3-compatibility-test-ceph-rhev-7.9"
+			label "docker-image-builder-centos-7.9.2009"
             // Use custom workspace for easy troublshooting
             customWorkspace "/root/compatability-test/${INTEGRATION_TYPE}"
 		}
