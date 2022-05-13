fae2ec9df5448306eae974b54ccf39f4e125e931
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-05-13 12:01:17 +0530

    Update repo-health.groovy
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/automation/reports/repo-health.groovy
+++ jenkins/automation/reports/repo-health.groovy
@@ -1,42 +1,57 @@
-#!/bin/bash
-#
-# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# For any questions about this software or licensing,
-# please email opensource@seagate.com or cortx-questions@seagate.com.
-#
+pipeline {
+    agent {
+        node {
+            label 'k8-executor'
+        }
+    }
 
-set -eo pipefail
+    options {
+        timeout(time: 420, unit: 'MINUTES')
+        timestamps()
+    }
+    
+    environment {
+        GH_OATH = credentials('shailesh-github-token')
+        CODACY_OATH = credentials('shailesh-codacy-auth')
+    }
 
-if [ -z "$GH_OATH" ]; then echo "GH_OATH is not set. Existing..."; exit 1; fi
-if [ -z "$CODACY_OATH" ]; then echo "CODACY_OATH is not set. Existing..."; exit 1; fi
+    parameters {
+        string(name: 'CORTX_RE_BRANCH', defaultValue: 'main', description: 'Branch or GitHash to Generate Repo Health Report', trim: true)
+        string(name: 'CORTX_RE_REPO', defaultValue: 'https://github.com/Seagate/cortx-re', description: 'Repository to to Generate Repo Health Report', trim: true)
+    }    
 
-#Install reuired packages
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
-yum install gcc python3-devel git-lfs pango-devel -y
-pip3 install seaborn matplotlib pandas xlrd requests python-dateutil pyGithub jupyterlab django-weasyprint weasyprint==52.5
-git lfs install
+    stages {
 
-#Clone Repo
-git clone https://github.com/Seagate/cortx --depth=1
-pushd cortx && git lfs pull && popd
+        stage('Checkout Script') {
+            steps {
+                script {
+                    checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: "${CORTX_RE_BRANCH}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${CORTX_RE_REPO}"]]]
+                }
+            }
+        }
 
-#Execute notebook
-pushd cortx/metrics
-    mkdir report cache
-    jupyter nbconvert --debug --log-level 10 --execute --to html --ExecutePreprocessor.timeout=18000 --output-dir=/tmp --no-input --output-dir=report --output Repo_Health Repo_Health.ipynb
-#Generate PDF
-    cp report/Repo_Health.html /tmp/ && python3 ./html_to_pdf.py
-popd
+        stage('Generate Report') {
+            steps {
+                script {
+                    sh '''
+                        pushd scripts/reports
+                            sh ./repo-health.sh
+                        popd
+                    '''
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            script{ 
+               echo 'Cleanup Workspace.'
+                archiveArtifacts artifacts: "scripts/reports/cortx/metrics/report/*.html, scripts/reports/cortx/metrics/cache/repo_health.pdf", onlyIfSuccessful: false, allowEmptyArchive: true
+                
+                echo 'Cleanup Workspace.'
+                deleteDir() /* clean up our workspace */
+            }
+        }
+    }        
+}
