6bbcf4c586b18346ff2fb0af7b66c26a6a24f61e
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-05-13 18:32:41 +0530

    CORTX-31176 Generate Repo Health report (#1008)
    * Create repo-health.sh
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Enable debug mode
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * DEBUG
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * DEBUG
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Add support for PDF generation
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Fixed path
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Fixed syntax issue
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Remove variable print
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Add jenkinsfile
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Update repo-health.groovy
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Use container node
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>
    
    * Fixed Codacy report
    
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

---
+++ scripts/reports/repo-health.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
+set -eo pipefail
+
+if [ -z "$GH_OATH" ]; then echo "GH_OATH is not set. Existing..."; exit 1; fi
+if [ -z "$CODACY_OATH" ]; then echo "CODACY_OATH is not set. Existing..."; exit 1; fi
+
+#Install reuired packages
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
+yum install gcc python3-devel git-lfs pango-devel -y
+pip3 install seaborn matplotlib pandas xlrd requests python-dateutil pyGithub jupyterlab django-weasyprint weasyprint==52.5
+git lfs install
+
+#Clone Repo
+git clone https://github.com/Seagate/cortx --depth=1
+pushd cortx && git lfs pull && popd
+
+#Execute notebook
+pushd cortx/metrics
+    mkdir report cache
+    jupyter nbconvert --debug --log-level 10 --execute --to html --ExecutePreprocessor.timeout=18000 --output-dir=/tmp --no-input --output-dir=report --output Repo_Health Repo_Health.ipynb
+#Generate PDF
+    cp report/Repo_Health.html /tmp/ && python3 ./html_to_pdf.py
+popd
