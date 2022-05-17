8be6e5e67dc36955d643b746b1d1608bd91ffcd6
Abhirup Ghosh <abhirup.ghosh@seagate.com>
2022-05-17 17:11:10 +0530

    updated the script as per request
    Signed-off-by: Abhirup Ghosh <abhirup.ghosh@seagate.com>

--- scripts/security/whitesource_container_scan.sh
+++ scripts/security/whitesource_container_scan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,27 +22,20 @@ source /root/cortx-re/solutions/kubernetes/functions.sh
 #Download WhiteSource Plugin 
 wget http://cortx-storage.colo.seagate.com/releases/cortx/security/whitesource/ws-k8s-agent.tar && tar -xvf ws-k8s-agent.tar >/dev/null 2>&1
 
-#user input
-read -p 'Please enter url: ' URL
-read -p 'Please enter apiKey: ' APIKEY
-read -p 'Please enter userKey: ' USERKEY
-read -p 'Please enter productName: ' PRODUCTNAME
-read -p 'Please enter docker registry: ' REGISTRY
-read -p 'Please enter mainPod details: ' MAINPOD
-read -p 'Please enter workerPod details: ' WORKERPOD
-read -p 'Please enter image pullSecret: ' PULLSECRET
-
 #Updating the configuration file
-sed -Ei "s,(url: ).*,\1$URL,g" /root/ws-k8s-agent/helm-chart/values.yaml; sed -Ei "s,(apiKey: ).*,\1$APIKEY,g" /root/ws-k8s-agent/helm-chart/values.yaml
-sed -Ei "s,(userKey: ).*,\1$USERKEY,g" /root/ws-k8s-agent/helm-chart/values.yaml; sed -Ei "s,(productName: ).*,\1$PRODUCTNAME,g" /root/ws-k8s-agent/helm-chart/values.yaml
-sed -Ei "s,(registry: ).*,\1$REGISTRY,g" /root/ws-k8s-agent/helm-chart/values.yaml; sed -Ei "s,(mainPod: ).*,\1$MAINPOD,g" /root/ws-k8s-agent/helm-chart/values.yaml
-sed -Ei "s,(workerPod: ).*,\1$WORKERPOD,g" /root/ws-k8s-agent/helm-chart/values.yaml; sed -Ei "s,(pullSecret: ).*,\1$PULLSECRET,g" /root/ws-k8s-agent/helm-chart/values.yaml
+sed -Ei 's,(url: ).*,\1"'"$WHITESOURCE_SERVER_URL"'",g' /root/ws-k8s-agent/helm-chart/values.yaml; sed -Ei 's,(apiKey: ).*,\1"'"$API_KEY"'",g' /root/ws-k8s-agent/helm-chart/values.yaml
+sed -Ei 's,(userKey: ).*,\1"'"$USER_KEY"'",g' /root/ws-k8s-agent/helm-chart/values.yaml; sed -Ei "s,(productName: ).*,\1$PRODUCT_NAME,g" /root/ws-k8s-agent/helm-chart/values.yaml
+sed -Ei "s,(registry: ).*,\1$DOCKER_REGISTRY,g" /root/ws-k8s-agent/helm-chart/values.yaml; sed -Ei "s,(mainPod: ).*,\1$MAIN_POD,g" /root/ws-k8s-agent/helm-chart/values.yaml
+sed -Ei "s,(workerPod: ).*,\1$WORKER_POD,g" /root/ws-k8s-agent/helm-chart/values.yaml; sed -Ei 's,(pullSecret: ).*,\1"'"$PULL_SECRET"'",g' /root/ws-k8s-agent/helm-chart/values.yaml
+
+#print values.yaml
+cat /root/ws-k8s-agent/helm-chart/values.yaml | egrep -iw "url: |apiKey: |userKey: |pullSecret: |productName:|registry:|mainPod:|workerPod:"| head -8
 
 #Pulling whitesource images https://ghcr.io/v2/
 docker pull ghcr.io/seagate/whitesource-pre-configure:20.11.1 && docker pull ghcr.io/seagate/whitesource-main:20.11.1 && docker pull ghcr.io/seagate/whitesource-worker:20.11.1
 
 #Uninstall/stop existing scanner
-#cd root/ws-k8s-agent
+#pushd root/ws-k8s-agent
 #helm uninstall whitesource-k8s
 add_common_separator "Run helm uninstall whitesource-k8s to stop existing scanner"
 
