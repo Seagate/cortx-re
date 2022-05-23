77884e43b9140a81f693ef2856530abc524640fa
Aayushi Sharma <aayushi.sharma@seagate.com>
2022-05-23 04:27:17 -0600

    Updated YQ
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>

--- scripts/provisioner/update_solution.sh
+++ scripts/provisioner/update_solution.sh
@@ -20,7 +20,7 @@ source "$WORKSPACE"/solutions/kubernetes/functions.sh
 
 HOST_FILE=$PWD/hosts
 SCRIPT_PATH=/root/cortx-k8s/k8_cortx_cloud
-YQ_VERSION=v4.13.3
+YQ_VERSION=v4.25.1
 YQ_BINARY=yq_linux_386
 
 cp "$WORKSPACE"/scripts/provisioner/hosts "$WORKSPACE"/solutions/kubernetes/hosts
@@ -34,6 +34,7 @@ function clone_script_repo() {
 }
 
 function install_yq_module() {
+    add_secondary_separator "Installing yq-$YQ_VERSION"
     pip3 show yq && pip3 uninstall yq -y
     wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz -O - | tar xz && mv ${YQ_BINARY} /usr/bin/yq
     if [ -f /usr/local/bin/yq ]; then rm -rf /usr/local/bin/yq; fi
