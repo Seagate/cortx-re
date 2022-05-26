d2224a4cc3e4bdf330323695bceabd457054bb7f
Aayushi Sharma <aayushi.sharma@seagate.com>
2022-05-26 05:34:28 -0600

    Added client image
    Signed-off-by: Aayushi Sharma <aayushi.sharma@seagate.com>

--- jenkins/automation/kubernetes/deploy-cortx-k8-cluster.groovy
+++ jenkins/automation/kubernetes/deploy-cortx-k8-cluster.groovy
@@ -23,7 +23,6 @@ pipeline {
         string(name: 'CORTX_ALL_IMAGE', defaultValue: 'ghcr.io/seagate/cortx-all:2.0.0-latest', description: 'CORTX-ALL image', trim: true)
         string(name: 'CORTX_SERVER_IMAGE', defaultValue: 'ghcr.io/seagate/cortx-rgw:2.0.0-latest', description: 'CORTX-SERVER image', trim: true)
         string(name: 'CORTX_DATA_IMAGE', defaultValue: 'ghcr.io/seagate/cortx-data:2.0.0-latest', description: 'CORTX-DATA image', trim: true)
-        string(name: 'CORTX_CLIENT_IMAGE', defaultValue: 'ghcr.io/seagate/cortx-data:2.0.0-latest', description: 'CORTX-DATA image', trim: true)
         choice(
             name: 'DEPLOYMENT_METHOD',
             choices: ['standard', 'data-only'],
