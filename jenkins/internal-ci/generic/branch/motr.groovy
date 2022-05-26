6145859e8b23f8635b2e210065888e6ba419e4e5
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-05-26 22:18:18 +0530

    Disable Hare job trigger
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/internal-ci/generic/branch/motr.groovy
+++ jenkins/internal-ci/generic/branch/motr.groovy
@@ -129,6 +129,7 @@ pipeline {
         }
         
         stage ("Trigger Downstream Jobs") {
+            when { expression { false } }
             parallel {
                 stage ("build S3Server") {
                     when { expression { false } }
@@ -179,8 +180,8 @@ pipeline {
                 '''
                 sh label: 'Tag last_successful for dep component', script: '''
                     # Hare Build 
-                    test -L $build_upload_dir_hare/last_successful && rm -f $build_upload_dir_hare/last_successful
-                    ln -s $build_upload_dir_hare/$HARE_BUILD_NUMBER $build_upload_dir_hare/last_successful
+                    #test -L $build_upload_dir_hare/last_successful && rm -f $build_upload_dir_hare/last_successful
+                    #ln -s $build_upload_dir_hare/$HARE_BUILD_NUMBER $build_upload_dir_hare/last_successful
 
                     #test -L /mnt/bigstorage/releases/cortx/components/github/rgw/rockylinux-8.4/dev/hare/last_successful && rm -rf /mnt/bigstorage/releases/cortx/components/github/rgw/rockylinux-8.4/dev/hare/last_successful
                     #ln -s /mnt/bigstorage/releases/cortx/components/github/rgw/rockylinux-8.4/dev/hare/$HARE_BUILD_NUMBER /mnt/bigstorage/releases/cortx/components/github/rgw/rockylinux-8.4/dev/hare/last_successful
