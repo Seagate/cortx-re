0e5419a706f914f4e949ff1e189317e8cf34d6bd
Abhirup Ghosh <abhirup.ghosh@seagate.com>
2022-05-30 18:53:51 +0530

    fixed the ssh session issue
    Signed-off-by: Abhirup Ghosh <abhirup.ghosh@seagate.com>

--- jenkins/deployment/community/community-build-cortx.groovy
+++ jenkins/deployment/community/community-build-cortx.groovy
@@ -15,11 +15,8 @@ pipeline {
 
     parameters {
         string(name: 'CORTX_RE_BRANCH', defaultValue: 'main', description: 'Branch or GitHash for CORTX Cluster scripts', trim: true)
-         choice(
-            name: 'OS_VERSION',
-            choices: ['CentOS 7.9.2009 x86_64', 'CentOS 7.8.2003 x86_64'],
-            description: 'Operating system version'
-        )
+        string(name: 'CORTX_RE_REPO', defaultValue: 'https://github.com/Seagate/cortx-re/', description: 'Repository for CORTX Cluster scripts', trim: true)
+        string(name: 'OS_VERSION', defaultValue: 'CentOS 7.9.2009 x86_64', description: 'Operating system version', trim: true)
         string(name: 'REGION', defaultValue: 'ap-south-1', description: 'AWS region', trim: true)
         text(defaultValue: '''hostname=<hostname>,user=<user>,pass=<password>''', description: 'VM details to be used. First node will be used as Primary node', name: 'hosts')
 
@@ -49,11 +46,10 @@ pipeline {
                     ./tool_setup.sh
                     sed -Ei 's,(os_version          =).*,\1 "'"$OS_VERSION"'",g' user.tfvars && sed -Ei 's,(region              =).*,\1 "'"$REGION"'",g' user.tfvars && sed -Ei 's,(security_group_cidr =).*,\1 "'"$VM_IP/32"'",g' user.tfvars
                     cat user.tfvars | tail -3
+                    terraform validate && terraform apply -var-file user.tfvars --auto-approve
                     ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@$AWS_IP sudo bash /home/centos/setup.sh
                     sleep 60
-                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' root@$AWS_IP
-                    git clone https://github.com/Seagate/cortx-re && pushd $PWD/cortx-re/solutions/community-deploy
-                    time ./build-cortx.sh -b ${CORTX_RE_BRANCH}
+                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' root@$AWS_IP git clone https://github.com/Seagate/cortx-re && pushd $PWD/cortx-re/solutions/community-deploy && time ./build-cortx.sh -b ${CORTX_RE_BRANCH}
                 popd
             '''
             }
