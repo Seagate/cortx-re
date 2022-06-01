4a876b03867fd78e20be31af33e0bac20f93d8b1
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-06-01 09:52:09 -0600

    Fix value print issues
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/performance/run-performace-tests-functions.sh
+++ scripts/performance/run-performace-tests-functions.sh
@@ -82,7 +82,8 @@ function update_setup_confiuration() {
     S3_CONFIG_FILE=$SCRIPT_LOCATION/performance/PerfPro/roles/benchmark/vars/s3config.yml 
     sed -i -e '/CLUSTER_PASS/s/:/: '$PRIMARY_CRED'/g' -e '/END_POINTS/s/:/: '${ENDPOINT_URL//\//\\/}'/g' $CONFIG_FILE
     sed -i -e '/node_number_srvnode-*/d' -e '/#client_number/d' -e '/NODES/{n;s/.*/  - 1: '$PRIMARY_NODE'/}' -e '/CLIENTS/{n;s/.*/  - 1: '$CLIENT_NODE'/}' $CONFIG_FILE
-    #sed -i -e '/BUILD_URL/s/\:/: '${BUILD_URL//\//\\/}'/g' -e 's/https\:\/\/s3.seagate.com/'${ENDPOINT_URL//\//\\/}'/g' $CONFIG_FILE
+    sed -i -e '/BUILD_URL/s/\:/: '${BUILD_URL//\//\\/}'/g' 
+    #-e 's/https\:\/\/s3.seagate.com/'${ENDPOINT_URL//\//\\/}'/g' $CONFIG_FILE
 
     if [ $CLUSTER_TYPE == VM ]; then
 	sed -i -e 's/00/0/g' -e 's/450/45/g'  /root/performance-scripts/performance/PerfPro/roles/benchmark/vars/s3config.yml    
