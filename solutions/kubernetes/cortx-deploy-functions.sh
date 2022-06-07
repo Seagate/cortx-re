096f73412177e698281c03a003d6bf3e3dc4b5c4
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-06-07 10:22:25 +0530

    Remove cortx-hax-svc service name
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- solutions/kubernetes/cortx-deploy-functions.sh
+++ solutions/kubernetes/cortx-deploy-functions.sh
@@ -115,7 +115,6 @@ function update_solution_config(){
         yq e -i '.solution.common.motr.start_port_num = 29000' solution.yaml
         yq e -i '.solution.common.motr.extra_configuration = ""' solution.yaml
         yq e -i '.solution.common.hax.protocol = "https"' solution.yaml
-        yq e -i '.solution.common.hax.service_name = "cortx-hax-svc"' solution.yaml
         yq e -i '.solution.common.hax.port_num = 22003' solution.yaml
         yq e -i '.solution.common.storage_sets.name = "storage-set-1"' solution.yaml
 
