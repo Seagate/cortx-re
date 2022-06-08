216dead8ecce84377ff38cc932bf7bd8d70f377b
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-06-08 16:06:17 +0530

    chore: update solution.yaml
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- solutions/kubernetes/cortx-deploy-functions.sh
+++ solutions/kubernetes/cortx-deploy-functions.sh
@@ -116,6 +116,7 @@ function update_solution_config(){
         yq e -i '.solution.common.motr.group_size = 1' solution.yaml
         yq e -i '.solution.common.motr.extra_configuration = ""' solution.yaml
         yq e -i '.solution.common.hax.protocol = "https"' solution.yaml
+        yq e -i '.solution.common.hax.service_name = "cortx-hax-svc"' solution.yaml
         yq e -i '.solution.common.hax.port_num = 22003' solution.yaml
         yq e -i '.solution.common.storage_sets.name = "storage-set-1"' solution.yaml
 
