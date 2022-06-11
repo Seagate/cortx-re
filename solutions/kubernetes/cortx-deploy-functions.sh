4134d54b3e93efc5493f328bcd4276508988980c
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-06-12 01:56:45 +0530

    chore: update namespace var
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- solutions/kubernetes/cortx-deploy-functions.sh
+++ solutions/kubernetes/cortx-deploy-functions.sh
@@ -247,7 +247,7 @@ function copy_solution_config() {
         if [ -f '$SOLUTION_CONFIG' ]; then echo "file $SOLUTION_CONFIG not available..."; exit 1; fi	
         cp $SOLUTION_CONFIG .
         yq eval -i 'del(.solution.nodes)' solution.yaml
-        NAMESPACE=$(yq e '.solution.namespace' solution.yaml)
+        env.NAMESPACE=$(yq e '.solution.namespace' solution.yaml)
     popd 
 }
 
