00e560f14e9226b75e07764ff5c627069c555677
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-06-13 19:02:58 +0530

    chore: remove kubectl context setup from upgrade script
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- solutions/kubernetes/cortx-upgrade.sh
+++ solutions/kubernetes/cortx-upgrade.sh
@@ -103,7 +103,7 @@ function upgrade_cluster() {
     pushd deploy-scripts/k8_cortx_cloud &&
     echo $SOLUTION_CONFIG
     NAMESPACE='cortx' &&
-    if [ "$SOLUTION_CONFIG_TYPE" == "manual" ]; then copy_solution_config $SOLUTION_CONFIG $PWD; setup_kubectl_context $NAMESPACE; fi &&
+    if [ "$SOLUTION_CONFIG_TYPE" == "manual" ]; then copy_solution_config $SOLUTION_CONFIG $PWD; fi &&
     add_secondary_separator 'Download Upgrade Images' && 
     pull_image $CORTX_SERVER_IMAGE &&
     pull_image $CORTX_DATA_IMAGE &&
