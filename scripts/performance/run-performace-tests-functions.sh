031af4e9beec28b347b6fee83ec0ae351738a858
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-06-14 10:27:48 +0530

    Debug
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/performance/run-performace-tests-functions.sh
+++ scripts/performance/run-performace-tests-functions.sh
@@ -97,7 +97,7 @@ function execute_perfpro() {
     yum install ansible -y
     pushd $SCRIPT_LOCATION/performance/PerfPro
     add_primary_separator "Executing Ansible CLI"
-    ANSIBLE_LOG_PATH=$ANIBLE_LOG_FILE ansible-playbook perfpro.yml -i inventories/hosts --extra-vars '{ "EXECUTION_TYPE" : "sanity" ,"REPOSITORY":{"motr":"cortx-motr","rgw":"cortx-rgw"} , "COMMIT_ID": { "main" : "d1234c" , "dev" : "a5678b"},"PR_ID" : "cortx-rgw/1234" , "USER":"Shailesh Vaidya","GID" : "729494" }' -v
+    #ANSIBLE_LOG_PATH=$ANIBLE_LOG_FILE ansible-playbook perfpro.yml -i inventories/hosts --extra-vars '{ "EXECUTION_TYPE" : "sanity" ,"REPOSITORY":{"motr":"cortx-motr","rgw":"cortx-rgw"} , "COMMIT_ID": { "main" : "d1234c" , "dev" : "a5678b"},"PR_ID" : "cortx-rgw/1234" , "USER":"Shailesh Vaidya","GID" : "729494" }' -v
     popd
 }
 
