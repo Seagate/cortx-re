a6d9242b990c57ed2c03ba7a715f5e2e7a83b01e
vijwani-seagate <anmol.vijwani@seagate.com>
2022-06-16 13:18:47 +0530

    Adding groovy script to get Jenkins node status
    Signed-off-by: vijwani-seagate <anmol.vijwani@seagate.com>

---
+++ jenkins/env_setup/Jenkinsnodedetails.groovy
@@ -0,0 +1,26 @@
+Jenkins h = Jenkins.getInstance();
+for (Node n : h.getNodes()) {
+for (hudson.plugins.jobConfigHistory.ComputerConfigHistoryAction m : n.getComputer().actions) {
+    for(hudson.plugins.jobConfigHistory.ConfigInfo config : m.getAgentConfigs() ){
+        println('Slave:' + n.name + ' User:'+ config.getUser() + ' UserId:'+ config.getUserID());
+    }
+}
+println('Host:'+ n.launcher.host + ' Label:' + n.getLabelString() + ' Computer.isOffline:' + n.getComputer().isOffline()) ;
+println "--------------------------------"
+}
+//Have to make changes accordingly to Jenkins Production
+/*
+pipeline{
+    agent { label 'master'}
+    stages{
+        stage('Gettinglogfile')
+        {  
+           steps
+           {
+                sh 'curl -u user2:user2@123 -o $WORKSPACE/console.log "http://ssc-vm-g4-rhev4-1690.colo.seagate.com:8080/job/Finduserdetails/lastBuild/consoleText"'
+           }
+          
+        }
+        
+    }
+} */
