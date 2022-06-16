4ba53fca27371605879596ddb3caf3cbd9a391e0
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-06-17 02:41:14 +0530

    test: fetch actual image
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- solutions/kubernetes/cortx-upgrade.sh
+++ solutions/kubernetes/cortx-upgrade.sh
@@ -84,12 +84,9 @@ function upgrade_cluster() {
     ssh_primary_node "source /var/tmp/functions.sh &&
     if [ "$SOLUTION_CONFIG_TYPE" == "manual" ]; then copy_solution_config "$REMOTE_SOLUTION_CONFIG" "$SCRIPT_LOCATION"; fi &&
     add_secondary_separator 'Download Upgrade Images' &&
-    if [[ "$CORTX_SERVER_IMAGE" =~ "latest" ]]; then CORTX_SERVER_IMAGE=$( get_actual_image_version $CORTX_SERVER_IMAGE ); fi &&
-    if [[ "$CORTX_DATA_IMAGE" =~ "latest" ]]; then CORTX_DATA_IMAGE=$( get_actual_image_version $CORTX_DATA_IMAGE ); fi &&
-    if [[ "$CORTX_CONTROL_IMAGE" =~ "latest" ]]; then CORTX_CONTROL_IMAGE=$( get_actual_image_version $CORTX_CONTROL_IMAGE ); fi &&
-    pull_image $CORTX_SERVER_IMAGE &&
-    pull_image $CORTX_DATA_IMAGE &&
-    pull_image $CORTX_CONTROL_IMAGE &&
+    CORTX_SERVER_IMAGE=$(pull_image $CORTX_SERVER_IMAGE) &&
+    CORTX_DATA_IMAGE=$(pull_image  $CORTX_DATA_IMAGE) &&
+    CORTX_CONTROL_IMAGE=$(pull_image  $CORTX_CONTROL_IMAGE) &&
     add_secondary_separator 'Updating CORTX Images info in solution.yaml' &&
     update_image control-pod $CORTX_CONTROL_IMAGE &&
     update_image data-pod $CORTX_DATA_IMAGE &&
