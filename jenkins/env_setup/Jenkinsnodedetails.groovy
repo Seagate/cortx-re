4199f6cfec6c71a5dd23a599fdf024a5e93b8b2a
vijwani-seagate <anmol.vijwani@seagate.com>
2022-06-17 17:02:09 +0530

    Changed the name for troubleshooting
    Signed-off-by: vijwani-seagate <anmol.vijwani@seagate.com>

--- jenkins/env_setup/Jenkinsnodedetails.groovy
+++ jenkins/env_setup/Jenkinsnodedetails.groovy
@@ -1,9 +1,9 @@
 for (Node n : Jenkins.get().getNodes()) {
-println('Host:'+ n.launcher.host + ' Label:' + n.getLabelString() + ' Computer.isOffline:' + n.getComputer().isOffline()) ;
+println('Host_node:'+ n.launcher.host + ' Label:' + n.getLabelString() + ' Computer.isOffline:' + n.getComputer().isOffline()) ;
 println "--------------------------------"
         for (hudson.plugins.jobConfigHistory.ComputerConfigHistoryAction m : n.getComputer().actions) {
                 for(hudson.plugins.jobConfigHistory.ConfigInfo config : m.getSlaveConfigs()) {
-                        println('Slave:' + n.name + ' User:'+ config.getUser() + ' UserId:'+ config.getUserID());
+                        println('Slave_name:' + n.name + ' User:'+ config.getUser() + ' UserId:'+ config.getUserID());
                  }
         }
 }
