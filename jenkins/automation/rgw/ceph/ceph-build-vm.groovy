1a63802314a53b7e19856e9400935fe0f7f1c406
Nitish Singh <nitish.singh@seagate.com>
2022-06-24 13:56:02 +0530

    CORTX-30132: Ceph image build (#1055)
    * Ceph container image build.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>

--- jenkins/automation/rgw/ceph/ceph-build-vm.groovy
+++ jenkins/automation/rgw/ceph/ceph-build-vm.groovy
@@ -14,16 +14,23 @@ pipeline {
     }
 
     environment {
-        component="ceph"
-        build_upload_dir="/mnt/bigstorage/releases/ceph/${component}"
-        VM_BUILD=true
+        BUILD_LOCATION = "/var/log/ceph-build/${BUILD_NUMBER}"
+        MOUNT = "cortx-storage.colo.seagate.com:/mnt/data1/releases/ceph"
+        build_upload_dir = "/mnt/bigstorage/releases/ceph"
+        VM_BUILD = true
+
+        // Motr dependencies environment variables
+        release_tag = "last_successful_prod"
+        os_version = "rockylinux-8.4"
+        branch = "main"
     }
 
     parameters {
-        string(name: 'CORTX_RE_REPO', defaultValue: 'https://github.com/Seagate/cortx-re/', description: 'Repository for Cluster Setup scripts', trim: true)
-        string(name: 'CORTX_RE_BRANCH', defaultValue: 'main', description: 'Branch or GitHash for Cluster Setup scripts', trim: true)
-        string(name: 'CEPH_REPO', defaultValue: 'https://github.com/ceph/ceph/', description: 'Repository for Cluster Setup scripts', trim: true)
-        string(name: 'CEPH_BRANCH', defaultValue: 'quincy', description: 'Branch or GitHash for Cluster Setup scripts', trim: true)
+        string(name: 'CORTX_RE_REPO', defaultValue: 'https://github.com/Seagate/cortx-re/', description: 'Repository for Cluster Setup scripts.', trim: true)
+        string(name: 'CORTX_RE_BRANCH', defaultValue: 'main', description: 'Branch or GitHash for Cluster Setup scripts.', trim: true)
+        string(name: 'CEPH_REPO', defaultValue: 'ceph/ceph', description: 'Repository for Cluster Setup scripts.', trim: true)
+        string(name: 'CEPH_BRANCH', defaultValue: 'quincy', description: 'Branch or GitHash for Cluster Setup scripts.', trim: true)
+
         choice(
             name: 'BUILD_OS',
             choices: ['rockylinux-8.4', 'ubuntu-20.04', 'centos-8'],
@@ -44,27 +51,27 @@ pipeline {
         stage ('Build Ceph Binary Packages') {
             steps {
                 script { build_stage = env.STAGE_NAME }
-                sh label: 'Build Binary Packages', script: '''
-                    pushd solutions/kubernetes/
-                        export CEPH_REPO=${CEPH_REPO}
-                        export CEPH_BRANCH=${CEPH_BRANCH}
-                        export BUILD_OS=${BUILD_OS}
-                        bash ceph-binary-build.sh --ceph-build /var/log/ceph-build
-                    popd
-                '''
+                sh label: 'Build Binary Packages', script: """
+                pushd solutions/kubernetes/
+                    export CEPH_REPO=${CEPH_REPO}
+                    export CEPH_BRANCH=${CEPH_BRANCH}
+                    export BUILD_OS=${BUILD_OS}
+                    bash ceph-binary-build.sh --ceph-build ${BUILD_LOCATION}
+                popd
+                """
             }
         }
 
         stage ('Upload RPMS') {
             steps {
                 script { build_stage = env.STAGE_NAME }
-                sh label: 'Upload RPMS', script: '''
+                sh label: 'Upload RPMS', script: """
                 pushd solutions/kubernetes/
                     export CEPH_BRANCH=${CEPH_BRANCH}
                     export BUILD_OS=${BUILD_OS}
-                    bash ceph-binary-build.sh --upload-packages /var/log/ceph-build cortx-storage.colo.seagate.com:/mnt/data1/releases/ceph
+                    bash ceph-binary-build.sh --upload-packages ${BUILD_LOCATION} ${MOUNT}
                 popd
-                '''
+                """
             }
         }
     }
@@ -72,6 +79,9 @@ pipeline {
     post {
         always {
             cleanWs()
+            sh label: 'Cleanup Build Location', script: """
+            rm -rf ${BUILD_LOCATION}
+            """
         }
     }
 }
