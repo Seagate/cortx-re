3c4a6bd6f2c72868a52ea8b3856923588ece680e
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-07-15 12:15:23 +0530

    chore: update component name
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- scripts/release_support/changelog.sh
+++ scripts/release_support/changelog.sh
@@ -103,7 +103,7 @@ do
         elif [ "$component" == "cortx-csm_agent" ] || [ "$component" == "cortx-csm_web" ]; then
                 start_hash=$(grep "$component-" start_build_manifest.txt | head -1 | awk -F['_'] '{print $3}' |  cut -d. -f1); echo "$start_hash"
                 target_hash=$(grep "$component-" target_build_manifest.txt | head -1 | awk -F['_'] '{print $3}' |  cut -d. -f1); echo "$target_hash"
-        elif [ "$component" == "cortx-provisioner" ] || [ "$component" == "cortx-rgw-integration" ] ; then
+        elif [ "$component" == "cortx-prvsnr" ] || [ "$component" == "cortx-rgw-integration" ] ; then
                 start_hash=$(grep "$component-" start_build_manifest.txt | tail -1 | awk -F['_'] '{print $2}' | sed 's/git//g' | cut -d. -f1); echo "$start_hash"
                 target_hash=$(grep "$component-" target_build_manifest.txt | tail -1 | awk -F['_'] '{print $2}' | sed 's/git//g' | cut -d. -f1); echo "$target_hash"
         elif [ "$component" == "ceph-base" ]; then
