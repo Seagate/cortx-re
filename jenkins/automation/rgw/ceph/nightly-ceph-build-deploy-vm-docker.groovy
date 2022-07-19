3399c60739d46c7fe419a975594e2d2d950a4a7f
Nitish Singh <nitish.singh@seagate.com>
2022-07-19 07:17:27 -0600

    Codacy fix.
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>

--- jenkins/automation/rgw/ceph/nightly-ceph-build-deploy-vm-docker.groovy
+++ jenkins/automation/rgw/ceph/nightly-ceph-build-deploy-vm-docker.groovy
@@ -25,18 +25,12 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 script {
-
-                    try {
-                        def buildCeph = build job: 'Ceph Build in Docker', wait: true,
+                    catchError(stageResult: 'FAILURE') {
+                        build job: 'Ceph Build in Docker', wait: true,
                         parameters: [
                             string(name: 'CORTX_RE_BRANCH', value: "${CORTX_RE_BRANCH}"),
                             string(name: 'CORTX_RE_REPO', value: "${CORTX_RE_REPO}")
                         ]
-                    }
-
-                    catch (err) {
-                        build_stage = env.STAGE_NAME
-                        error "Failed to Build Ceph Packages."
                     }
                 }
             }
@@ -46,14 +40,8 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 script {
-
-                    try {
-                        def buildCephImage = build job: 'Ceph Build Container Image', wait: true
-                    }
-
-                    catch (err) {
-                        build_stage = env.STAGE_NAME
-                        error "Failed to Build Ceph Image."
+                    catchError(stageResult: 'FAILURE') {
+                        build job: 'Ceph Build Container Image', wait: true
                     }
                 }
             }
@@ -63,18 +51,13 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 script {
-                    try {
-                        def destroyCephVM = build job: 'Ceph Destroy', wait: true,
+                    catchError(stageResult: 'FAILURE') {
+                        build job: 'Ceph Destroy', wait: true,
                         parameters: [
                             choice(name: 'DEPLOYMENT_TYPE', value: "VM_DEPLOYMENT"),
                             text(name: 'hosts', value: "${vm_hosts}")
                         ]
-                    }
-                    
-                    catch (err) {
-                        build_stage = env.STAGE_NAME
-                        error "Failed to Deploy Ceph on VM."
-                    }
+                    }                    
                 }
             }
         }
@@ -83,20 +66,14 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 script {
-
-                    try {
-                        def deployCephVM = build job: 'Ceph VM Deploy', wait: true,
+                    catchError(stageResult: 'FAILURE') {
+                        build job: 'Ceph VM Deploy', wait: true,
                         parameters: [
                             string(name: 'CORTX_RE_BRANCH', value: "${CORTX_RE_BRANCH}"),
                             string(name: 'CORTX_RE_REPO', value: "${CORTX_RE_REPO}"),
                             text(name: 'hosts', value: "${vm_hosts}"),
                             text(name: 'hosts', value: "${OSD_Disks}")
                         ]
-                    }
-
-                    catch (err) {
-                        build_stage = env.STAGE_NAME
-                        error "Failed to Deploy Ceph on VM."
                     }
                 }
             }
@@ -106,17 +83,12 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 script {
-                    try {
-                        def destroyCephVM = build job: 'Ceph Destroy', wait: true,
+                    catchError(stageResult: 'FAILURE') {
+                        build job: 'Ceph Destroy', wait: true,
                         parameters: [
                             choice(name: 'DEPLOYMENT_TYPE', value: "DOCKER_DEPLOYMENT"),
                             text(name: 'hosts', value: "${docker_hosts}")
                         ]
-                    }
-                    
-                    catch (err) {
-                        build_stage = env.STAGE_NAME
-                        error "Failed to Deploy Ceph on VM."
                     }
                 }
             }
@@ -126,20 +98,14 @@ pipeline {
             steps {
                 script { build_stage = env.STAGE_NAME }
                 script {
-
-                    try {
-                        def deployCephinDocker = build job: 'Ceph Docker Deploy', wait: true,
+                    catchError(stageResult: 'FAILURE') {
+                        build job: 'Ceph Docker Deploy', wait: true,
                         parameters: [
                             string(name: 'CORTX_RE_BRANCH', value: "${CORTX_RE_BRANCH}"),
                             string(name: 'CORTX_RE_REPO', value: "${CORTX_RE_REPO}"),
                             text(name: 'hosts', value: "${docker_hosts}"),
                             text(name: 'CEPH_IMAGE', value: "${CEPH_IMAGE}")
                         ]
-                    }
-
-                    catch (err) {
-                        build_stage = env.STAGE_NAME
-                        error "Failed to Deploy Ceph in Docker."
                     }
                 }
             }
@@ -150,21 +116,14 @@ pipeline {
         always {
             cleanWs()
             script {
-                env.build_stage = "${build_stage}"
-
-                def toEmail = ""
-                def recipientProvidersClass = [[$class: 'DevelopersRecipientProvider']]
-                if ( manager.build.result.toString() == "FAILURE" ) {
-                    toEmail = "CORTX.DevOps.RE@seagate.com"
-                    recipientProvidersClass = [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
-                }
-
-                emailext (
+                def recipientProvidersClass = [[$class: 'RequesterRecipientProvider']]
+                mailRecipients = "CORTX.DevOps.RE@seagate.com"
+                emailext ( 
                     body: '''${SCRIPT, template="cluster-setup-email.template"}''',
                     mimeType: 'text/html',
                     subject: "[Jenkins Build ${currentBuild.currentResult}] : ${env.JOB_NAME}",
                     attachLog: true,
-                    to: toEmail,
+                    to: "${mailRecipients}",
                     recipientProviders: recipientProvidersClass
                 )
             }
