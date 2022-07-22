71a42b29d9100d65a93d8f10faa9c367f8d40f0b
Abhirup Ghosh <abhirup.ghosh@seagate.com>
2022-07-22 14:32:33 +0530

    addressed the review comment
    Signed-off-by: Abhirup Ghosh <abhirup.ghosh@seagate.com>

--- jenkins/internal-ci/generic/pr/component_test/Motr_Pre_merge_DTM_Integration.groovy
+++ jenkins/internal-ci/generic/pr/component_test/Motr_Pre_merge_DTM_Integration.groovy
@@ -13,7 +13,7 @@ pipeline {
     }
     parameters {  
 	    string(name: 'MOTR_REPO', defaultValue: 'https://github.com/Seagate/cortx-motr', description: 'Repo for Motr')
-        string(name: 'MOTR_BRANCH', defaultValue: 'main', description: 'Branch for Motr')
+            string(name: 'MOTR_BRANCH', defaultValue: 'main', description: 'Branch for Motr')
     }
     environment {
         // Motr Repo Info
