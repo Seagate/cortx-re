e673fcb313b75c4c0edf73c6e772ae16763726f6
vijwani-seagate <anmol.vijwani@seagate.com>
2022-07-24 05:37:17 +0530

    Changing shell script and folder name as per reviewer
    Signed-off-by: vijwani-seagate <anmol.vijwani@seagate.com>

--- solutions/kubernetes/metrics-server/setup-metric-server.sh
+++ solutions/kubernetes/metrics-server/setup-metric-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,12 +22,14 @@ source /var/tmp/functions.sh
 function install_metricsserver() {
     add_primary_separator "Installing Metrics Server"
     # For Testing purpose: Fetching file from test repo, raised same file with the PR.
-    # kubectl apply -f https://raw.githubusercontent.com/seagate/cortx-re/main/solutions/kubernetes/resources/metric-server-components.yaml
+    # kubectl apply -f https://raw.githubusercontent.com/seagate/cortx-re/main/solutions/kubernetes/metric-server/metric-server-components.yaml
     kubectl apply -f https://raw.githubusercontent.com/vijwani-seagate/kubernetes/master/metric-server-components.yaml
-    kubectl get pods --namespace=kube-system
-    sleep 45
-    kubectl top node
-    check_status "Getting error in Metrics server, Please check!"
+    POD_NAME=$(kubectl get pods --namespace=kube-system | grep metrics-server | cut -d " " -f 1)
+    kubectl wait --for=condition=Ready pod/"$POD_NAME" --namespace=kube-system --timeout=60s
+    COUNT=0
+    until kubectl top node 2>/dev/null || ((COUNT++ >= 5)); do
+        sleep 5
+    done
     add_secondary_separator "Deployed Metrics Server successfully"
 }
 function install_k8dashboard() {
@@ -39,7 +41,7 @@ function install_k8dashboard() {
     DOMAIN=$(kubectl -n kubernetes-dashboard get pods -o wide | awk '$1 ~/kubernetes-dashboard/ {print $7}')
     kubectl -n kubernetes-dashboard patch svc kubernetes-dashboard -p '{"spec": {"ports": [{"port": 443,"targetPort": 8443,"nodePort": 32323}],"type": "NodePort"}}'
     # For Testing purpose: Fetching file from test repo, raised same file with the PR.
-    # kubectl apply -f https://raw.githubusercontent.com/seagate/cortx-re/main/solutions/kubernetes/resources/cluster_admin_svc_acnt.yaml
+    # kubectl apply -f https://raw.githubusercontent.com/seagate/cortx-re/main/solutions/kubernetes/metric-server/cluster_admin_svc_acnt.yaml
     kubectl apply -f https://raw.githubusercontent.com/vijwani-seagate/kubernetes/master/dashboard/cluster_admin_svc_acnt.yaml
     TOKEN=$(kubectl -n kube-system describe sa dashboard-admin | awk '/Tokens/ {print $2}')
     SECRET=$(kubectl -n kube-system describe secret "$TOKEN" | grep token | tail -n +3 | awk '{print $2}')
