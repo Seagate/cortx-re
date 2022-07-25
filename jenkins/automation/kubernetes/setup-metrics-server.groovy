8f76c5e7abdb5e5dd9f3f24b9d886d7f60214454
vijwani-seagate <anmol.vijwani@seagate.com>
2022-07-25 18:39:02 +0530

    Adding shell & groovy
    Signed-off-by: vijwani-seagate <anmol.vijwani@seagate.com>

---
+++ jenkins/automation/kubernetes/setup-metrics-server.groovy
@@ -0,0 +1,94 @@
+pipeline {
+    agent {
+        node {
+            label 'docker-k8-deployment-node'
+        }
+    }
+    
+    options {
+        timeout(time: 240, unit: 'MINUTES')
+        timestamps()
+        buildDiscarder(logRotator(daysToKeepStr: '30', numToKeepStr: '30'))
+        ansiColor('xterm')
+    }
+
+    parameters {
+        string(name: 'CORTX_RE_BRANCH', defaultValue: 'main', description: 'Branch or GitHash for Cluster Setup scripts', trim: true)
+        string(name: 'CORTX_RE_REPO', defaultValue: 'https://github.com/Seagate/cortx-re/', description: 'Repository for Cluster Setup scripts', trim: true)
+        string(defaultValue: '''hostname=<hostname>,user=<user>,pass=<password>''', description: 'Enter Primary node of your K8s Cluster, If 1N cluster make sure it is untainted', name: 'hosts')
+
+    }    
+
+    stages {
+        stage('Checkout Script') {
+            steps { 
+                cleanWs()            
+                script {      
+                    checkout([$class: 'GitSCM', branches: [[name: 'CORTX-33620']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '6c2477fa-c4bc-463f-b926-4c343b86b8c5', url: 'https://github.com/vijwani-seagate/cortx-re/']]])          
+                }
+            }
+        }
+
+        stage ('Setup Metrics server & K8s dashboard') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Tag last_successful', script: '''
+                    pushd solutions/kubernetes/metrics-server/
+                        echo $hosts | tr ' ' '\n' > hosts
+                        cat hosts
+                        ./setup-metrics-server.sh
+                    popd
+                '''
+            }
+        }
+    }
+
+    post {
+        always {
+
+            script {
+
+                // Jenkins Summary
+                clusterStatus = ""
+                if ( fileExists('/var/tmp/cluster-status.txt') && currentBuild.currentResult == "SUCCESS" ) {
+                    clusterStatus = readFile(file: '/var/tmp/cluster-status.txt')
+                    MESSAGE = "Cluster Setup Success for the build ${build_id}"
+                    ICON = "accept.gif"
+                    STATUS = "SUCCESS"
+                } else if ( currentBuild.currentResult == "FAILURE" ) {
+                    manager.buildFailure()
+                    MESSAGE = "Cluster Setup Failed for the build ${build_id}"
+                    ICON = "error.gif"
+                    STATUS = "FAILURE"
+                } else {
+                    manager.buildUnstable()
+                    MESSAGE = "Cluster Setup unstable for the build ${build_id}"
+                    ICON = "warning.gif"
+                    STATUS = "UNSTABLE"
+                }
+                
+                clusterStatusHTML = "<pre>${clusterStatus}</pre>"
+
+                manager.createSummary("${ICON}").appendText("<h3>K8 Cluster Setup ${currentBuild.currentResult} </h3><p>Please check <a href=\"${BUILD_URL}/console\">cluster setup logs</a> for more info <h4>Cluster Status:</h4>${clusterStatusHTML}", false, false, false, "red")
+
+                // Email Notification
+                env.cluster_status = "${clusterStatusHTML}"
+
+                def toEmail = ""
+                def recipientProvidersClass = [[$class: 'RequesterRecipientProvider']]
+                if ( manager.build.result.toString() == "FAILURE" ) {
+                    toEmail = "CORTX.DevOps.RE@seagate.com"
+                    recipientProvidersClass = [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
+                }
+                emailext ( 
+                    body: '''${SCRIPT, template="cluster-setup-email.template"}''',
+                    mimeType: 'text/html',
+                    subject: "[Jenkins Build ${currentBuild.currentResult}] : ${env.JOB_NAME}",
+                    attachLog: true,
+                    to: toEmail,
+                    recipientProviders: recipientProvidersClass
+                )
+            }
+        }
+    }
+}
