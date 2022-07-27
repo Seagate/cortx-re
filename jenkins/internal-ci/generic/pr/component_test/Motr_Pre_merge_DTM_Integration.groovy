0c22f402ecf9b2674a185e4741b21e54b43daa7c
Abhirup Ghosh <abhirup.ghosh@seagate.com>
2022-07-27 10:28:15 +0530

    made changes to MOTR_REMOTE_REPO
    Signed-off-by: Abhirup Ghosh <abhirup.ghosh@seagate.com>

--- jenkins/internal-ci/generic/pr/component_test/Motr_Pre_merge_DTM_Integration.groovy
+++ jenkins/internal-ci/generic/pr/component_test/Motr_Pre_merge_DTM_Integration.groovy
@@ -17,7 +17,7 @@ pipeline {
     }
     environment {
         // Motr Repo Info
-        GPR_REPO = "https://github.com/${ghprbAuthorRepoGitUrl}"
+        GPR_REPO = "https://github.com/${ghprbGhRepository}"
         MOTR_REMOTE_REPO = "${ghprbAuthorRepoGitUrl != null ? GPR_REPO : MOTR_REMOTE_REPO}"
         MOTR_BRANCH = "${ghprbSourceBranch != null ? ghprbSourceBranch : MOTR_BRANCH}"
         MOTR_GPR_REFSEPEC = "+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*"
