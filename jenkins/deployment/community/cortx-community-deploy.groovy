ca61431c8b2a70297c989a7faee3417d2a25bd3b
Nitish Singh <nitish.singh@seagate.com>
2022-07-28 13:43:23 +0530

    CORTX-33719: Modification to Configurable AWS EBS volumes. (#1152)
    * Update count & size.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>
    
    * Update ebs volutme attach count method slice.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>
    
    * Update ebs attach count.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>
    
    Update ebs atrtach count.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>
    
    Update ebs atrtach count.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>
    
    Update ebs atrtach count.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>
    
    Update ebs atrtach count.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>
    
    * Update EBS volume attach logic.
    
    Signed-off-by: Nitish Singh <nitish.singh@seagate.com>

--- jenkins/deployment/community/cortx-community-deploy.groovy
+++ jenkins/deployment/community/cortx-community-deploy.groovy
@@ -23,8 +23,9 @@ pipeline {
         string(name: 'OS_VERSION', defaultValue: 'CentOS 7.9.2009 x86_64', description: 'Operating system version', trim: true)
         string(name: 'REGION', defaultValue: 'ap-south-1', description: 'AWS region', trim: true)
         string(name: 'KEY_NAME', defaultValue: 'automation-key', description: 'Key name', trim: true)
+        string(name: 'EBS_VOLUME_COUNT', defaultValue: '9', description: 'EBS volumes to attach onto nodes. Currently maximum 25 disks are supported', trim: true)
+        string(name: 'EBS_VOLUME_SIZE', defaultValue: '10', description: 'EBS volume size in GB to attach onto nodes', trim: true)
         string(name: 'COMMUNITY_USE', defaultValue: 'yes', description: 'Only use during community deployment', trim: true)
-        
         // Please configure ROOT_PASSWORD, ACCESS_KEY and SECRET_KEY parameters in Jenkins job configuration.
     }
 
@@ -39,56 +40,58 @@ pipeline {
             }
         }
 
-        stage ('Install tools') {
+        stage ('Install Tools') {
             steps {
                 script { build_stage = env.STAGE_NAME }
-                sh label: 'install tools', script: '''
+                sh label: 'Install tools', script: '''
                 VM_IP=$(curl ipinfo.io/ip)
                 export OS_VERSION=${OS_VERSION}
                 export REGION=${REGION}
                 export SECRET_KEY=${SECRET_KEY}
                 export ACCESS_KEY=${ACCESS_KEY}
                 export KEY_NAME=${KEY_NAME}
+                export EBS_VOLUME_COUNT=${EBS_VOLUME_COUNT}
+                export EBS_VOLUME_SIZE=${EBS_VOLUME_SIZE}
+
                 rm -rvf /usr/local/bin/aws /usr/local/bin/aws_completer /usr/local/aws-cli >/dev/null 2>&1
                 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && yum install unzip -y && unzip awscliv2.zip
                 ./aws/install
                 aws configure set default.region $REGION; aws configure set aws_access_key_id $ACCESS_KEY; aws configure set aws_secret_access_key $SECRET_KEY
                 pushd solutions/community-deploy/cloud/AWS
                     ./tool_setup.sh
-                    sed -i 's,os_version          =.*,os_version          = "'"$OS_VERSION"'",g' user.tfvars && sed -i 's,region              =.*,region              = "'"$REGION"'",g' user.tfvars && sed -i 's,security_group_cidr =.*,security_group_cidr = "'"$VM_IP/32"'",g' user.tfvars
-                    echo key_name            = '"'$KEY_NAME'"' | cat >>user.tfvars
-                    cat user.tfvars | tail -4
+                    sed -i 's,os_version =.*,os_version = "'"$OS_VERSION"'",g' user.tfvars && sed -i 's,region =.*,region = "'"$REGION"'",g' user.tfvars && sed -i 's,security_group_cidr =.*,security_group_cidr = "'"$VM_IP/32"'",g' user.tfvars && sed -i 's,ebs_volume_count =.*,ebs_volume_count = "'"$EBS_VOLUME_COUNT"'",g' user.tfvars && sed -i 's,ebs_volume_size =.*,ebs_volume_size = "'"$EBS_VOLUME_SIZE"'",g' user.tfvars
+                    echo key_name = '"'$KEY_NAME'"' | cat >>user.tfvars
+                    cat user.tfvars | tail -6
                 popd
                 '''
             }
-        }
-
-        stage ('Create EC2 instace') {
-            steps {
-                script { build_stage = env.STAGE_NAME }
-                sh label: 'Setting up EC2 instance', script: '''
-                    pushd solutions/community-deploy/cloud/AWS
-                        terraform validate && terraform apply -var-file user.tfvars --auto-approve
-                    popd
-            '''
             }
-        }
 
-        stage ('Network and storage configuration') {
-            steps {
-                script { build_stage = env.STAGE_NAME }
-                script {
-                    env.AWS_IP = sh( script: '''
-                    cd solutions/community-deploy/cloud/AWS && terraform show -json terraform.tfstate | jq .values.outputs.cortx_deploy_ip_addr.value | tr -d '"'
-                    ''', returnStdout: true).trim()
+        stage ('Create EC2 Instance') {
+                steps {
+                    script { build_stage = env.STAGE_NAME }
+                    sh label: 'Setting up EC2 instance', script: '''
+                        pushd solutions/community-deploy/cloud/AWS
+                            terraform init && terraform validate
+                            terraform plan -var-file user.tfvars
+                            terraform apply -var-file user.tfvars --auto-approve
+                        popd
+                '''
                 }
+        }
+
+        stage ('Network and Storage Configuration') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
                 catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-                sh label: 'Setting up Network and Storage devices for CORTX. Script will reboot the instance on completion', script: '''
-                pushd solutions/community-deploy/cloud/AWS
-                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${AWS_IP}" sudo bash /home/centos/setup.sh
-                    sleep 240
-                popd
-                '''
+                    sh label: 'Setting up Network and Storage devices for CORTX. Script will reboot the instance on completion', script: '''
+                        pushd solutions/community-deploy/cloud/AWS
+                            AWS_IP=$(terraform show -json terraform.tfstate | jq .values.outputs.cortx_deploy_ip_addr.value 2>&1 | tee ip.txt)
+                            IP=$(cat ip.txt | tr -d '""')
+                            ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@${IP} sudo bash /home/centos/setup.sh
+                            sleep 240
+                        popd
+                    '''
                 }
             }
         }
