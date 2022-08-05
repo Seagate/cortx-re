1cb13e325011fe6bf45b91479174f5421ab44d72
Mukul Malhotra <mukul.malhotra@seagate.com>
2022-08-05 18:00:01 +0530

    Delete output.tf
    Signed-off-by: Mukul Malhotra <mukul.malhotra@seagate.com>

--- solutions/community-deploy/cloud/AWS/output.tf
+++
@@ -1,9 +0,0 @@
-output "aws_instance_public_ip_addr" {
-  value       = aws_instance.cortx_deploy.*.public_ip
-  description = "Public IP to connect CORTX server"
-  }
-
-output "aws_instance_private_ip_addr" {
-  value       = aws_instance.cortx_deploy.*.private_ip
-  description = "Private IP to connect to EC2 Instances"
-}
