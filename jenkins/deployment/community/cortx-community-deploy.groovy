a1a492e37ac5addcdebd7cd9c7b913b27c7e6805
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-08-10 00:46:21 +0530

    Restore email address
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/deployment/community/cortx-community-deploy.groovy
+++ jenkins/deployment/community/cortx-community-deploy.groovy
@@ -205,7 +205,7 @@ pipeline {
             def toEmail = ""
             def recipientProvidersClass = [[$class: 'DevelopersRecipientProvider']]
             if ( manager.build.result.toString() == "FAILURE" ) {
-                toEmail = "shailesh.vaidya@seagate.com"
+                toEmail = "CORTX.DevOps.RE@seagate.com"
                 recipientProvidersClass = [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
             }
             emailext ( 
