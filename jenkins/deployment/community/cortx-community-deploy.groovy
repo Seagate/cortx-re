470b6dda3efec066820b20b052638cbc4bf5da14
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-08-09 20:14:31 +0530

    Fix issue with fetching public ip
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/deployment/community/cortx-community-deploy.groovy
+++ jenkins/deployment/community/cortx-community-deploy.groovy
@@ -81,16 +81,13 @@ pipeline {
                 script { build_stage = env.STAGE_NAME }
                 script {
                     env.AWS_IP = sh( script: '''
-                    pushd solutions/community-deploy/cloud/AWS
-                        terraform show -json terraform.tfstate | jq .values.outputs.cortx_deploy_ip_addr.value | tr -d '""'
-                    popd
+                    cd solutions/community-deploy/cloud/AWS && terraform show -json terraform.tfstate | jq .values.outputs.cortx_deploy_ip_addr.value | tr -d '""'
                     ''', returnStdout: true).trim()
                 }
                 catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
                 sh label: 'Setting up Network and Storage devices for CORTX. Script will reboot the instance on completion', script: '''
                 pushd solutions/community-deploy/cloud/AWS
-                    echo "${env.AWS_IP}"
-                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${env.AWS_IP}" sudo bash /home/centos/setup.sh
+                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${AWS_IP}" sudo bash /home/centos/setup.sh
                     sleep 240
                 popd
                 '''
@@ -105,7 +102,7 @@ pipeline {
                 pushd solutions/community-deploy/cloud/AWS
                     export CORTX_RE_BRANCH=${CORTX_RE_BRANCH}
                     export CORTX_TAG=${CORTX_TAG}                
-                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${env.AWS_IP}" "export CORTX_RE_BRANCH=$CORTX_RE_BRANCH; git clone $CORTX_RE_REPO -b $CORTX_RE_BRANCH; pushd /home/centos/cortx-re/solutions/community-deploy; time sudo ./build-cortx.sh -b ${CORTX_TAG}"
+                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${AWS_IP}" "export CORTX_RE_BRANCH=$CORTX_RE_BRANCH; git clone $CORTX_RE_REPO -b $CORTX_RE_BRANCH; pushd /home/centos/cortx-re/solutions/community-deploy; time sudo ./build-cortx.sh -b ${CORTX_TAG}"
                 popd
             '''
             }
@@ -117,7 +114,7 @@ pipeline {
                 sh label: 'Changing root password and creating hosts file', script: '''
                 pushd solutions/community-deploy/cloud/AWS
                     export ROOT_PASSWORD=${ROOT_PASSWORD}              
-                    ssh -i cortx.pem -o StrictHostKeyChecking=no centos@"${env.AWS_IP}" "export ROOT_PASSWORD=$ROOT_PASSWORD && echo $ROOT_PASSWORD | sudo passwd --stdin root && pushd /home/centos/cortx-re/solutions/kubernetes && echo "'"hostname=$HOSTNAME,user=root,pass="'" > hosts && sed -i 's,pass=.*,pass=$ROOT_PASSWORD,g' hosts && cat hosts"
+                    ssh -i cortx.pem -o StrictHostKeyChecking=no centos@"${AWS_IP}" "export ROOT_PASSWORD=$ROOT_PASSWORD && echo $ROOT_PASSWORD | sudo passwd --stdin root && pushd /home/centos/cortx-re/solutions/kubernetes && echo "'"hostname=$HOSTNAME,user=root,pass="'" > hosts && sed -i 's,pass=.*,pass=$ROOT_PASSWORD,g' hosts && cat hosts"
                     popd
             '''
             }
@@ -128,7 +125,7 @@ pipeline {
                 script { build_stage = env.STAGE_NAME }
                 sh label: 'setting up K8s cluster on EC2', script: '''
                 pushd solutions/community-deploy/cloud/AWS                
-                    ssh -i cortx.pem -o StrictHostKeyChecking=no centos@"${env.AWS_IP}" "pushd /home/centos/cortx-re/solutions/kubernetes && sudo ./cluster-setup.sh true"
+                    ssh -i cortx.pem -o StrictHostKeyChecking=no centos@"${AWS_IP}" "pushd /home/centos/cortx-re/solutions/kubernetes && sudo ./cluster-setup.sh true"
                     popd
                 '''
             }
@@ -139,9 +136,9 @@ pipeline {
                 script { build_stage = env.STAGE_NAME }
                 sh label: 'Deploying 1N cortx cluster on EC2', script: '''
                 pushd solutions/community-deploy/cloud/AWS
-                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${env.AWS_IP}" 'sudo sed -i 's/cortx-docker.colo.seagate.com/'$HOSTNAME':8080/g' /etc/docker/daemon.json && sudo systemctl restart docker'
+                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${AWS_IP}" 'sudo sed -i 's/cortx-docker.colo.seagate.com/'$HOSTNAME':8080/g' /etc/docker/daemon.json && sudo systemctl restart docker'
                     sleep 240
-                    ssh -i cortx.pem -o StrictHostKeyChecking=no centos@"${env.AWS_IP}" 'pushd /home/centos/cortx-re/solutions/kubernetes && 
+                    ssh -i cortx.pem -o StrictHostKeyChecking=no centos@"${AWS_IP}" 'pushd /home/centos/cortx-re/solutions/kubernetes && 
                     export SOLUTION_CONFIG_TYPE='automated' && 
                     export COMMUNITY_USE='yes' && 
                     export CORTX_SERVER_IMAGE=$HOSTNAME:8080/seagate/cortx-rgw:2.0.0-0 && 
@@ -158,7 +155,7 @@ pipeline {
                 script { build_stage = env.STAGE_NAME }
                 sh label: 'IO Sanity on CORTX Cluster to validate bucket creation and object upload in deployed cluster', script: '''
                 pushd solutions/community-deploy/cloud/AWS
-                    ssh -i cortx.pem -o StrictHostKeyChecking=no centos@"${env.AWS_IP}" "pushd /home/centos/cortx-re/solutions/kubernetes && sudo ./cortx-deploy.sh --io-sanity"
+                    ssh -i cortx.pem -o StrictHostKeyChecking=no centos@"${AWS_IP}" "pushd /home/centos/cortx-re/solutions/kubernetes && sudo ./cortx-deploy.sh --io-sanity"
                 popd
             '''
             }
