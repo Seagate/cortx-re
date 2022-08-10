cb4809b221842b8b849fbbaa3ae813eac0b970e5
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-08-10 19:01:27 +0530

    chore: update github release script to avoid double quotes in pr titles
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- scripts/release_support/github-release.sh
+++ scripts/release_support/github-release.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
 
 PRE="true"
 REGISTRY="ghcr.io/seagate"
@@ -24,11 +42,12 @@ if [[ -z "$TAG" || -z "$SERVICES_VERSION" || -z "$CHANGESET_URL" ]]; then
         exit 1
 fi
 
-# Install required packages
+# Install required packages (Required only if gh api used for GitHub Release)
 # yum install -y https://github.com/cli/cli/releases/download/v2.14.3/gh_2.14.3_linux_amd64.rpm || { echo "ERROR: failed to install gh"; exit 1; } 
 # yum install jq -y || { echo "ERROR: failed to install jq"; exit 1; } 
+
 # set release content
-CHANGESET=$( curl -ks $CHANGESET_URL | tail -n +2 )
+CHANGESET=$( curl -ks $CHANGESET_URL | tail -n +2 | sed 's/"//g' )
 CORTX_SERVER_IMAGE="[$REGISTRY/cortx-rgw:$TAG](https://github.com/Seagate/cortx/pkgs/container/cortx-rgw)"
 CORTX_DATA_IMAGE="[$REGISTRY/cortx-data:$TAG](https://github.com/Seagate/cortx/pkgs/container/cortx-data)"
 CORTX_CONTROL_IMAGE="[$REGISTRY/cortx-control:$TAG](https://github.com/Seagate/cortx/pkgs/container/cortx-control)"
@@ -37,13 +56,13 @@ SERVICES_VERSION="[$SERVICES_VERSION](https://github.com/Seagate/cortx-k8s/relea
 MESSAGE="$CORTX_IMAGE_TITLE\n$IMAGES_INFO\n\n$SERVICES_VERSION_TITLE$SERVICES_VERSION\n\n$BUILD_INFO\n$DEPLOY_INFO\n\n$CHANGESET_TITLE\n\n${CHANGESET//$'\n'/\\n}"
 
 API_JSON=$(printf '{"tag_name":"%s","name":"%s","body":"%s","prerelease":%s}' "$TAG" "$TAG" "$MESSAGE" "$PRE" )
-# echo $API_JSON
-# if jq -n $API_JSON | gh api /repos/$RELEASE_REPO_OWNER/$RELEASE_REPO_NAME/releases --input - > /tmp/api_response.html
-# gh release create test-tag --repo gauravchaudhari02/cortx-re --notes-file CHANGESET.md -p
-if curl --data "$API_JSON" -sif -H "Accept: application/json" -H "Authorization: token $GH_TOKEN" "https://api.github.com/repos/$RELEASE_REPO_OWNER/$RELEASE_REPO_NAME/releases" -o api_response.html
+# Other ways to create GitHub Release using gh api
+# jq -n $API_JSON | gh api /repos/$RELEASE_REPO_OWNER/$RELEASE_REPO_NAME/releases --input - > api_response.html
+# gh release create <tag-name> --repo seagate/cortx --notes-file CHANGESET.md -p
+if curl --data "$API_JSON" -sif -H "Accept: application/json" -H "Authorization: token $GH_TOKEN" "https://api.github.com/repos/$RELEASE_REPO_OWNER/$RELEASE_REPO_NAME/releases" > api_response.html
 then
     echo "https://github.com/$RELEASE_REPO_OWNER/$RELEASE_REPO_NAME/releases/tag/$TAG"
 else
     echo "ERROR: curl command has failed. Please check API response for more details"
     exit 1
-fi      
+fi
