16af97f598ba7178bcc375f8f05e4c79a2cca48a
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-08-16 13:52:27 +0530

    chore: define array
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/release-process/github-release.groovy
+++ jenkins/release-process/github-release.groovy
@@ -31,7 +31,7 @@ pipeline {
                 script { build_stage = env.STAGE_NAME }
                 script {
                     withCredentials([string(credentialsId: 'gaurav-github-token', variable: 'GH_TOKEN')]) {
-                        github_release_info = sh( script: """
+                        def github_release_info [] = sh( script: """
                             bash scripts/release_support/create-cortx-github-release.sh -t $GIT_TAG -v $SERVICES_VERSION -c $CHANGESET_URL -r $RELEASE_REPO
                         """, returnStdout: true).trim()
                         env.github_release_url = github_release_info.split()[0]
