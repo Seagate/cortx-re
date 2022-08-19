def739b4ed949639dcb678da87a74bde5baca325
Mukul Malhotra <mukul.malhotra@seagate.com>
2022-08-19 11:12:02 +0530

    Update cortx-community-deploy.groovy
    Signed-off-by: Mukul Malhotra <mukul.malhotra@seagate.com>

--- jenkins/deployment/community/cortx-community-deploy.groovy
+++ jenkins/deployment/community/cortx-community-deploy.groovy
@@ -32,7 +32,6 @@ pipeline {
     }
 
         stages {
-
             stage('Checkout Script') {
                 steps {
                     cleanWs()
@@ -99,9 +98,9 @@ pipeline {
                 sh label: 'Changing root password & creating hosts file', script: '''
                 pushd solutions/community-deploy/cloud/AWS
                     export ROOT_PASSWORD=${ROOT_PASSWORD}
-                    EC2HOSTNAME=$(for ip in $PUBLIC_IP;do ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@$ip echo '$HOSTNAME';done | head -1 > ec2_hostname.txt)
+                    EC2HOSTNAME=$(for ip in $PUBLIC_IP;do ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@$ip echo '$HOSTNAME';done)
                     PUBLIC_IP=$(terraform show -json terraform.tfstate | jq .values.outputs.aws_instance_public_ip_addr.value 2>&1 | tee ip_public.txt | tr -d '",[]' | sed '/^$/d')
-                    for ip in $PUBLIC_IP;do ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@${ip} "export ROOT_PASSWORD=$ROOT_PASSWORD && echo $ROOT_PASSWORD | sudo passwd --stdin root && git clone https://github.com/mukul-seagate11/cortx-re-1; pushd /home/centos/cortx-re-1/solutions/kubernetes && touch hosts && echo "'"hostname=$EC2HOSTNAME,user=root,pass="'" > hosts && sed -i 's,pass=.*,pass=$ROOT_PASSWORD,g' hosts && cat hosts && sleep 120";done
+                    for ip in $PUBLIC_IP;do ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@${ip} "export ROOT_PASSWORD=$ROOT_PASSWORD && echo $ROOT_PASSWORD | sudo passwd --stdin root && git clone https://github.com/mukul-seagate11/cortx-re-1; pushd /home/centos/cortx-re-1/solutions/kubernetes && touch hosts && echo "'"hostname=$EC2HOSTNAME,user=root,pass="'" > hosts && sed -i 's,pass=.*,pass=$ROOT_PASSWORD,g' hosts && cat hosts";done
                 popd
             '''
             }
