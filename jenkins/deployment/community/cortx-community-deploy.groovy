4404e33a1a3b8d0821f6303a856c63010e510869
Mukul Malhotra <mukul.malhotra@seagate.com>
2022-08-21 17:41:41 +0530

    Update cortx-community-deploy.groovy
    Signed-off-by: Mukul Malhotra <mukul.malhotra@seagate.com>

--- jenkins/deployment/community/cortx-community-deploy.groovy
+++ jenkins/deployment/community/cortx-community-deploy.groovy
@@ -135,8 +135,8 @@ pipeline {
                     export CORTX_DATA_IMAGE="cortx-data:2.0.0-0"
                     export CORTX_CONTROL_IMAGE="cortx-control:2.0.0-0"
                     export CORTX_ALL_IMAGE="cortx-all:2.0.0-0"
-                    ssh -i cortx.pem -o StrictHostKeyChecking=no centos@${PRIMARY_PUBLIC_IP} sudo -- sh -c "pushd /home/centos/cortx-re-1/solutions/kubernetes && ./cluster-setup.sh true && sed -i 's,cortx-docker.colo.seagate.com,${HOST1},g' /etc/docker/daemon.json && systemctl restart docker && sleep 5"
-                    for wp in $WORKER_IP;do ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@${wp} sudo -- sh -c "sed -i 's,cortx-docker.colo.seagate.com,${HOST1}:8080,g' /etc/docker/daemon.json && systemctl restart docker && sleep 5 && docker pull ${HOST1}:8080/seagate/${CORTX_SERVER_IMAGE} && docker pull ${HOST1}:8080/seagate/${CORTX_DATA_IMAGE} && docker pull ${HOST1}:8080/seagate/${CORTX_CONTROL_IMAGE} && docker pull ${HOST1}:8080/seagate/${CORTX_ALL_IMAGE}";done
+                    ssh -i cortx.pem -o StrictHostKeyChecking=no centos@${PRIMARY_PUBLIC_IP} "sudo -- sh -c 'pushd /home/centos/cortx-re-1/solutions/kubernetes && ./cluster-setup.sh true && sed -i 's,cortx-docker.colo.seagate.com,${HOST1},g' /etc/docker/daemon.json && systemctl restart docker && sleep 5'"
+                    for wp in $WORKER_IP;do ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@${wp} "sudo -- sh -c 'sed -i 's,cortx-docker.colo.seagate.com,${HOST1}:8080,g' /etc/docker/daemon.json && systemctl restart docker && sleep 5 && docker pull ${HOST1}:8080/seagate/${CORTX_SERVER_IMAGE} && docker pull ${HOST1}:8080/seagate/${CORTX_DATA_IMAGE} && docker pull ${HOST1}:8080/seagate/${CORTX_CONTROL_IMAGE} && docker pull ${HOST1}:8080/seagate/${CORTX_ALL_IMAGE}'";done
                     popd
             '''
             }
