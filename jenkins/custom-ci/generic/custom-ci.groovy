3b56852f8d0a4e90fe7333662b9f4a29b04d93a9
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-08-30 20:47:31 +0530

    chore: fix motr custom-ci job link
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/custom-ci/generic/custom-ci.groovy
+++ jenkins/custom-ci/generic/custom-ci.groovy
@@ -175,7 +175,7 @@ pipeline {
                         script { build_stage = env.STAGE_NAME }
                         script {
                             try {
-                                def motrbuild = build job: 'Release_Engineering/re-workspace/motr-custom-build/', wait: true,
+                                def motrbuild = build job: 'GitHub-custom-ci-builds/generic/motr-custom-build/', wait: true,
                                         parameters: [
                                                         string(name: 'MOTR_URL', value: "${MOTR_URL}"),
                                                         string(name: 'MOTR_BRANCH', value: "${MOTR_BRANCH}"),
