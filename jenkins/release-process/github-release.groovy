68edf2e6b98cc85588103ebb89a5b3691a809358
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-09-14 14:22:50 +0530

    chore: update command
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/release-process/github-release.groovy
+++ jenkins/release-process/github-release.groovy
@@ -32,8 +32,7 @@ pipeline {
                 script {
                     withCredentials([string(credentialsId: 'cortx-admin-token', variable: 'GH_TOKEN')]) {
                         github_release_info = sh( script: '''
-                            set -x
-                            ./scripts/release_support/create-cortx-github-release.sh -t "\${GIT_TAG}" -v "\${SERVICES_VERSION}" -c "\${CHANGESET_URL}" -r "\${RELEASE_REPO}"
+                            bash -x scripts/release_support/create-cortx-github-release.sh -t "\${GIT_TAG}" -v "\${SERVICES_VERSION}" -c "\${CHANGESET_URL}" -r "\${RELEASE_REPO}"
                         ''', returnStdout: true).trim()
                         env.github_release_url = github_release_info.split()[0]
                         env.tags = github_release_info.split()[1]
