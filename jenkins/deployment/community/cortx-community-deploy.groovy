a1de14a0d56dbf58bd6035f8600e6ae83c9a87d8
Mukul Malhotra <mukul.malhotra@seagate.com>
2022-09-17 13:23:21 +0530

    Update cortx-community-deploy.groovy
    Signed-off-by: Mukul Malhotra <mukul.malhotra@seagate.com>

--- jenkins/deployment/community/cortx-community-deploy.groovy
+++ jenkins/deployment/community/cortx-community-deploy.groovy
@@ -9,7 +9,7 @@ pipeline {
         timeout(time: 360, unit: 'MINUTES')
         timestamps()
         disableConcurrentBuilds()
-        buildDiscarder(logRotator(daysToKeepStr: '30', numToKeepStr: '5'))
+        buildDiscarder(logRotator(daysToKeepStr: '30', numToKeepStr: '10'))
         ansiColor('xterm')
     }
 
@@ -100,15 +100,125 @@ pipeline {
                 }
             }
         }
-        post {
-            always {
-                retry(count: 3) {
+        stage('EC2 connection prerequisites') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Changing root password & creating hosts file', script: '''
+                pushd solutions/community-deploy/cloud/AWS
+                export ROOT_PASSWORD=${ROOT_PASSWORD}
+                export PRIMARY_PUBLIC_IP=$(cat ip_public.txt | jq '.[0]'| tr -d '",[]')
+                export PUBLIC_IP=$(terraform show -json terraform.tfstate | jq .values.outputs.aws_instance_public_ip_addr.value 2>&1 | tee ip_public.txt | tr -d '",[]' | sed '/^$/d')
+                terraform show -json terraform.tfstate | jq .values.outputs.aws_instance_private_dns.value 2>&1 | tee ec2_hostname.txt
+                export BUILD_NODE=$(cat ec2_hostname.txt | jq '.[0]'| tr -d '",[]')
+                export HOST2=$(cat ec2_hostname.txt | jq '.[1]'| tr -d '",[]')
+                export HOST3=$(cat ec2_hostname.txt | jq '.[2]'| tr -d '",[]')
+                export HOST4=$(cat ec2_hostname.txt | jq '.[3]'| tr -d '",[]')
+                if [ "$INSTANCE_COUNT" -eq 1 ]; then
+                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${PRIMARY_PUBLIC_IP}" "sudo -- sh -c 'export ROOT_PASSWORD=$ROOT_PASSWORD && echo $ROOT_PASSWORD | sudo passwd --stdin root && git clone https://github.com/Seagate/cortx-re && pushd /home/centos/cortx-re/solutions/kubernetes && touch hosts && echo hostname=${BUILD_NODE},user=root,pass= > hosts && sed -i 's,pass=.*,pass=$ROOT_PASSWORD,g' hosts && cat hosts && sleep 240'"
+                elif [ "$INSTANCE_COUNT" -gt 1 ]; then
+                    for ip in $PUBLIC_IP;do ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${ip}" "sudo -- sh -c 'export ROOT_PASSWORD=$ROOT_PASSWORD && echo $ROOT_PASSWORD | sudo passwd --stdin root && git clone https://github.com/Seagate/cortx-re && pushd /home/centos/cortx-re/solutions/kubernetes && touch hosts && echo hostname=${BUILD_NODE},user=root,pass= > hosts && sed -i 's,pass=.*,pass=$ROOT_PASSWORD,g' hosts && echo hostname=${HOST2},user=root,pass= >> hosts && sed -i 's,pass=.*,pass=$ROOT_PASSWORD,g' hosts && echo hostname=${HOST3},user=root,pass= >> hosts && sed -i 's,pass=.*,pass=$ROOT_PASSWORD,g' hosts && echo hostname=${HOST4},user=root,pass= >> hosts && sed -i 's,pass=.*,pass=$ROOT_PASSWORD,g' hosts && cat hosts && sleep 240'";done
+                    popd
+                fi
+            '''
+            }
+        }
+        stage('Execute cortx build script') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Executing cortx build image script on Primary node', script: '''
+                pushd solutions/community-deploy/cloud/AWS
+                    export PRIMARY_PUBLIC_IP=$(cat ip_public.txt | jq '.[0]'| tr -d '",[]')
+                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${PRIMARY_PUBLIC_IP}" "sudo -- sh -c 'pushd /home/centos/cortx-re/solutions/community-deploy && systemctl start docker && sleep 120 && time ./build-cortx.sh'"
+                popd
+            '''
+            }
+        }
+        stage('Setup K8s cluster') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Setting up K8s cluster on EC2', script: '''
+                    pushd solutions/community-deploy/cloud/AWS
+                    export BUILD_NODE=$(cat ec2_hostname.txt | jq '.[0]'| tr -d '",[]')
+                    export PRIMARY_PUBLIC_IP=$(cat ip_public.txt | jq '.[0]'| tr -d '",[]')
+                    export WORKER_IP=$(cat ip_public.txt | jq '.[0]','.[1]','.[2]','.[3]' | tr -d '",[]')
+                    if [ "$INSTANCE_COUNT" -eq 1 ]; then
+                        ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${PRIMARY_PUBLIC_IP}" "sudo -- sh -c 'pushd /home/centos/cortx-re/solutions/kubernetes && ./cluster-setup.sh true && sed -i 's,cortx-docker.colo.seagate.com,${BUILD_NODE}:8080,g' /etc/docker/daemon.json && systemctl restart docker && sleep 240'"
+                        popd
+                    elif [ "$INSTANCE_COUNT" -gt 1 ]; then
+                        ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${PRIMARY_PUBLIC_IP}" "sudo -- sh -c 'pushd /home/centos/cortx-re/solutions/kubernetes && ./cluster-setup.sh true && sed -i 's,cortx-docker.colo.seagate.com,${BUILD_NODE}:8080,g' /etc/docker/daemon.json && systemctl restart docker && sleep 240'"
+                        for wp in $WORKER_IP;do ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${wp}" "sudo -- sh -c 'sed -i 's,cortx-docker.colo.seagate.com,${BUILD_NODE}:8080,g' /etc/docker/daemon.json && systemctl restart docker && sleep 120'";done
+                        popd
+                    fi
+            '''
+            }
+        }
+        stage('Pull locally generated images') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Generate locally build cortx images on worker nodes', script: '''
+                export WORKER_IP=$(cat ip_public.txt | jq '.[0]','.[1]','.[2]','.[3]' | tr -d '",[]')
+                export BUILD_NODE=$(cat ec2_hostname.txt | jq '.[0]'| tr -d '",[]')
+                export CORTX_SERVER_IMAGE="${BUILD_NODE}:8080/seagate/cortx-rgw:2.0.0-0"
+                export CORTX_DATA_IMAGE="${BUILD_NODE}:8080/seagate/cortx-data:2.0.0-0"
+                export CORTX_CONTROL_IMAGE="${BUILD_NODE}:8080/seagate/cortx-control:2.0.0-0"
+                pushd solutions/community-deploy/cloud/AWS
+                if [ "$INSTANCE_COUNT" -gt 1 ]; then
+                    for wp in $WORKER_IP;do ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@${wp} "sudo docker pull '${CORTX_SERVER_IMAGE}' && sudo docker pull '${CORTX_DATA_IMAGE}' && sudo docker pull '${CORTX_CONTROL_IMAGE}'";done
+                    popd
+                else
+                    echo -n ".... Deployment for 1N continues ...."
+                fi
+            '''
+            }
+        }
+        stage('Deploy CORTX cluster') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Deploying multi-node cortx cluster by locally generating images', script: '''
+                pushd solutions/community-deploy/cloud/AWS
+                    export PRIMARY_PUBLIC_IP=$(cat ip_public.txt | jq '.[0]'| tr -d '",[]')
+                    export BUILD_NODE=$(cat ec2_hostname.txt | jq '.[0]'| tr -d '",[]')
+                    export SOLUTION_CONFIG_TYPE='automated'
+                    export COMMUNITY_USE='yes'
+                    export CORTX_SERVER_IMAGE="${BUILD_NODE}:8080/seagate/cortx-rgw:2.0.0-0"
+                    export CORTX_DATA_IMAGE="${BUILD_NODE}:8080/seagate/cortx-data:2.0.0-0"
+                    export CORTX_CONTROL_IMAGE="${BUILD_NODE}:8080/seagate/cortx-control:2.0.0-0"
+                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${PRIMARY_PUBLIC_IP}" 'pushd /home/centos/cortx-re/solutions/kubernetes && export CORTX_SERVER_IMAGE='${CORTX_SERVER_IMAGE}' && export CORTX_DATA_IMAGE='${CORTX_DATA_IMAGE}' && export CORTX_CONTROL_IMAGE='${CORTX_CONTROL_IMAGE}' && sudo env SOLUTION_CONFIG_TYPE='${SOLUTION_CONFIG_TYPE}' env CORTX_SERVER_IMAGE='${CORTX_SERVER_IMAGE}' env CORTX_CONTROL_IMAGE='${CORTX_CONTROL_IMAGE}' env CORTX_DATA_IMAGE='${CORTX_DATA_IMAGE}' env COMMUNITY_USE='${COMMUNITY_USE}' ./cortx-deploy.sh --cortx-cluster'
+                    popd
+            '''
+            }
+        }
+        stage('Basic I/O Test') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'IO Sanity on CORTX Cluster to validate bucket creation and object upload in deployed cluster', script: '''
+                pushd solutions/community-deploy/cloud/AWS
+                    export PRIMARY_PUBLIC_IP=$(cat ip_public.txt | jq '.[0]'| tr -d '",[]')
+                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${PRIMARY_PUBLIC_IP}" 'pushd /home/centos/cortx-re/solutions/kubernetes && sudo ./cortx-deploy.sh --io-sanity'
+                    popd
+            '''
+            }
+        }
+        stage('Basic Management Path Check') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Basic Management Path Check', script: '''
+                pushd solutions/community-deploy/cloud/AWS
+                    export PRIMARY_PUBLIC_IP=$(cat ip_public.txt | jq '.[0]'| tr -d '",[]')
+                    ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${PRIMARY_PUBLIC_IP}" 'pushd /home/centos/cortx-re/solutions/kubernetes && sudo ./cortx-deploy.sh --mangement-health-check'
+                    popd
+                '''
+            }
+        }
+        }
+    post {
+        always {
+            retry(count: 3) {
                     sh label: 'Destroying EC2 instance', script: '''
                     pushd solutions/community-deploy/cloud/AWS
                         terraform validate && terraform destroy -var-file user.tfvars --auto-approve
                         popd
                     '''
-                }
             }
         }
     }
