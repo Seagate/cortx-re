b31b8c6bd9f10c6bc5581b3645d43e6dc30b553e
Mukul Malhotra <mukul.malhotra@seagate.com>
2022-09-17 15:59:00 +0530

    Update setup.sh
    Signed-off-by: Mukul Malhotra <mukul.malhotra@seagate.com>

--- solutions/community-deploy/cloud/AWS/setup.sh
+++ solutions/community-deploy/cloud/AWS/setup.sh
@@ -24,6 +24,10 @@ if [ $(whoami) != root ];then
 	exit
 fi
 
+#Install packages & start service
+yum -y remove docker* && yum install -y yum-utils git firewalld epel-release && yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y && yum install -y jq docker-ce docker-ce-cli containerd.io docker-compose-plugin
+systemctl start docker && systemctl enable docker
+
 #Setup udev rules for ESB volumes. Refer- https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-types.html#ec2-nitro-instances 
 echo "Mapping Nitro EBS volumes"
 declare -a device_list
