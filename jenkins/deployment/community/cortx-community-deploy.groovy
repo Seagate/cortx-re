52844f786abcc82272de8990b7a3aedc92416d85
Mukul Malhotra <mukul.malhotra@seagate.com>
2022-09-18 16:51:37 +0530

    Update cortx-community-deploy.groovy
    Signed-off-by: Mukul Malhotra <mukul.malhotra@seagate.com>

--- jenkins/deployment/community/cortx-community-deploy.groovy
+++ jenkins/deployment/community/cortx-community-deploy.groovy
@@ -89,7 +89,7 @@ pipeline {
                     if [ "$INSTANCE_COUNT" -eq 1 ]; then
                         echo -n "Setting up Network and Storage devices on 1N Deployment  ........."
                         ssh -i cortx.pem -o 'StrictHostKeyChecking=no' centos@"${PRIMARY_PUBLIC_IP}" sudo bash /home/centos/setup.sh
-                        sleep 640
+                        sleep 860
 
                     elif [ "$INSTANCE_COUNT" -gt 1 ]; then
                         echo -n "Setting up Network and Storage devices for multinode Deployment  ........."
