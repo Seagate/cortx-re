e2f417634e9cce9e6f4b86aad53214b09c278c06
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-09-21 11:16:56 +0530

    CORTX-34001: Fix GitHub release process (#1230)
    * CORTX-34001: Fix GitHub release process
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- jenkins/release-process/github-release.groovy
+++ jenkins/release-process/github-release.groovy
@@ -31,9 +31,9 @@ pipeline {
                 script { build_stage = env.STAGE_NAME }
                 script {
                     withCredentials([string(credentialsId: 'cortx-admin-token', variable: 'GH_TOKEN')]) {
-                        github_release_info = sh( script: """
-                            bash -x scripts/release_support/create-cortx-github-release.sh -t $GIT_TAG -v $SERVICES_VERSION -c $CHANGESET_URL -r $RELEASE_REPO
-                        """, returnStdout: true).trim()
+                        github_release_info = sh( script: '''
+                            bash -x scripts/release_support/create-cortx-github-release.sh -t "\${GIT_TAG}" -v "\${SERVICES_VERSION}" -c "\${CHANGESET_URL}" -r "\${RELEASE_REPO}"
+                        ''', returnStdout: true).trim()
                         env.github_release_url = github_release_info.split()[0]
                         env.tags = github_release_info.split()[1]
                     }			
