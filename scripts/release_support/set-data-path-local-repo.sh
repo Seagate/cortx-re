9c794803e54fab9c5beab35c0d5f5507b1cdc4d5
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-10-03 19:30:26 +0530

    Update set-data-path-local-repo.sh
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/set-data-path-local-repo.sh
+++ scripts/release_support/set-data-path-local-repo.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,40 +17,30 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
 #!/bin/bash
+set -euf -o pipefail
+
 REPO=${1:-seagate/cortx-motr}
-RELEASE=${2}
-
+REPO_LATEST_RELEASE=$(curl -s https://api.github.com/repos/"$REPO"/releases/latest  | jq ".. .tag_name? // empty" | tr -d '"')
+RELEASE=${2:-$REPO_LATEST_RELEASE}
 RELEASE_REPO_LOCATION="/var/tmp/$RELEASE"
-RELEASE_REPO_FILE="$RELEASE.repo"
+RELEASE_REPO_FILE="/etc/yum.repos.d/$(echo "$RELEASE_REPO_LOCATION" | sed -e 's/\///' -e 's/\//_/g').repo"
 
+#Install githubrelease package
+rpm -qa python3-pip || yum install python3-pip -y
+pip3 show githubrelease || (pip3 install click==7.1.2 && pip3 install githubrelease==1.5.8)
 
-function _create_local_repo () {
-        REPO_LOCATION=$1
-        DOWNLOAD_RELEASE=$2
-        rm -rf $REPO_LOCATION
-        mkdir $REPO_LOCATION && pushd $REPO_LOCATION
-        githubrelease asset $REPO download $DOWNLOAD_RELEASE && /bin/createrepo -v . || exit
-        popd
-}
+#Download packages from GitHub Release
+echo -e "\n################################################################################"
+echo -e "REPO=$REPO"
+echo -e "RELEASE=$RELEASE"
+echo -e "################################################################################\n"
+rm -rf "$RELEASE_REPO_LOCATION" && mkdir "$RELEASE_REPO_LOCATION" || exit
+pushd "$RELEASE_REPO_LOCATION" || exit
+        githubrelease asset "$REPO" download "$RELEASE" && /bin/createrepo -v . || exit
+popd || exit
 
-function _create_local_repo_file () {
-rm -rf $RELEASE_REPO_FILE
-cat << EOF >> $RELEASE_REPO_FILE
-[releases_$RELEASE]
-name=Cortx $RELEASE  Repository
-baseurl=file://$RELEASE_REPO_LOCATION
-gpgcheck=0
-enabled=1
-EOF
-}
-
-function _setup_local_repo () {
-cp -f $RELEASE_REPO_FILE /etc/yum.repos.d/
+#Setup yum repository
+rm -f $RELEASE_REPO_FILE
+yum-config-manager --add-repo file://"$RELEASE_REPO_LOCATION"
+echo "gpgcheck=0" >> "$RELEASE_REPO_FILE"
 yum clean all; rm -rf /var/cache/yum
-}
-
-
-_create_local_repo $RELEASE_REPO_LOCATION $DOWNLOAD_RELEASE
-#_create_local_repo $UPLOADS_REPO_LOCATION $UPLOADS
-_create_local_repo_file
-_setup_local_repo
