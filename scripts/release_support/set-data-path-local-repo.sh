5ddcc9ca9c782ec41bb19c02d2d925ffcd07fa7b
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-10-03 20:05:35 +0530

    Update set-data-path-local-repo.sh
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- scripts/release_support/set-data-path-local-repo.sh
+++ scripts/release_support/set-data-path-local-repo.sh
@@ -19,20 +19,24 @@
 #!/bin/bash
 set -euf -o pipefail
 
+#Install githubrelease package
+rpm -q jq || (yum install epel-release -y && yum install jq -y)
+rpm -q python3-pip || yum install python3-pip -y
+rpm -q createrepo || yum install createrepo -y
+export LC_ALL=en_US.utf-8 && export LANG=en_US.utf-8
+pip3 show githubrelease || (pip3 install click==7.1.2 && pip3 install githubrelease==1.5.8)
+
+
 REPO=${1:-seagate/cortx-motr}
 REPO_LATEST_RELEASE=$(curl -s https://api.github.com/repos/"$REPO"/releases/latest  | jq ".. .tag_name? // empty" | tr -d '"')
 RELEASE=${2:-$REPO_LATEST_RELEASE}
 RELEASE_REPO_LOCATION="/var/tmp/$RELEASE"
 RELEASE_REPO_FILE="/etc/yum.repos.d/$(echo "$RELEASE_REPO_LOCATION" | sed -e 's/\///' -e 's/\//_/g').repo"
 
-#Install githubrelease package
-rpm -qa python3-pip || yum install python3-pip -y
-pip3 show githubrelease || (pip3 install click==7.1.2 && pip3 install githubrelease==1.5.8)
-
 #Download packages from GitHub Release
 echo -e "\n################################################################################"
-echo -e "REPO=$REPO"
-echo -e "RELEASE=$RELEASE"
+echo -e "### REPO=$REPO"
+echo -e "### RELEASE=$RELEASE"
 echo -e "################################################################################\n"
 rm -rf "$RELEASE_REPO_LOCATION" && mkdir "$RELEASE_REPO_LOCATION" || exit
 pushd "$RELEASE_REPO_LOCATION" || exit
