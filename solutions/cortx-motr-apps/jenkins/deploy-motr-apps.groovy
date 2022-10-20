3de5fdcf4b22daff995229543e423724bd3e009e
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-10-20 11:57:44 +0530

    Add repo setup steps
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- solutions/cortx-motr-apps/jenkins/deploy-motr-apps.groovy
+++ solutions/cortx-motr-apps/jenkins/deploy-motr-apps.groovy
@@ -16,28 +16,40 @@ pipeline {
         GITHUB_CRED = credentials('shailesh-github-token')
     }
 
-
     parameters {
-        string(name: 'CORTX_RE_BRANCH', defaultValue: 'main', description: 'Branch or GitHash for Cluster Setup scripts', trim: true)
-        string(name: 'CORTX_RE_REPO', defaultValue: 'https://github.com/Seagate/cortx-re/', description: 'Repository for Cluster Setup scripts', trim: true)    
+        string(name: 'CORTX_RE_BRANCH', defaultValue: 'main', description: 'Branch or GitHash for Setup scripts', trim: true)
+        string(name: 'CORTX_RE_REPO', defaultValue: 'https://github.com/Seagate/cortx-re/', description: 'Repository for Setup scripts', trim: true)
+        string(name: 'CORTX_MOTR_REPO', defaultValue: 'seagate/cortx-motr', description: 'Repository for Motr Release', trim: true)
+        string(name: 'CORTX_MOTR_RELEASE', defaultValue: 'latest', description: 'Motr Release', trim: true)
     }    
 
     stages {
 
         stage('Checkout Script') {
             steps { 
-                cleanWs()            
+                cleanWs()
                 script {
                     checkout([$class: 'GitSCM', branches: [[name: "${CORTX_RE_BRANCH}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'cortx-admin-github', url: "${CORTX_RE_REPO}"]]])                
                 }
             }
         }
 
-
-        stage ('Deploy Third Party Components') {
+        stage ('Setup CORTX Repository') {
             steps {
                 script { build_stage = env.STAGE_NAME }
-                sh label: 'Tag last_successful', script: '''
+                sh label: 'Setup CORTX rpm packages repository', script: '''
+                    pushd scripts/release_support
+                        ./set-local-repo.sh $CORTX_MOTR_REPO $CORTX_MOTR_RELEASE
+                    popd
+                '''
+            }
+        }
+
+
+        stage ('Deploy Motr Apps') {
+            steps {
+                script { build_stage = env.STAGE_NAME }
+                sh label: 'Deploy Motr Apps', script: '''
                     pushd solutions/cortx-motr-apps/scripts/
                         bash ./deploy-motr-apps.sh
                     popd
