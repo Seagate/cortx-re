045d035de05ad4be096ffec9f7ef09e5a1f04105
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-12-01 17:38:22 +0530

    chore: update terraform variables
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

--- scripts/vRealize/variable.tf
+++ scripts/vRealize/variable.tf
@@ -31,13 +31,32 @@ variable "vm_names" {
   type = list(string)
 }
 
-variable "vm_resources" {
-  type = map
-  default = {
-      "image" = "Rocky-8-Library-VMs"
-      "cpu" = 4
-      "memory" = 2048
-      "disk_size" = 10
-      "disk_count" = 4
-  }
+variable "catalog_item_version" {
+  type = number
+  default = 5
+}
+
+variable "vm_image" {
+  type = string
+  default = "Rocky-8-Library-VMs"
+}
+
+variable "vm_cpu" {
+  type = number
+  default = 4
+}
+
+variable "vm_memory" {
+  type = number
+  default = 2048
+}
+
+variable "vm_disk_size" {
+  type = number
+  default = 10
+}
+
+variable "vm_disk_count" {
+  type = number
+  default = 4
 }
