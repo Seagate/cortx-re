ac52996e897fcb28c08a88dfe97b9d1dd9adb81c
Gaurav Chaudhari <gaurav.chaudhari@seagate.com>
2022-12-13 18:27:55 +0530

    CORTX-34380: Automate VRA infrastructure provisioning (#1266)
    * CORTX-34380: Automate VRA infrastructure provisioning
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@seagate.com>

---
+++ solutions/vmware/terraform/variable.tf
@@ -0,0 +1,56 @@
+variable "owner_account" {
+  type = string
+  default = "EOS_SVC_RE1"
+}
+
+variable "vra_url" {
+  type = string
+}
+
+variable "vra_project" {
+  type = string
+  default = "SSC-CICD"
+}
+
+variable "vra_catalog_item" {
+  type = string
+  default = "ssc-cicd-rocky"
+}
+
+variable "vra_refresh_token" {
+  type = string
+}
+
+variable "catalog_item_version" {
+  type = number
+  default = 5
+}
+
+variable "vm_names" {
+  type = list(string)
+}
+
+variable "vm_image" {
+  type = string
+  default = "Rocky-8-Library-VMs"
+}
+
+variable "vm_cpu" {
+  type = number
+  default = 4
+}
+
+variable "vm_memory" {
+  type = number
+  default = 2048
+}
+
+variable "vm_disk_size" {
+  type = number
+  default = 10
+}
+
+variable "vm_disk_count" {
+  type = number
+  default = 4
+}
