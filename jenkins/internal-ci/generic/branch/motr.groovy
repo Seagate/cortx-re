41d7e589932c9f12f26d32b709b1e7fce8d04d62
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2023-02-03 16:29:21 +0530

    Updated email alias details
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/internal-ci/generic/branch/motr.groovy
+++ jenkins/internal-ci/generic/branch/motr.groovy
@@ -267,7 +267,7 @@ pipeline {
                 def toEmail = ""
                 def recipientProvidersClass = [[$class: 'DevelopersRecipientProvider']]
                 if ( manager.build.result.toString() == "FAILURE" ) {
-                    toEmail = "shailesh.vaidya@seagate.com"
+                    toEmail = "CORTX.DevOps.RE@seagate.com"
                     recipientProvidersClass = [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
                 }
 
