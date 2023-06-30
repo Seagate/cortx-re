31eb423a2697d5e54ea37bcdf96defdd23f6ddf2
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2023-06-30 19:30:03 +0530

    Added closing bracket
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- jenkins/automation/vra_infra_provisioner.groovy
+++ jenkins/automation/vra_infra_provisioner.groovy
@@ -65,7 +65,7 @@ pipeline {
                     pushd solutions/vmware/terraform/
                         mv /etc/yum.repos.d/rockylinux-8.4.repo /etc/yum.repos.d/rockylinux-8.4.repo.bkp
                         yum-config-manager --add-repo http://fwinfra-katello2.colo.seagate.com/pulp/content/FWINFRA/Development/Rocky-8-SSC/custom/HashiCorp_EL8_x86_64/HashiCorp_EL8_x86_64/
-                        echo 'gpgcheck = 0 >> /etc/yum.repos.d/fwinfra-katello2.colo*.repo
+                        echo 'gpgcheck = 0' >> /etc/yum.repos.d/fwinfra-katello2.colo*.repo
                         yum -y install terraform
                         rm -rf .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup
                         terraform init
